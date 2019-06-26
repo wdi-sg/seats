e1384bbe6a6c804a734588026d4e0113049b6db1
EliseChia <swedesurrender@gmail.com>
2019-06-26 20:52:49 +0800

    Part 1 (done by increasing the ticket price by 1.05% for each person after the first, need to go back and change it to +2.50 for each person)

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-console.log("hello script js");
+var numberOfTakenSeats = 1;
+var ticketPrice = 50;
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
-};
+var inputHappened = function(currentInput) {
+    inputLC = currentInput.toLowerCase();
+    console.log(inputLC);
+        if (inputLC === "buy") {
+            if (numberOfTakenSeats === 1) {
+                ticketPrice = (numberOfTakenSeats * ticketPrice);
+                console.log(ticketPrice);
+                var data = `Your tickets costs $${ticketPrice}.`;
+                console.log(data);
+                display(data);
+            } else if (numberOfTakenSeats > 1 && numberOfTakenSeats <=10 ) {
+                ticketPrice = (ticketPrice * 1.05);
+                console.log(ticketPrice);
+                data = `Your tickets costs $${ticketPrice}.`;
+                console.log(data);
+                display(data);
+            }
+            numberOfTakenSeats += 1;
+            console.log(numberOfTakenSeats);
+            }
+            else {
+                display("Please input buy");
+            }
+        };
