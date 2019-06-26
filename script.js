3256e20df6391538299634ee84dc40ba5c530885
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-06-26 20:26:52 +0800

    first commit, part 1 of assignment

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 console.log("hello script js");
 
+var totalSeats = 10;
+var boughtSeats = 1;
+var seatPrice = 0;
+
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  //display( "WOW SOMETHING HAPPENED" );
+  input = currentInput.toLowerCase();
+  console.log(`Lower Case ${input}`);
+  if (input === "buy") {
+    if (boughtSeats === 1) {
+        seatPrice = 50;
+        console.log(seatPrice);
+        display(seatPrice);
+        //return seatPrice;
+    } else if (boughtSeats > 1 || boughtSeats < 10) {
+        seatPrice = seatPrice * 1.05;
+        console.log(seatPrice);
+        display(seatPrice);
+    }
+    boughtSeats = boughtSeats +1;
+    totalSeats = totalSeats - 1;
+    console.log(boughtSeats);
+    console.log(totalSeats);
+  } else if (boughtSeats = 0) {
+    display("Tickets are sold out.")
+} else {
+    display("Do you want to BUY the ticket?");
+  }
 };
+
+
+/*var pricing = function(totalSeats) {
+
+};*/
