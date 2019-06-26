6e9813ef26cbfa16827f47a9d50321df24d7cd3c
Yi Xin Ang <angyixin@gmail.com>
2019-06-26 21:31:22 +0800

    buy button and total seats countdown with feedback

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
-};
+// var inputHappened = function(currentInput){
+//   console.log( currentInput );
+//   display( "WOW SOMETHING HAPPENED" );
+// };
+
+var totalSeats = 10;
+var ticketPrice = 50;
+
+
+document.getElementById("buy").addEventListener("click", buySeat);
+document.getElementById("price").textContent = "$" + ticketPrice;
+
+
+function buySeat(){
+  document.getElementById("feedback").textContent = "seat bought at" + " $" + ticketPrice;
+  setTimeout(function(){
+     document.getElementById("feedback").textContent = "";
+   }, 1000);
+  totalSeats = totalSeats - 1;
+  console.log(totalSeats);
+}
+// var buySeat = function(){
+//   console.log("test");
+// }
+
+// onclick , buySeat, getNewPrice, displayPrice
+// for each seat sold, each seat remaining cost 50% more.
+//   when user buy a seat, seat sold +1 ,
