e079f7e549a905c7f1ee3d9b8e44896e17575de6
EliseChia <swedesurrender@gmail.com>
2019-06-26 21:28:29 +0800

    Part 2 - Different fixed pricing for seats 1-5, 6-9 and 10.

--- script.js
+++ script.js
@@ -5,18 +5,26 @@ var inputHappened = function(currentInput) {
     inputLC = currentInput.toLowerCase();
     console.log(inputLC);
         if (inputLC === "buy") {
-            if (numberOfTakenSeats === 1) {
-                ticketPrice = (numberOfTakenSeats * ticketPrice);
+            if (numberOfTakenSeats > 0 && numberOfTakenSeats < 6) {
+                ticketPrice = 51.5;
                 console.log(ticketPrice);
                 var data = `Your tickets costs $${ticketPrice}.`;
                 console.log(data);
                 display(data);
-            } else if (numberOfTakenSeats > 1 && numberOfTakenSeats <=10 ) {
-                ticketPrice = (ticketPrice * 1.05);
+            } else if (numberOfTakenSeats >= 6 && numberOfTakenSeats < 10 ) {
+                ticketPrice = 52.5;
                 console.log(ticketPrice);
                 data = `Your tickets costs $${ticketPrice}.`;
                 console.log(data);
                 display(data);
+            } else if (numberOfTakenSeats === 10) {
+                ticketPrice = 91000;
+                console.log(ticketPrice);
+                data = `Your tickets costs $${ticketPrice}.`;
+                console.log(data);
+                display(data);
+            } else {
+                display("Tickets are sold out!")
             }
             numberOfTakenSeats += 1;
             console.log(numberOfTakenSeats);
@@ -25,3 +33,5 @@ var inputHappened = function(currentInput) {
                 display("Please input buy");
             }
         };
+
+        //string.include();
