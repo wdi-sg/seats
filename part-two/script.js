b3a7811acb6360aa5651754e0001a962279e2a43
gitwenlei <ngwenlei@gmail.com>
2019-06-26 21:34:54 +0800

    Update display of ticket prices

--- part-two/script.js
+++ part-two/script.js
@@ -30,10 +30,13 @@ var inputHappened = function (userInput) {
         //display("You ticket costs $" + newPrice );
         if (seatsSold <= 5) {
             console.log("You tickets costs 3% more");
+            display("You ticket costs $" + (ticketPrice + (ticketPrice * priceIncreased3Percent)) );
         } else if (seatsSold > 5 && seatsSold <= 9) {
             console.log("You tickets costs 5% more");
+            display("You ticket costs $" + (ticketPrice + (ticketPrice * priceIncreased5Percent)) );
         } else {
             console.log("You pay $91,000");
+            display("You ticket costs $91,000" );
         }
     } else {
         console.log("Please enter 'buy' to purchase ticket.");
