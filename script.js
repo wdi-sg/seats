f208a83d5887c11f1ca824f1223f659d98f48cb3
Jonathan <jonathanbek93@gmail.com>
2019-06-26 22:13:31 +0800

    finished setup, inputHappened unfinished

--- script.js
+++ script.js
@@ -1,6 +1,57 @@
-console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+
+
+
+
+// console.log(50 / 100 * 5);
+// //ORIGINAL INPUT CODE
+// var inputHappened = function(currentInput){
+//   console.log( currentInput );
+//   display( "WOW SOMETHING HAPPENED" );
+// };
+
+
+
+// code:
+// create global variable starting price = 50
+// every subsequent ticket = +2.5
+//price increase (2.5) and number of buys (number) --> total increase to price
+//current price = starting price + total increase to price
+//function inputHappened MUST calculate starting price + total increase to price AND add to the numberOfBuys counter
+
+var startPrice = 50;
+var eachIncrement = 2.5;
+var numberOfBuys = 0;
+var increaseToPrice = 0;
+var currentPrice = 50;
+
+//function = each increment * number of buys = increase to price
+var calcIncreaseToPrice = function(eachIncrement, numberOfBuys) {
+    increaseToPrice = eachIncrement * numberOfBuys;
+}
+
+//initial call to establish base increase to price
+calcIncreaseToPrice(eachIncrement, numberOfBuys);
+
+//function = starting price + increase to price = current price
+var calcCurrentPrice = function(startPrice, increaseToPrice){
+    currentPrice = startPrice + increaseToPrice;
+}
+
+//initial call to establish initial current price
+calcCurrentPrice(startPrice, increaseToPrice);
+
+
+
+console.log("Initial value of increaseToPrice :" + increaseToPrice);
+console.log("Initial value of currentPrice :" + currentPrice);
+
+// when input,
+// show current price
+// update number of buys
+var inputHappened = function(currentInput, currentPrice){
+  console.log( "You entered: " + currentInput );
+  console.log("You paid: " + currentPrice);
+
+
 };
