b401cea8d27597717f3c23dde024118e9bf86cde
Yew <limtheanyew@gmail.com>
2019-06-27 00:19:16 +0800

    changes to script.js file

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 console.log("hello script js");
 
 var numSeats = 10;
+var numSeatsLeft = numSeats;
 var priceSeat = 50;
 var beginningPriceSeat = 50;
+var numSeatsLeftBracket = 5;
+
+var seatTierIndicate = function(seatsLeft){
+    if (seatsLeft < numSeats/2){
+        return "Current Price Bracket: +5%. Total Seats Left: " + seatsLeft;
+    }
+    else{
+        return "Current Price Bracket: +3%. Number of Seats Left in this price bracket: " + numSeatsLeftBracket + " . Total Seats Left: " + seatsLeft;
+    }
+};
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
 
   var message = '';
 
-  if (currentInput === "buy" && numSeats > 0) {
+  if (currentInput === "buy" && numSeatsLeft > 0) {
     message = "Seat Sold. Price: $"+priceSeat.toFixed(2);
+    if (numSeatsLeft > (numSeats/2 + 1)){
+        priceSeat += beginningPriceSeat * 0.03;
+    }
+    else{
+        priceSeat += beginningPriceSeat * 0.05;
+    }
     //priceSeat *= 1.05;
-    priceSeat += beginningPriceSeat * 0.05
-    numSeats--;
+    numSeatsLeft--;
+    numSeatsLeftBracket--; //for the price bracket indicator
   }
   else if (currentInput === "buy"){
     message = "Sorry, seats sold out.";
@@ -21,6 +38,6 @@ var inputHappened = function(currentInput){
   else{
     message = "Please enter a valid input.";
   };
-    display(message);
+    display(message,seatTierIndicate(numSeatsLeft));
     console.log(message);
 };
