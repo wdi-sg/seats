466ef4f8cc1d0515f1bd0ad72893c02eaa3c845c
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-06-27 08:51:47 +0800

    added logic for part 2, addition of 3% and 5% of $50 (depends on tier)

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ var seatPrice = 0;
   }
 };*/
 
-//part 2
+//part 2 & part 4
 var inputHappened = function(currentInput){
   console.log( currentInput );
   //display( "WOW SOMETHING HAPPENED" );
@@ -45,12 +45,14 @@ var inputHappened = function(currentInput){
         display(`The price of this ticket is $${seatPrice}.`);
         //If statement for seats 2 to 5
     } else if (boughtSeats > 1 && boughtSeats <= 5) {
-        seatPrice = seatPrice * 1.03; //cumulative 3% on previous ticket price
+        //seatPrice = seatPrice + (50*0.03); //cumulative price which adds 3% of $50 on to previous ticket price (part 2)
+        seatPrice = seatPrice * 1.03; //cumulative 3% on previous ticket price (part 4)
         console.log(seatPrice);
         display(`The price of this ticket is $${seatPrice}.`);
         //If statement for seats 6 to 9
     } else if (boughtSeats >= 6 && boughtSeats < 10) {
-        seatPrice = seatPrice * 1.05; //cumulative 5% on previous ticket price
+        //seatPrice = seatPrice + (50*0.05); //cumulative price which adds 5% of $50 on to previous ticket price (part 2)
+        seatPrice = seatPrice * 1.05; //cumulative 5% on previous ticket price (part 4)
         console.log(seatPrice);
         display(seatPrice);
     } else if (boughtSeats = 10) {
