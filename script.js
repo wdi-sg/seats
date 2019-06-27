2cc2c4609091521ec1b9c4bb6dc49d344ba04843
w0bbbles <wobblebeat@gmail.com>
2019-06-27 17:14:26 +0800

    Seats new file

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-console.log("hello script js");
+//Price of empty seat (number)
+var emptySeatPrice = 50;
+console.log(emptySeatPrice + " " + typeof emptySeatPrice);
+
+// Price of sold seat (number) - part 1
+var soldSeatPrice = 1.05 * emptySeatPrice;
+
+// No of seats left (number)
+    var seatsLeft = 0;
+    console.log(seatsLeft + " " + typeof seatsLeft);
+
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+    console.log(currentInput + "hi")
+  if (currentInput === "b" && seatsLeft <= 10) {
+    display ("Purchase tix at $" + soldSeatPrice);
+    seatsLeft = seatsLeft + 1;
+    console.log(seatsLeft)
+  }
+  else {
+    display ("purchase cancelled");
+  }
 };
