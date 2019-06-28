61f791d65d4c5abde7be199077793ed2545f45fe
thea-fan <julythefourteen@gmail.com>
2019-06-28 20:25:54 +0800

    Third further complete

--- script.js
+++ script.js
@@ -22,13 +22,13 @@ var inputHappened = function (currentInput) {
         newPrice = price * 1.05;
 		seatLeft = seatLeft - 1;
         display(`The price is : $${newPrice}`)
-		
+
     }else if (seatLeft === 10){
-		display(`The price is : $${price}`); 
+		display(`The price is : $${price}`);
 		seatLeft = seatLeft - 1;
 
 	}else {
-		display(`Sorry, tickets sold out`); 
+		display(`Sorry, tickets sold out`);
 	}
 	console.log('Number of seats left: '+ seatLeft);
 };
@@ -55,34 +55,34 @@ var inputHappened = function (currentInput) {
         newPrice = price * 1.03; //price should return 51.5
 		seatLeft = seatLeft - 1;
         display(`The price is : $${newPrice}`)
-		
+
     }else if (seatLeft > 1 && seatLeft < 6){
 		newPrice = price * 1.05; // price should return 52.5
 		seatLeft = seatLeft - 1;
         display(`The price is : $${newPrice}`)
 
 	}else if (seatLeft === 1){
-		newPrice = '91,000'; 
-		display(`The price is : $${newPrice}`); 
+		newPrice = '91,000';
+		display(`The price is : $${newPrice}`);
 		seatLeft = seatLeft - 1;
 
 	}else if (seatLeft === 10){
-		display(`The price is : $${price}`); 
+		display(`The price is : $${price}`);
 		seatLeft = seatLeft - 1;
 
 	} else {
-		display(`Sorry, tickets sold out`); 
+		display(`Sorry, tickets sold out`);
 	}
 	console.log('Number of seats left: '+ seatLeft);
 };
 */
 
-/* SECOND FURTHER 
+/* SECOND FURTHER
 
 When outputing, a message tell the user how many tickets are left before the price bracket goes up.
 */
 
-/* SECOND FURTHER CODE 
+/* SECOND FURTHER CODE
 
 var seatLeft = 10;
 var seatLeftFirstTier = 4;
@@ -91,40 +91,40 @@ console.log('Number of seats left: '+ seatLeft);
 var price = 50;
 
 var inputHappened = function (currentInput) {
-	
+
     if (seatLeft > 5 && seatLeft < 10) {
         newPrice = price * 1.03; //price should return 51.5
 		seatLeft = seatLeft - 1;
 		seatLeftFirstTier = seatLeftFirstTier - 1;
-        display(`The price is : $${newPrice}. 
+        display(`The price is : $${newPrice}.
 		You are ${seatLeftFirstTier} tickets away from the next price surge!`)
-		
+
     }else if (seatLeft > 1 && seatLeft < 6){
 		newPrice = price * 1.05; // price should return 52.5
 		seatLeft = seatLeft - 1;
         seatLeftSecondTier = seatLeftSecondTier - 1;
-        display(`The price is : $${newPrice}. 
+        display(`The price is : $${newPrice}.
 		You are ${seatLeftSecondTier} tickets away from the next price surge!`)
 
 	}else if (seatLeft === 1){
-		newPrice = '91,000'; 
+		newPrice = '91,000';
 		display(`The price is : $${newPrice}.
-		You grabbed the last ticket!`); 
+		You grabbed the last ticket!`);
 		seatLeft = seatLeft - 1;
 
 	}else if (seatLeft === 10){
 		display(`The price is : $${price}.
-		You grabbed the last cheapest ticket!`); 
+		You grabbed the last cheapest ticket!`);
 		seatLeft = seatLeft - 1;
 
 	} else {
-		display(`Sorry, tickets sold out`); 
+		display(`Sorry, tickets sold out`);
 	}
 	console.log('Number of seats left: '+ seatLeft);
 };
 */
 
-/* THIRD FURTHER  -- ######INCOMPLETE
+/* THIRD FURTHER
 
 Change the pricing to increase by 3% and 5% over the current price of the ticket. (i.e., each ticket is some percentage more than the last)
 
@@ -140,8 +140,8 @@ var price = 50;
 
 /*Round off prices to 2dp*/
 function precise_round(num, dec){
-  if ((typeof num !== 'number') || (typeof dec !== 'number')) 
-return false; 
+  if ((typeof num !== 'number') || (typeof dec !== 'number'))
+return false;
 
   var num_sign = num >= 0 ? 1 : -1;
   return (Math.round((num*Math.pow(10,dec))+(num_sign*0.0001))/Math.pow(10,dec)).toFixed(dec);
@@ -156,40 +156,40 @@ function isEmpty(currentInput){
 }
 
 var inputHappened = function (currentInput) {
-	
-	
+
+
     if ( !isEmpty ){
-		display(`Pls type something`); 
-	
+		display(`Pls type something`);
+
 	 }else if ( typeof currentInput != null && seatLeft > 5 && seatLeft < 10) {
-        price = price * 1.03; 
+        price = price * 1.03;
 		price = precise_round(price,2);
 		seatLeft = seatLeft - 1;
 		seatLeftFirstTier = seatLeftFirstTier - 1;
-        display(`The price is : $${price}. 
+        display(`The price is : $${price}.
 		You are ${seatLeftFirstTier} tickets away from the next big price surge!`)
-		
+
     }else if (typeof currentInput != null  && seatLeft > 1 && seatLeft < 6){
-		price = price * 1.05; 
+		price = price * 1.05;
 		price = precise_round(price,2);
 		seatLeft = seatLeft - 1;
         seatLeftSecondTier = seatLeftSecondTier - 1;
-        display(`The price is : $${price}. 
+        display(`The price is : $${price}.
 		You are ${seatLeftSecondTier} tickets away from the next big price surge!`)
 
 	}else if (typeof currentInput != null  && seatLeft === 1){
-		newPrice = '91,000'; 
+		newPrice = '91,000';
 		display(`The price is : $${newPrice}.
-		You grabbed the last ticket!`); 
+		You grabbed the last ticket!`);
 		seatLeft = seatLeft - 1;
 
 	}else if (typeof currentInput != null  && seatLeft === 10){
 		display(`The price is : $${price}.
-		You grabbed the last cheapest ticket!`); 
+		You grabbed the last cheapest ticket!`);
 		seatLeft = seatLeft - 1;
 
 	} else if (typeof currentInput != null  && seatLeft === 0) {
-		display(`Sorry, tickets sold out`); 
+		display(`Sorry, tickets sold out`);
 	}
 	console.log('Number of seats left: '+ seatLeft);
 
