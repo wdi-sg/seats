847d5bd3a0929490b891b46f0e66e0020d7740a1
datguyrhy <54259724+datguyrhy@users.noreply.github.com>
2019-09-19 23:18:15 +0800

    Add files via upload
    created ticket countdown & price calculator (basic deliverables)

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 console.log("hello script js");
 
+var ticket = {
+noOfSeats: 10,
+basePrice: 50
+};
+var seatsLeft = function(a, b){ 
+	var sub = a - b;
+	console.log('hi');
+	return sub;
+};
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+	var result = seatsLeft(ticket.noOfSeats, 1);
+	 	ticket.noOfSeats = result;
+		priceUp = 1.05;
+		var tixPrice = ticket.basePrice * priceUp;
+		display(tixPrice);
+  	 	return "Price is: $ " + tixPrice + " & seats left are : " + result;
 };
