17518bdd6bda88bc391b93d8fa33d2e70f1f6939
“woofborn” <wolf.born@gmail.com>
2019-09-19 18:18:11 +0800

    Calculate 3% and 5% increment on first/second half of plane

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
 console.log("hello script js");
 
-var ticketPrice = 50;
-var incrementPrice = 0.05 * 50;
-var planeSeats = 10;
+//PART 1 OF HOMEWORK
+// var ticketPrice = 50;
+// var incrementPrice = 0.05 * 50;
+// var planeSeats = 10;
 
-// function ticketCalculate(){
 
-//     ticketPrice = ticketPrice + incrementPrice;
-//     planeSeats -=;
+// var inputHappened = function(currentInput){
+//   console.log( currentInput );
 
-//         if (planeSeats === 0){
+//   if (planeSeats === 10){
+//     planeSeats --;
+//     return `Your plane ticket costs $${ticketPrice.toFixed(2)}`
+//   } else {
+//    ticketPrice = ticketPrice + incrementPrice;
+//     planeSeats --;
+
+//         if (planeSeats < 0){
 //             alert ("TOO BAD NO MORE SEATS");
 //         }
-//     return `Your plane ticket costs ${ticketPrice}`;
+//     console.log(ticketPrice);
+//     return `Your plane ticket costs $${ticketPrice.toFixed(2)}`;
+//     }
+// };
 
-// }
+//PART 2 OF HOMEWORK
+var ticketPrice = 50;
+var threeIncrement = 0.03 * 50;
+var fiveIncrement = 0.05 * 50;
+var planeSeats = 0;
+
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
 
-  if (planeSeats === 10){
-    planeSeats --;
-    return `Your plane ticket costs $${ticketPrice.toFixed(2)}`
-  } else {
-   ticketPrice = ticketPrice + incrementPrice;
-    planeSeats --;
-
-        if (planeSeats < 0){
-            alert ("TOO BAD NO MORE SEATS");
-        }
-    console.log(ticketPrice);
-    return `Your plane ticket costs $${ticketPrice.toFixed(2)}`;
+    if (planeSeats === 0){
+        planeSeats++;
     }
-};
+    else if (planeSeats <= 5){
+       ticketPrice = ticketPrice + threeIncrement;
+        planeSeats++;
+    }
+    else {
+        ticketPrice = ticketPrice + fiveIncrement;
+        planeSeats++;
+    };
+
+        if (planeSeats > 10){
+                alert ("TOO BAD NO MORE SEATS");
+        }
+
+    console.log(ticketPrice);
+    return `Your plane ticket costs $${ticketPrice}`;
+}
