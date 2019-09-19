e1698ec5c1ab765a8b2db29193d550fadd0e095d
David M <davidmosk@gmail.com>
2019-09-19 19:23:07 +0800

    working with countdown of tickets and price increases

--- script.js
+++ script.js
@@ -3,24 +3,37 @@ console.log("hello script js");
 var startingPrice = 50;
 var numberOfSeats = 10;
 var increasingPrice = 50;
+var warning ="";
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-// make a number and check the input is valid
- currentInput = Number(currentInput);
- if (isNaN(currentInput)) { 
-  return "Please enter a number"; }
-  return currentInput;
-};
+// make a number and check the input is valid (no need to check...)
+ // currentInput = Number(currentInput);
+ // if (isNaN(currentInput)) { 
+ //  return "Please enter a number"; }
+ 	increaseCalculation();
+	decreaseTickets()
+	if(numberOfSeats<0) {
+  return "No More Tickes Available";
+}
+	if(numberOfSeats===1) {
+warning ="Only 1 seat available!";
+}
+	if(numberOfSeats===0) {
+warning ="All Tickets Sold!"}
+  return "Price you bought at: " + increasingPrice + "\n  Number of seats left: " + numberOfSeats + "\n " + warning;
+	}
+
 
 
 /// plane has 10 seats
 /// check how many seats are available
 /// tickets are $50, but for each one sold increase the price 5%
 
-var startingPrice = 50;
-var numberOfSeats = 10;
-var increasingPrice = 50;
 var increaseCalculation = function (){
 increasingPrice = increasingPrice * 1.05; 
+} 
+
+var decreaseTickets = function (){
+numberOfSeats  = numberOfSeats -1; 
 } 
