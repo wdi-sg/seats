6f8c97a013ab018eb6486d5f72635f3b9cd04ad3
garricktgh <garrick@hotmail.sg>
2019-09-19 17:33:22 +0800

    complete deliverables

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 console.log("hello script js");
+var planeSeats = 10;
+var seatPrice = 50;
+var seatsSold = 1;
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+  var output = "";
+  planeSeats = planeSeats - 1;
+  if (planeSeats <= 0 ) {
+    output = "sorry, seats are sold out";
+  } else {
+    var purchasePrice = parseInt(seatPrice) * (1 + (0.05 * seatsSold));
+        console.log(0.05 * parseInt(seatsSold));
+    seatsSold = seatsSold + 1;
+    console.log(seatsSold);
+    output = "Thank you for purchasing a seat at $" + purchasePrice.toFixed(2) + ". There are " + planeSeats + "left.";
+}
+  return output;
 };
