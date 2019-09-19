429b7df57a9812233740ccfd9c0b850382554a01
reshinto <terencekong2002@gmail.com>
2019-09-19 19:17:35 +0800

    completed basic requirements

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-console.log("hello script js");
+const input = document.getElementById("input");
+input.placeholder = "Buy a ticket (y/n)?";
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+let numOfTicketsSold = 0;
+
+const plane = {
+  seats: 10,
+};
+
+let seatPrice = 0;
+
+const inputHappened = function(currentInput) {
+  if (!isNaN(Number(currentInput))) {
+    return `${currentInput} not allowed. Key in y or n.`;
+  }
+  if (currentInput.toLowerCase() === "y") {
+    seatPrice =
+      numOfTicketsSold === 0 ? 50 : (seatPrice = (seatPrice * 1.05).toFixed(2));
+  } else {
+    return "Ok...";
+  }
+  plane.seats -= 1;
+  numOfTicketsSold += 1;
+  return `You paid for $${seatPrice} for an Airplane seat.`;
 };
