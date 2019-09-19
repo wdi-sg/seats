889cde01bb3b016b531c8b4c84ccf140041db377
datguyrhy <54259724+datguyrhy@users.noreply.github.com>
2019-09-19 23:33:41 +0800

    Add files via upload
    add countdown before price tier change & last ticket alert

--- script.js
+++ script.js
@@ -17,25 +17,26 @@ var inputHappened = function(currentInput){
 		console.log( currentInput );
 		var tixPrice = "Ticket is $ " + ticket.basePrice;
 		display(tixPrice);
-		return "Price is: $ " + tixPrice + " & seats left are : " + result;
+		return "Price is: $ " + tixPrice + " seats left are : " + result;
 }	else if(ticket.noOfSeats < 10 && ticket.noOfSeats >5){
 		console.log(result);
+		var tierChange = result - 5;
 		priceUp1 = 1.03;
 		var tixPrice = ticket.basePrice * priceUp1;
 		display(tixPrice);
-  	 	return "Price is: $ " + tixPrice + " & seats left are : " + result;
+  	 	return "Price is: $ " + tixPrice + "\n Seats left are : " + result + "\n" + "No. of ticket before price increases!: " + tierChange;
 }	else if(ticket.noOfSeats <= 5 && ticket.noOfSeats >=2){
 		console.log(result);
 		priceUp = 1.05;
 		var tixPrice = ticket.basePrice * priceUp;
 		display(tixPrice);
-  	 	return "Price is: $ " + tixPrice + " & seats left are : " + result;
+  	 	return "Price is: $ " + tixPrice + "\n Seats left are : " + result;
 }
 	else if(ticket.noOfSeats === 1){
-		console.log(result);
+		alert("THIS IS THE LAST TICKET!!")
 		var tixPrice = 91000;
 		display(tixPrice);
-  	 	return "Price is: $ " + tixPrice + " & seats left are : " + result;
+  	 	return "Price is: $ " + tixPrice + "\n Seats left are : " + result;
 }
-	return "SOLD OUT!"
+	return "We are all outta tickets! SOLD OUT"
 };
