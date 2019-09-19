020353bbf57b1e126bfb3473f1e26556728d2043
datguyrhy <54259724+datguyrhy@users.noreply.github.com>
2019-09-19 23:20:19 +0800

    Add files via upload
    (further deliverable 1) added price tiers.

--- script.js
+++ script.js
@@ -12,9 +12,30 @@ var seatsLeft = function(a, b){
 
 var inputHappened = function(currentInput){
 	var result = seatsLeft(ticket.noOfSeats, 1);
-	 	ticket.noOfSeats = result;
+	 ticket.noOfSeats = result;
+	if(ticket.noOfSeats === 10){
+		console.log( currentInput );
+		var tixPrice = "Ticket is $ " + ticket.basePrice;
+		display(tixPrice);
+		return "Price is: $ " + tixPrice + " & seats left are : " + result;
+}	else if(ticket.noOfSeats < 10 && ticket.noOfSeats >5){
+		console.log(result);
+		priceUp1 = 1.03;
+		var tixPrice = ticket.basePrice * priceUp1;
+		display(tixPrice);
+  	 	return "Price is: $ " + tixPrice + " & seats left are : " + result;
+}	else if(ticket.noOfSeats <= 5 && ticket.noOfSeats >=2){
+		console.log(result);
 		priceUp = 1.05;
 		var tixPrice = ticket.basePrice * priceUp;
 		display(tixPrice);
   	 	return "Price is: $ " + tixPrice + " & seats left are : " + result;
+}
+	else if(ticket.noOfSeats === 1){
+		console.log(result);
+		var tixPrice = 91000;
+		display(tixPrice);
+  	 	return "Price is: $ " + tixPrice + " & seats left are : " + result;
+}
+	return "SOLD OUT!"
 };
