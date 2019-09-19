225a3c6b832c1e2f653a72e5a633adad14cbdfea
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-09-19 17:43:23 +0800

    Updated function to minus a seat and show user the price

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-console.log("hello script js");
+// Global variable for seatsAvailable = 10
+var seatsAvailable = 10;
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+
+    // If currentInput is not empty,
+    if(currentInput !== null) {
+
+        // minus 1 seat from seatsAvailable
+        // store the new value back to seatsAvailable
+        seatsAvailable = seatsAvailable - 1;
+
+        // set price = $50 + 5%;
+        var price = 50 + (50 * 0.05);
+
+        // return price to user
+        var result = "Your seat has been reserved. Please pay $ " + price + "\n" + "Number of Seats Available: " + seatsAvailable ;
+
+        display(result);
+
+    } else {
+
+        // If current Input is empty,
+            // set price is $50
+
+        var price = 50;
+        var result = "Current price " + price;
+
+        display(result);
+    }
 };
