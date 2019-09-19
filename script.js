b32b9477cdbf849cc641a2020e02a675feaa83e4
“woofborn” <wolf.born@gmail.com>
2019-09-19 17:48:43 +0800

    Calculate ticket price increment 5% of $50

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 console.log("hello script js");
 
+var ticketPrice = 50;
+var incrementPrice = 0.05 * 50;
+var planeSeats = 10;
+
+// function ticketCalculate(){
+
+//     ticketPrice = ticketPrice + incrementPrice;
+//     planeSeats -=;
+
+//         if (planeSeats === 0){
+//             alert ("TOO BAD NO MORE SEATS");
+//         }
+//     return `Your plane ticket costs ${ticketPrice}`;
+
+// }
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+
+  if (planeSeats === 10){
+    planeSeats --;
+    return `Your plane ticket costs $${ticketPrice.toFixed(2)}`
+  } else {
+   ticketPrice = ticketPrice + incrementPrice;
+    planeSeats --;
+
+        if (planeSeats < 0){
+            alert ("TOO BAD NO MORE SEATS");
+        }
+    console.log(ticketPrice);
+    return `Your plane ticket costs $${ticketPrice.toFixed(2)}`;
+    }
 };
