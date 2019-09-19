443b82d6c9049a3d8b9aa932a2e357328dad88d8
kevinngth <kevin.ng.th@gmail.com>
2019-09-19 23:04:45 +0800

    first logic done

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 console.log("hello script js");
 
+let seats = 10;
+
+let price = 47.62;
+
+let inputTracker = 0;
+
+// function to reduce seats
+let seatSold = function() {
+    seats -= 1;
+    console.log(seats);
+};
+
+// function to increase price by 5%
+let priceHike5 = function() {
+    price = price * 1.05;
+    price = parseFloat(price.toFixed(2))
+    console.log(price);
+}
+
+// function to increase price by 3%
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+
+// tracking to ensure the user's input is recorded.
+    inputTracker += 1;
+    console.log(inputTracker);
+
+// activate purchase, check if seats are still available.
+    if (seats > 2) {
+        priceHike()
+        seatSold();
+        return `You just bought a seat for $${price}, congratulations! There are ${seats} seats left.`;
+    } else if (seats === 2) {
+
+
+    } else if (seats === 1) {
+
+    } else {
+
+    };
+
+
+  return "statement";
 };
