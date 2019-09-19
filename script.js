ce4d1ecd722fdcf16dbec4ebc105392fa7019079
dannyozh <danielongzhihao@gmail.com>
2019-09-19 18:36:41 +0800

    changed exercise to reflect static increment of 2.50

--- script.js
+++ script.js
@@ -3,21 +3,32 @@ console.log("hello script js");
 //define the variables that you will need
 
 var baseCharge = 50;
-var addCharge = baseCharge * 1.05;
 var ticketCount = 10;
 
 //define the calculation function based on your global variables
 
 var calculateTicketPrice = function() {
-    baseCharge = baseCharge * 1.05;
+    baseCharge = baseCharge + 2.50;
     ticketCount --;
 };
+
+var firstBuyerPrice = function() {
+    // baseCharge;
+    console.log('in function firstBuyerPrice')
+    ticketCount --;
+    console.log(ticketCount)
+}
+
 
 //call your function within the main input function
 
 var inputHappened = function(currentInput){
     console.log( currentInput );
-    if (ticketCount > 0) {
+    if (ticketCount === 10) {
+        console.log(ticketCount);
+        firstBuyerPrice();
+        display ("Your ticket price is " + baseCharge);
+    } else if (ticketCount < 10) {
         calculateTicketPrice ();
         console.log(ticketCount)
         display ("Your ticket price is " + baseCharge);
