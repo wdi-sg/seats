fff3660f2e43604782433af16a5c684064132349
“woofborn” <wolf.born@gmail.com>
2019-09-19 19:13:20 +0800

    Alert remaining tickets until 5% increment

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("hello script js");
 
-//PART 1 OF HOMEWORK
+// // PART 1 OF HOMEWORK
 // var ticketPrice = 50;
 // var incrementPrice = 0.05 * 50;
 // var planeSeats = 10;
@@ -24,7 +24,49 @@ console.log("hello script js");
 //     }
 // };
 
-//PART 2 OF HOMEWORK
+// //PART 2 OF HOMEWORK
+// var ticketPrice = 50;
+// var threeIncrement = 0.03 * 50;
+// var fiveIncrement = 0.05 * 50;
+// var planeSeats = 1;
+
+
+// var inputHappened = function(currentInput){
+
+//  console.log(ticketPrice);
+//     console.log(planeSeats);
+//       if (planeSeats > 10) {
+//     alert ("TOO BAD NO MORE SEATS");
+//     }
+
+//        if (planeSeats === 1){
+//         planeSeats++;
+
+//     }
+//     else if (planeSeats <= 5){
+//        ticketPrice = ticketPrice + threeIncrement;
+//         planeSeats++;
+
+//     }
+//     else if (planeSeats <= 9){
+//         ticketPrice = ticketPrice + fiveIncrement;
+//         planeSeats++;
+
+//     }
+//     else if (planeSeats === 10){
+//     ticketPrice = 91000;
+//     planeSeats++
+//     return `Your plane ticket costs $${ticketPrice}...have you considered walking?`
+//     };
+
+//     return `Your plane ticket costs $${ticketPrice}`;
+
+//     console.log(ticketPrice);
+//     console.log(planeSeats);
+//     ;
+// }
+
+//PART 3 OF HOMEWORK
 var ticketPrice = 50;
 var threeIncrement = 0.03 * 50;
 var fiveIncrement = 0.05 * 50;
@@ -45,8 +87,10 @@ var inputHappened = function(currentInput){
     }
     else if (planeSeats <= 5){
        ticketPrice = ticketPrice + threeIncrement;
-        planeSeats++;
+       var remain = 5 - planeSeats;
+       planeSeats++;
 
+       return `Your plane ticket costs $${ticketPrice}. There are ${remain} seats left before the prices goes up EVEN MORE!`;
     }
     else if (planeSeats <= 9){
         ticketPrice = ticketPrice + fiveIncrement;
@@ -59,9 +103,8 @@ var inputHappened = function(currentInput){
     return `Your plane ticket costs $${ticketPrice}...have you considered walking?`
     };
 
-    return `Your plane ticket costs $${ticketPrice}`;
+    return `Your plane ticket costs $${ticketPrice}.`;
 
     console.log(ticketPrice);
     console.log(planeSeats);
-    ;
 }
