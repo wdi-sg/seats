3d9e52d92988621b3e28dcfc0e48b8258c764e54
Nathanael Tan <nathanael.tan92@gmail.com>
2019-09-19 22:22:35 +0800

    Main Logic for pricing and seats remaining done

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
-};
+// Plane Seats
+
+var seats = 10;
+var price = 50;
+
+
+var input = document.getElementById('input');
+
+
+var inputHappened = function (currentInput) {
+
+    var priceIncThree = price + (price * 0.03)
+    var priceIncFive = price + (price * 0.05)
+
+
+    if (currentInput !== " " && seats > 5) {
+        seats -= 1
+        price = priceIncThree
+        input.value = ''
+        console.log("seats:" + seats)
+        console.log("price: " + Math.floor(price))
+        return `Purchased: One Seat , Price: $${price.toFixed(0)}, Seats remaining: ${seats}  `
+
+    } else if (currentInput !== " " && seats <= 5 && seats > 1) {
+        seats -= 1
+        price = priceIncFive
+        input.value = ""
+        console.log("seats:" + seats)
+        console.log("price: " + Math.floor(price))
+        return `Purchased: One Seat , Price: $${price.toFixed(0)}, Seats remaining: ${seats}  `
+
+    } else if (currentInput !== " " && seats === 1) {
+        seats -= 1
+        console.log("seats:" + seats)
+        console.log("price: " + Math.floor(price))
+        return `Purchased: Last Seat, Price: $91,000 `
+
+    } else if(currentInput !== " " && seats === 0){
+        return `Sorry we are SOLD OUT 😔😔`
+    }
+
+}
