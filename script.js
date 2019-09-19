9917fb5c2cab418ad82c0d7440275f0a4f915bff
kevinngth <kevin.ng.th@gmail.com>
2019-09-19 23:19:10 +0800

    done further 3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("hello script js");
 
 let seats = 10;
 
-let price = 47.62;
+let price = 48.54;
 
 let inputTracker = 0;
 
@@ -17,9 +17,14 @@ let priceHike5 = function() {
     price = price * 1.05;
     price = parseFloat(price.toFixed(2))
     console.log(price);
-}
+};
 
 // function to increase price by 3%
+let priceHike3 = function() {
+    price = price * 1.03;
+    price = parseFloat(price.toFixed(2))
+    console.log(price);
+};
 
 var inputHappened = function(currentInput){
 
@@ -28,19 +33,23 @@ var inputHappened = function(currentInput){
     console.log(inputTracker);
 
 // activate purchase, check if seats are still available.
-    if (seats > 2) {
+    if (seats > 5) {
+        priceHike3();
+        seatSold();
+        return `You just bought a seat for $${price}, congratulations! There are ${seats} seats left.`;
+    } else if (seats > 2) {
         priceHike5();
         seatSold();
         return `You just bought a seat for $${price}, congratulations! There are ${seats} seats left.`;
     } else if (seats === 2) {
         priceHike5();
         seatSold();
-        return `You just bought a seat for $${price}, please take note that there's only 1 seat left! Hurry!`
+        return `You just bought a seat for $${price}, please take note that there's only 1 seat left! Hurry!`;
     } else if (seats === 1) {
         priceHike5();
         seatSold();
-        return `You bought the last seat for $${price}, congratulations!`
+        return `You bought the last seat for $91,000, congratulations!`;
     } else {
-        return "Please take another flight."
+        return "Please take another flight.";
     };
 };
