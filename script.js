c167c4ea3da4c0e1a21aa4e21cf3b64a948f86fb
penelopezwq <penelope.zwq@gmail.com>
2019-09-20 08:55:32 +0800

    question 1 done

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 console.log("hello script js");
 
+var ticketPrice = 50;
+var noOfSeats = 10;
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+    console.log( currentInput );
+    if( noOfSeats > 0){
+    noOfSeats = noOfSeats - 1;
+    //noOfSeats --;
+    calculatePrice();
+    display("Price of ticket "+ ticketPrice + "\nNo of seats Left: " + noOfSeats)
+
+    }else{
+        display("No more")
+    }
 };
+
+var calculatePrice = function(){
+    ticketPrice = ticketPrice * 1.05
+};
+
+
+
+
+
+
+
+/* no of seats vs price of seats
+only 10 seats, starting price = $50
+if seat -1, price + 5%
+if whatever type = seat sold n show the user the price of the seat.
+Define your variables?
+    calculatePrice
+    seatsleft
+*/
+
+
+/*var calculatePrice = function() {
+    console.log("inside the sayHello function now...");
+    // 5
+    return "50 dollars";
+}*/
