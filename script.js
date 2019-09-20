2c0afec86985deeadaa7393fe89858d88423eca6
garricktgh <garrick@hotmail.sg>
2019-09-20 15:05:12 +0800

    commit changes to push to master

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 console.log("hello script js");
 var planeSeats = 10;
 var seatPrice = 50;
-var seatsSold = 1;
+var seatsSold = 0;
+var priceIncrement = 0.05;
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
   var output = "";
   planeSeats = planeSeats - 1;
-  if (planeSeats <= 0 ) {
-    output = "sorry, seats are sold out";
+  if (planeSeats < 0 ) {
+    output = "Sorry, seats are sold out";
   } else {
-    var purchasePrice = parseInt(seatPrice) * (1 + (0.05 * seatsSold));
+    var purchasePrice = parseInt(seatPrice) * (1 + (priceIncrement * seatsSold));
         console.log(0.05 * parseInt(seatsSold));
     seatsSold = seatsSold + 1;
     console.log(seatsSold);
-    output = "Thank you for purchasing a seat at $" + purchasePrice.toFixed(2) + ". There are " + planeSeats + "left.";
+    output = "Thank you for purchasing a seat at $" + purchasePrice.toFixed(2) + ". Seats left: " + planeSeats;
 }
   return output;
 };
+
+//planeSeats is 10
+//when seats sold is 0 price is 50
+//when seats sold is 1 price increase by 5% from price
+//when there is input, seat is sold
+//plane seat is reduced by 1
+//seats sold is increased by 1
+//price increment is 5% per ticket sold
+//purchasePrice is 50 * 100% + (5% * number of ticket sold)
+//if seats are sold out, show message that seats are sold out
