55815c9c39110cbf532ac330d83300d6bb6f34b4
syahirah <syahirah.suradi@gmail.com>
2019-09-20 15:05:41 +0800

    added some variables and if statements

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-    if (currentInput !== null) {
-        var seatsLeft = initialTotalSeats - 1;
-        var priceCharged = initialTicketPrice;
+//initial total seats in plane is 10
+//initial  ticket price is 50
+//var initialTotalSeats = 10;
+var initialTicketPrice = 50;
+var seatsLeft = 10;
+var priceCharged;
+var surcharge = 0.05;
+
+//After user typed input, show ticket price as 50 AND minus total seats by 1
+var inputHappened = function (seatsLeft, priceCharged, currentInput){
+   /* if (currentInput && seatsLeft === 10) {
+        surcharge = 0;
+        priceCharged = initialTicketPrice;
+        seatsLeft = seatsLeft - 1;
+
         console.log(seatsLeft);
         console.log(priceCharged);
+    return;
     }
-  //
-  display( "1 ticket booked. Please pay $" + priceCharged + ". Seats left: " + seatsLeft);
+
+};*/
+//If total seats is 10, ticket price is $50
+if (seatsLeft === 10) {
+        surcharge = 0;
+        priceCharged = initialTicketPrice;
+        seatsLeft--;
+        console.log(seatsLeft);
+        console.log(priceCharged);
+    return;
+};  //
+if (seatsLeft = 9) {
+    surcharge = 0.05;
+    priceCharged = initialTicketPrice + (initialTicketPrice * surcharge);
+    seatsLeft--;
+    console.log(seatsLeft);
+    console.log(priceCharged);
+return;
+};
+
+while (seatsLeft <= 0) {
+    surcharge = surcharge * 1;
+    priceCharged = initialTicketPrice + (initialTicketPrice * surcharge);
+    seatsLeft--;
+    console.log(seatsLeft);
+    console.log(priceCharged);
+};
+/*output = "1 ticket booked. Please pay $" + priceCharged + ". Seats left: " +*/
+
+/*var nextPurchase = function(userKeyed) {
+        seatsLeft - 1;
+        priceCharged = initialTicketPrice + (initialTicketPrice * 0.05);
+        console.log(seatsLeft);
+        console.log(priceCharged);
 };
 
 
-//initial total seats in plane is 10
-//initial  ticket price is 50
-var initialTotalSeats = 10;
-var initialTicketPrice = 50;
-
-var nextPurchase = function(seatsLeft, priceCharged) {
-
-}
-
-//var userToPay = function (initialTotalSeats, initialTicketPrice) {
-//    if (initialTotalSeats = 10) {
-//        initialTicketPrice = 50;
-
-
-//    }
-//}
-//If total seats is 10, ticket price is $50
-//After user typed input, show ticket price as 50 AND minus total seats by 1
 //Total seats now 10 - 1, ticket price now $50 x (50 x 0.05)
 
 //When second user typed input, show ticket price as 50 x (50 x 0.05) AND minus total seats by 1
-//Total seats now 10 -1 -1, ticket price now 50 x (50 x (50 x (0.05 + 0.05))
+//Total seats now 10 -1 -1, ticket price now 50 x (50 x (50 x (0.05 + 0.05))*/
