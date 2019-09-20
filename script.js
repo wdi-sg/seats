a3ba9c694035956e19a31a882c578266030e11f6
norris.wong@hotmail.com <YOUR-EMAIL-ADDRESS>
2019-09-20 15:10:08 +0800

    Day 4 Seats HomeWork

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 console.log("hello script js");
 
+// var seatsLeft = 10;
+// var costOfSeat = 50;
+
+// var inputHappened = function(currentInput) {
+// 	if (currentInput == 1, seatsLeft-1) {
+		
+
+
+var costOfSeat = 50;
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
-};
+	if (currentInput == 1) {
+		var total = costOfSeat;
+	} else if (currentInput == 2) {
+		var total = (costOfSeat)*1.05;
+	} else if (currentInput == 3) {
+		var total = ((costOfSeat)*1.05)*1.05;
+	} else if (currentInput == 4) {
+        var total = (((costOfSeat)*1.05)*1.05)*1.05;
+	} else if (currentInput == 5) {
+		var total = ((((costOfSeat)*1.05)*1.05)*1.05)*1.05;
+	}  else if (currentInput == 6) {
+        var total = (((((costOfSeat)*1.05)*1.05)*1.05)*1.05)*1.05;
+	}  else if (currentInput == 4) {
+		var total = ((((((costOfSeat)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05;
+	}  else if (currentInput == 3) {
+		var total = (((((((costOfSeat)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05;
+	}  else if (currentInput == 2) {
+		var total = ((((((((costOfSeat)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05
+	}  else if (currentInput == 1) {
+		var total = (((((((((costOfSeat)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05;
+	}  else {
+		return "Sorry It's Sold Out!";
+	}
+	    return total.toFixed(2);
+	};
