317544895090c403b7b4ffceec7037ac9f4a82b6
DeeTrax <norris.wong@hotmail.com>
2019-09-21 16:26:28 +0800

    Merge branch 'master' of https://github.com/wdi-sg/seats

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var priceOfSeat = 50;
 var priceIncrement = 0.05;
 
 var inputHappened = function(currentInput) {
+	planeSeats = planeSeats - 1
 	priceOfSeat = priceOfSeat + (priceOfSeat * priceIncrement);
 	return priceOfSeat
 };
