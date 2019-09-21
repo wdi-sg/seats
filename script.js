8cc75958692b57ed287f6f7749cae94b56360cbf
DeeTrax <norris.wong@hotmail.com>
2019-09-21 16:27:38 +0800

    Add error message when all tickets sold out

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var priceOfSeat = 50;
 var priceIncrement = 0.05;
 
 var inputHappened = function(currentInput) {
+	if(planeSeats === 0) {
+		return "Sorry, all tickets have been sold out."
+	}
 	planeSeats = planeSeats - 1
 	priceOfSeat = priceOfSeat + (priceOfSeat * priceIncrement);
 	return priceOfSeat
