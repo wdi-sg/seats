628c3a4f6ab3c6f4f7ac32960b00d4da0d7e206e
norris.wong@hotmail.com <YOUR-EMAIL-ADDRESS>
2019-09-21 16:03:52 +0800

    add logic for incrementing ticket price by 5%

--- script.js
+++ script.js
@@ -1,38 +1,10 @@
 console.log("hello script js");
 
-// var seatsLeft = 10;
-// var costOfSeat = 50;
+var planeSeats = 10;
+var priceOfSeat = 50;
+var priceIncrement = 0.05;
 
-// var inputHappened = function(currentInput) {
-// 	if (currentInput == 1, seatsLeft-1) {
-		
-
-
-var costOfSeat = 50;
-
-var inputHappened = function(currentInput){
-	if (currentInput == 1) {
-		var total = costOfSeat;
-	} else if (currentInput == 2) {
-		var total = (costOfSeat)*1.05;
-	} else if (currentInput == 3) {
-		var total = ((costOfSeat)*1.05)*1.05;
-	} else if (currentInput == 4) {
-        var total = (((costOfSeat)*1.05)*1.05)*1.05;
-	} else if (currentInput == 5) {
-		var total = ((((costOfSeat)*1.05)*1.05)*1.05)*1.05;
-	}  else if (currentInput == 6) {
-        var total = (((((costOfSeat)*1.05)*1.05)*1.05)*1.05)*1.05;
-	}  else if (currentInput == 4) {
-		var total = ((((((costOfSeat)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05;
-	}  else if (currentInput == 3) {
-		var total = (((((((costOfSeat)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05;
-	}  else if (currentInput == 2) {
-		var total = ((((((((costOfSeat)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05
-	}  else if (currentInput == 1) {
-		var total = (((((((((costOfSeat)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05)*1.05;
-	}  else {
-		return "Sorry It's Sold Out!";
-	}
-	    return total.toFixed(2);
-	};
+var inputHappened = function(currentInput) {
+	priceOfSeat = priceOfSeat + (priceOfSeat * priceIncrement);
+	return priceOfSeat
+};
