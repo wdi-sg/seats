15590d25c72c1b9513e8f54cc900ce7f23dc1060
DeeTrax <norris.wong@hotmail.com>
2019-09-21 17:17:36 +0800

    add logic for calculating price based on number of tickets left

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ console.log("hello script js");
 
 var planeSeats = 10;
 var priceOfSeat = 50;
-var priceIncrement = 0.05;
+var lowerPriceIncrement = 0.03;
+var higherPriceIncrement = 0.05;
 
 var inputHappened = function(currentInput) {
 	if(planeSeats === 0) {
 		return "Sorry, all tickets have been sold out."
 	}
 	planeSeats = planeSeats - 1
-	priceOfSeat = priceOfSeat + (priceOfSeat * priceIncrement);
-	return `Current price of ticket is ${priceOfSeat.toFixed(2)} and the number of tickets left is ${planeSeats}`
+	priceOfSeat = calculateTicketPrice(planeSeats, priceOfSeat);
+	return `Current price of ticket is ${priceOfSeat.toFixed(2)} and 
+	the number of tickets left is ${planeSeats}.`
 };
+
+var calculateTicketPrice = function(numOfSeats, currentPriceOfSeat) {
+	if(numOfSeats > 4) {
+		return currentPriceOfSeat + (currentPriceOfSeat * lowerPriceIncrement)
+	}
+	if(numOfSeats > 0) {
+		return currentPriceOfSeat + (currentPriceOfSeat * higherPriceIncrement)
+	} else {
+		return 91000
+	}
+}
