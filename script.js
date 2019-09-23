c31378e15d0792023eb513ace96504be951628de
Sharon <sharxnrxse1312@gmail.com>
2019-09-23 14:03:46 +0800

    Seats Calculator

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 console.log("hello script js");
 
+var ticketPrice = 50;
+var seatsleft = 10;
+var priceCharged = ticketPrice + (5%);
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+    if (seatsleft == 10){
+        priceCharged = ticketPrice;
+        seatsleft = seatsleft - 1;
+        console.log(seatsleft);
+        console.log(priceCharged);
+
+        return (seatsleft + priceCharged);
+    }
+  return "WOW SOMETHING HAPPENED";
 };
+
+
