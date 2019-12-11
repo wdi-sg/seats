06743e7cfb022ce7936fe54858c472ca2b57c6cb
SamTanFL <samsite_tan@hotmail.com>
2019-12-11 19:52:00 +0800

    added seats till next bracket

--- script.js
+++ script.js
@@ -8,11 +8,17 @@ var filledSeats = 0; //Number of seats filled
 var currentSeatPrice; //Seat price at time of input
 var finalSeatPrice = 91000; //Seat of final seat
 var seatsLeft = 10; // Number of tickets before ticket being sold
+var seatsTillBracket; // Number of tickets before next bracket
+var bracketsMessage; // Output for brackets
 
 var calculateSeatsLeft = function() {
     console.log(seatsLeft);
     seatsLeft = 10 - filledSeats;
-    return seatsLeft;
+    if (filledSeats < 5) {
+        seatsTillBracket = seatsLeft - 6;
+    } else if (filledSeats < 10) {
+        seatsTillBracket = seatsLeft;
+    } return seatsLeft;
 }
 
 var calculateSeatPrice = function() {
@@ -46,5 +52,5 @@ var inputHappened = function(currentInput){
     return "Sorry but all seats are sold out."
   } else {
     filledSeats = filledSeats + 1;
-    return "There are " + seatsLeft + " seats left" + "\n" + currentSeatPrice; };
+    return "There are " + seatsLeft + " seats left" + "\n" + "$" + currentSeatPrice + "\n" + "There are " + seatsTillBracket + " seats till next bracket"};
 };
