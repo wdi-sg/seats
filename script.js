7f4d05b808df5fb74724092796b752c45d97375a
leowzhenkang <leowzk@hotmail.com>
2019-12-11 17:41:55 +0800

    Done requirement 1

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 console.log("hello script js");
 
+var numberSold = 0
+var seatPrice = 50
+//+2.5 every time a seat is sold
+
+var priceIncrease = 2.5
+
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  if(numberSold === 0 ){
+    numberSold++;
+//    seatPrice = seatPrice + priceIncrease;
+    return seatPrice;
+  }else if(numberSold < 10){
+    numberSold++
+    seatPrice = seatPrice + priceIncrease;
+    return seatPrice;
+  }else
+  return "sold out";
+
 };
