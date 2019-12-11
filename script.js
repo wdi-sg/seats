3d658e50608e877a0d361a2b34b938594daec261
weizheng1910 <weizheng1910@gmail.com>
2019-12-11 19:19:13 +0800

    step2

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ var inputHappened = function(currentInput){
   console.log( currentInput );
   var yourSeatPrice = seatPrice;
   seats--;
-  seatPrice = seatPrice * 1.05;
+  if(seats > 5){
+  	seatPrice = seatPrice * 1.03;
+  } else if (seats <= 5 && seats > 1){
+  	seatPrice = seatPrice * 1.05;
+  } else if (seats === 1){
+  	seatPrice = 91000;
+  }
   return "You bought a seat at "+yourSeatPrice+" dollars. There are "+seats+" seats left."
 };
