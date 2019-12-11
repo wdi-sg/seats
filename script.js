a2d7459876bff0df87dcc5946f7e3ef84a5a5e13
upieez <samuelhww@gmail.com>
2019-12-11 18:35:27 +0800

    add function to calculate plane tickets

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-console.log("hello script js");
+
+var planeSeats = 0;
+var price = 0;
+
+var seatPrice = function(){
+    if (planeSeats === 0) {
+        price = 50;
+        planeSeats += 1;
+    } else if (planeSeats > 0 && planeSeats <= 10){
+        price = price + (price * 0.05);
+        planeSeats += 1;
+    }
+};
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+    seatPrice();
+    if (planeSeats >= 10) {
+       return "Plane has reached it's max capacity!";
+    } else {
+     return "The cost of your ticket is $" + price.toFixed(2);
+    }
 };
