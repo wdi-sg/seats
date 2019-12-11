6275ed470a6618bb22a50bd82634324c4f5837eb
weizheng1910 <weizheng1910@gmail.com>
2019-12-11 20:12:04 +0800

    second last step

--- script.js
+++ script.js
@@ -1,36 +1,93 @@
 console.log("hello script js");
 
-var seats = 10;
-var seatPrice = 50;
+var economySeats = 15;
+var economySeatPrice = 50;
 var seatsBeforePriceIncrease; 
-var storeStartingPrice = seatPrice;
-var firstIncrease = seatPrice * 0.03;
-var secondIncrease = seatPrice * 0.05;
+var storeEconomyStartingPrice = economySeatPrice;
+var firstEconomyIncrease = storeEconomyStartingPrice * 0.03;
+var secondEconomyIncrease = storeEconomyStartingPrice * 0.05;
+
+var businessSeats = 6;
+var businessSeatPrice = 50;
+var storeBusinessStartingPrice = businessSeatPrice;
+var firstBusinessIncrease = storeBusinessStartingPrice * 0.06;
+var secondBusinessIncrease = storeBusinessStartingPrice * 0.10;
+
+var firstSeats = 4;
+var firstSeatPrice = 50;
+var storeFirstStartingPrice = firstSeatPrice;
+var firstPriceIncrease = storeFirstStartingPrice * 0.15;
 
 var inputHappened = function(currentInput){
-  if (isNaN(currentInput) == true) {
-  	return "invalid input, please type a number"
+  if (typeof(currentInput) == "Number") {
+  	return "invalid input, please type a string"
   } else {
 
-  	console.log( currentInput );
-  	var yourSeatPrice = seatPrice;
-  	seats--;
+  	if(currentInput == 'economy'){
 
-  	if(seats > 5){
-  		seatPrice = seatPrice + firstIncrease;
-  	} else if (seats <= 5 && seats > 1){
-  		seatPrice = seatPrice + secondIncrease;
-  	} else if (seats === 1){
-  		seatPrice = 91000;
+  	console.log( currentInput );
+  	var yourSeatPrice = economySeatPrice;
+  	economySeats--;
+
+  	if(economySeats > 7){
+  		economySeatPrice = economySeatPrice + firstEconomyIncrease;
+  	} else if (economySeats <= 7 && economySeats > 1){
+  		economySeatPrice = economySeatPrice + secondEconomyIncrease;
+  	} else if (economySeats === 1){
+  		economySeatPrice = 91000;
   	}
 
    
-  	if (seats > 5) { 
-  		seatsBeforePriceIncrease = Math.max(seats - 5,0);
+  	if (economySeats > 5) { 
+  		seatsBeforePriceIncrease = Math.max(economySeats - 5,0);
   	} else {
-  		seatsBeforePriceIncrease = Math.max(seats - 1,0);
+  		seatsBeforePriceIncrease = Math.max(economySeats - 1,0);
   	};
 
-  return "You bought a seat at "+yourSeatPrice+" dollars. There are "+seats+" seats left. Number of seats before price increase: "+seatsBeforePriceIncrease
+  return "You bought a seat at "+yourSeatPrice+" dollars. There are "+economySeats+" seats left. Number of seats before price increase: "+seatsBeforePriceIncrease
+	} else if (currentInput == 'business') {
+
+var yourSeatPrice = businessSeatPrice;
+  	businessSeats--;
+
+  	if(businessSeats > 3){
+  		businessSeatPrice = businessSeatPrice + firstBusinessIncrease;
+  	} else if (businessSeats <= 3 && businessSeats > 1){
+  		businessSeatPrice = businessSeatPrice + secondBusinessIncrease;
+  	} else if (businessSeats === 1){
+  		businessSeatPrice = 91000;
+  	}
+
+   
+  	if (businessSeats > 3) { 
+  		seatsBeforePriceIncrease = Math.max(businessSeats - 3,0);
+  	} else {
+  		seatsBeforePriceIncrease = Math.max(businessSeats - 1,0);
+  	};
+
+  return "You bought a seat at "+yourSeatPrice+" dollars. There are "+businessSeats+" seats left. Number of seats before price increase: "+seatsBeforePriceIncrease
+
+
+} else if (currentInput == 'firstClass'){
+
+	var yourSeatPrice = firstSeatPrice;
+  	firstSeats--;
+
+  	if(firstSeats > 1){
+  		firstSeatPrice = firstSeatPrice + firstPriceIncrease
+  	} else if (firstSeats === 1){
+  		firstSeatPrice = 91000;
+  	}
+
+   
+  	if (firstSeats > 1) { 
+  		seatsBeforePriceIncrease = Math.max(firstSeats - 1,0);
+  	} else {
+  		seatsBeforePriceIncrease = 0
+  	};
+
+  return "You bought a seat at "+yourSeatPrice+" dollars. There are "+firstSeats+" seats left. Number of seats before price increase: "+seatsBeforePriceIncrease
+
 }
-};
+}
+}
