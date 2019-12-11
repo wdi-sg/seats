7d1183660c7ce693a2b59da7f21aa219673b76af
rachellesg <rachelle.yeo@outlook.com>
2019-12-11 18:24:37 +0800

    updated script with part 1, each seat sold is 5% of base price

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 console.log("hello script js");
 
+var basePrice = 50;
+var planeSeats = 10;
+
+var message;
+var updatedPrice;
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+
+  if (planeSeats === 10) {
+    planeSeats = planeSeats - 1;
+    updatedPrice = basePrice;
+    console.log(planeSeats, basePrice);
+  } else if (planeSeats < 10) {
+    planeSeats = planeSeats - 1;
+    updatedPrice = basePrice * 1.05;
+    console.log(planeSeats, basePrice);
+  }
+    message = "SEAT SOLD! Your total price is: $" + updatedPrice + "\n" + "Seats left: " + planeSeats;
+    return message;
 };
