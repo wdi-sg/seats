061528b18cdbfa882eabe699ca4e1b8368ae2d70
eunicelok <eunicelp511@gmail.com>
2019-12-12 00:16:21 +0800

    Did part 1 but decrement not working

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 console.log("hello script js");
 
+// define which variables are needed
+var seatsLeft = 10;
+var actualPrice = 50;
+var latestPrice;
+
+//has a small error, decrement not working
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
-};
+    console.log( currentInput );
+    if (seatsLeft === 10) {
+        seatsLeft = seatsLeft - 1;
+        console.log(actualPrice);
+    }
+     else if (seatsLeft <10 && seatsLeft >0) {
+        seatsLeft = seatsLeft - 1;
+        latestPrice = actualPrice * 1.05;
+        console.log(latestPrice);
+     }
+
+    else {console.log("Sorry, tickets are sold out!");}
+}
