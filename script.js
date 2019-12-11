6b1eb7040fe0a8f922ab08db57cef3c89bf05d18
SamTanFL <samsite_tan@hotmail.com>
2019-12-11 17:41:56 +0800

    basic logic complete

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 console.log("hello script js");
 
+//basic logic is done
+
+var totalPlaneSeats = 10; //total number of seats in the plane
+var startingPrice = 50; //price of a seat when plane is empty
+var priceIncrement = 2.5; //Price increase per bought seat
+var filledSeats = 0; //Number of seats filled
+var currentSeatPrice; //Seat price at time of input
+
+
+
+var calculateSeatPrice = function() {
+    console.log(filledSeats);
+    currentSeatPrice = startingPrice + (filledSeats * priceIncrement)
+    return currentSeatPrice;
+};
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  calculateSeatPrice();
+  filledSeats = filledSeats + 1;
+/*  if (filledSeats = 10) {
+    return "Sorry, the plane is full. No more seats available."
+  } else {*/
+  return currentSeatPrice;
 };
