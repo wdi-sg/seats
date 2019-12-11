71150c99153a9e4391ce1cc37a8c7915080914ae
weizheng1910 <weizheng1910@gmail.com>
2019-12-11 19:13:18 +0800

    step1

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 console.log("hello script js");
 
+var seats = 10;
+var seatPrice = 50;
+
+
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  var yourSeatPrice = seatPrice;
+  seats--;
+  seatPrice = seatPrice * 1.05;
+  return "You bought a seat at "+yourSeatPrice+" dollars. There are "+seats+" seats left."
 };
