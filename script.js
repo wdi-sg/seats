be3b32f22f97a8cfebb685a7acd148a09fb49be5
Stuart Myers <stuart.myers@gmail.com>
2019-12-11 17:35:15 +0800

    Completed part 1

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-console.log("hello script js");
+console.log("Ready to sell seats");
+
+var planeSeats = 10;
+var seatsSold = 0;
+var percentageIncrement = 5;
+var seatPrice = 50;
+
+console.log((planeSeats - seatsSold) + " seats left.");
+
+var sellSeat = function(sellSeat) {
+    if (seatsSold === planeSeats) {
+        return "Sorry plane is sold out. :("
+    }
+
+    var seatAdjustedPrice = seatPrice + (seatPrice * (percentageIncrement / 100) * seatsSold);
+    seatsSold++;
+
+    console.log((planeSeats - seatsSold) + " seats left.");
+    return "Sold you a seat for " + seatAdjustedPrice;
+}
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+    return sellSeat();
 };
