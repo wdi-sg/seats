3949c87849c0e0656701bbe27e5b7664584017e8
rohai <rohai.sul@gmail.com>
2019-12-11 18:55:39 +0800

    1st part done - 5% price increment

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 console.log("hello script js");
 
+
+var totalSeats = 10;
+var baseSeatPrice = 50;
+var seatsFilled = 0;
+var currentSeatPrice;
+var priceIncrease = 0.05*50;
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+
+    if (seatsFilled >= 10){
+        return "Tickets Sold Out - The plane is full.";
+    } else {
+
+    currentSeatPrice = baseSeatPrice + (seatsFilled * priceIncrease);
+    seatsFilled = seatsFilled + 1;
+    console.log( currentInput );
+    return "Please pay $" + currentSeatPrice + "!";
+}
+
+
 };
