d3ac423ddc1c4d3c56f38f052dd401d0c9bb74cd
Angel Ferreros <AngelFerreros@tjs-macbook-pro.lan>
2019-12-12 01:43:19 +0800

    revised js

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-console.log("hello script js");
+//A plane has 10 seats.
+var planeSeats = 10;
+var seatsSold = 1;
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
-};
+var newPrice = 50;
+
+//starting value
+var inputEntry = false;
+console.log("Has input: "+inputEntry);
+var data = null;
+
+var inputHappened = function(userInput) {
+
+    if ( userInput === false )
+    {
+ data = inputEntry;
+    userInput = true;
+    console.log(userInput);
+
+        return "The seat is $"+newPrice;
+        seatsSold ++;
+        console.log(seatsSold);
+    }
+    while ((seatsSold <= 10) && (userInput = true))
+    {
+        if (seatsSold != 1) {
+        var newPrice = newPrice*1.05;
+        }
+        return "The seat is $"+newPrice;
+        console.log(newPrice);
+        seatsSold ++;
+        console.log(seatsSold);
+        userInput=false;
+    }
+}
