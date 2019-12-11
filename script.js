227517b4eeca79dcf473ffe72822a00c103817a2
leowzhenkang <leowzk@hotmail.com>
2019-12-11 19:16:14 +0800

    part 4 done

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var numberSold = 10
 var seatPrice = 50
 //+2.5 every time a seat is sold
 
-var firstHalfPriceIncrease = 1.5
-var secondHalfPriceIncrease = 2.5
+var firstHalfPriceIncrease = 1.03
+var secondHalfPriceIncrease = 1.05
 var beforeFirstIncrease = 5
 var beforeSecondIncrease = 4
 
@@ -20,19 +20,19 @@ var inputHappened = function(currentInput){
   }else if(numberSold >= 6){
     numberSold--;
     beforeFirstIncrease--;
-    seatPrice = seatPrice + firstHalfPriceIncrease;
-    return "Price: " + seatPrice + "\n" + "Seats left: " + numberSold + "\n" + "Seats before price increase: " + beforeFirstIncrease;
+    seatPrice = seatPrice * firstHalfPriceIncrease;
+    return "Price: " + Math.round(seatPrice) + "\n" + "Seats left: " + numberSold + "\n" + "Seats before price increase: " + beforeFirstIncrease;
   }else if(numberSold >=2){
     numberSold--;
     beforeSecondIncrease--;
-    seatPrice = seatPrice + secondHalfPriceIncrease;
-    return "Price: " + seatPrice + "\n" + "Seats left: " + numberSold + "\n" + "Seats before price increase: " + beforeSecondIncrease;
+    seatPrice = seatPrice * secondHalfPriceIncrease;
+    return "Price: " + Math.round(seatPrice) + "\n" + "Seats left: " + numberSold + "\n" + "Seats before price increase: " + beforeSecondIncrease;
   }else if(numberSold === 1){
     numberSold--;
     beforeSecondIncrease--;
     seatPrice = 91000
     return "Price: " + seatPrice + "\n" + "Seats left: " + numberSold;
   }else
-  return "sold out";
+  return "Sold Out";
 
 };
