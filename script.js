cb29cf6edcdff5c72e547fc0ffe66320c9c8c90d
rachellesg <rachelle.yeo@outlook.com>
2019-12-11 19:17:15 +0800

    added message to tell user how many tickets are left before next pricing tier

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var planeSeats = 10;
 var message;
 var updatedPrice;
 
+var toSecondTier = 4;
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
 
@@ -14,21 +16,26 @@ var inputHappened = function(currentInput){
         updatedPrice = basePrice;
         planeSeats--;
         console.log(updatedPrice,planeSeats);
+        message = "SEAT SOLD! Your total price is: $" + updatedPrice + "\n" + "Seats left: " + planeSeats;
       } else if (planeSeats >= 6) {
-        updatedPrice = basePrice * 1.03;
+        updatedPrice = updatedPrice * 1.03;
         planeSeats--;
+        toSecondTier--;
         console.log(planeSeats, updatedPrice);
+        message = "SEAT SOLD! Your total price is: $" + Math.round(updatedPrice * 100) / 100 + "\n" + "Seats left: " + planeSeats + "\n" + "Seats before price increase: " + toSecondTier;
       } else if (planeSeats >= 2) {
-        updatedPrice = basePrice * 1.05;
+        updatedPrice = updatedPrice * 1.05;
         planeSeats--;
         console.log(planeSeats, updatedPrice);
+        message = "SEAT SOLD! Your total price is: $" + Math.round(updatedPrice * 100) / 100 + "\n" + "Seats left: " + planeSeats;
       } else if (planeSeats >= 1) {
-        updatedPrice = 500;
+        updatedPrice = 91000;
         planeSeats--;
         console.log(updatedPrice);
+        message = "SEAT SOLD! Your total price is: $" + Math.round(updatedPrice * 100) / 100 + "\n" + "Seats left: " + planeSeats;
       } else {
         planeSeats = 0;
+        message = "SOLD OUT! Better luck next time!";
       }
-        message = "SEAT SOLD! Your total price is: $" + updatedPrice + "\n" + "Seats left: " + planeSeats;
         return message;
 };
