afb551ada8007d69b61227a9c54e6ba634713cef
SamTanFL <samsite_tan@hotmail.com>
2019-12-11 19:12:59 +0800

    finished compounding increment

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 console.log("hello script js");
 
-//finished 2nd part
+//finished compound increment
 
 var totalPlaneSeats = 10; //total number of seats in the plane
 var startingPrice = 50; //price of a seat when plane is empty
-var halfFullPrice = 56 // price of a seat when plane is half full
-var priceIncrement1 = 1.5; //Price increase per seat first half
-var priceIncrement2 = 2.5; //Price increase per seat second half
 var filledSeats = 0; //Number of seats filled
 var currentSeatPrice; //Seat price at time of input
 var finalSeatPrice = 91000; //Seat of final seat
@@ -19,20 +16,26 @@ var calculateSeatsLeft = function() {
 }
 
 var calculateSeatPrice = function() {
-    console.log(filledSeats);
-    if (filledSeats < 5) {
-    currentSeatPrice = startingPrice + (filledSeats * priceIncrement1);
-    console.log(currentSeatPrice);
-} else if (filledSeats < 9) {
-    currentSeatPrice = halfFullPrice + ((filledSeats - 4) * priceIncrement2);
-    console.log(currentSeatPrice);
-    return "There are " + seatsLeft + " seats left" + "\n" + currentSeatPrice;
-} else {
-    currentSeatPrice = finalSeatPrice;
-    console.log(currentSeatPrice);
-    return "There are " + seatsLeft + " seats left" + "\n" + currentSeatPrice;
+    console.log("ello");
+    if (filledSeats === 0) {
+        currentSeatPrice = startingPrice;
+        console.log("ello there");
+        return currentSeatPrice;
+}   else if (filledSeats < 5) {
+        currentSeatPrice = currentSeatPrice * 1.03;
+        console.log(currentSeatPrice);
+        return currentSeatPrice;
+}   else if (filledSeats < 9) {
+        currentSeatPrice = currentSeatPrice * 1.05;
+        console.log(currentSeatPrice);
+        return currentSeatPrice;
+}   else {
+        currentSeatPrice = finalSeatPrice;
+        console.log(currentSeatPrice);
+        return currentSeatPrice;
 }
 };
+
 
 var inputHappened = function(currentInput){
   console.log( filledSeats );
