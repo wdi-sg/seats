3d6c6de3308760f3da347d1e3c92f60f09176256
upieez <samuelhww@gmail.com>
2019-12-11 19:25:22 +0800

    fix last pricing error

--- script.js
+++ script.js
@@ -6,20 +6,21 @@ var seatPrice = function(){
     if (planeSeats === 0) {
         price = 50;
         planeSeats += 1;
-    } else if (planeSeats >= 1 && planeSeats <= 5) {
+    } else if (planeSeats > 0 && planeSeats <= 4){
         price = price + (price * 0.03);
         planeSeats += 1;
-    } else if (planeSeats >= 6 && planeSeats <= 8){
+    } else if (planeSeats >= 5 && planeSeats <= 8){
         price = price + (price * 0.05);
         planeSeats += 1;
-    } else if (planeSeats === 9) {
+    } else if (planeSeats === 9 || planeSeats <= 10){
         price = 91000;
+        planeSeats += 1;
     }
 };
 
 var inputHappened = function(currentInput){
     seatPrice();
-    if (planeSeats >= 10) {
+    if (planeSeats > 10) {
        return "Plane has reached it's max capacity!";
     } else {
      return "The cost of your ticket is $" + price.toFixed(2);
