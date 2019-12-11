f61a3d2f772c4c83e1e7ffc6a304d5c8acda7aa9
leowzhenkang <leowzk@hotmail.com>
2019-12-11 18:53:40 +0800

    part 3 done

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 console.log("hello script js");
 
-var numberSold = 1
+var numberSold = 10
 var seatPrice = 50
 //+2.5 every time a seat is sold
 
 var firstHalfPriceIncrease = 1.5
 var secondHalfPriceIncrease = 2.5
+var beforeFirstIncrease = 5
+var beforeSecondIncrease = 4
+
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  if(numberSold === 1 ){
-    numberSold++;
+  if(numberSold === 10 ){
+    numberSold--;
+    beforeFirstIncrease--
 //    seatPrice = seatPrice + priceIncrease;
-    return "Price: " + seatPrice + "\n" + "Seats left: " + numberSold;
-  }else if(numberSold <= 5){
-    numberSold++
+    return "Price: " + seatPrice + "\n" + "Seats left: " + numberSold + "\n" + "Seats before price increase: " + beforeFirstIncrease;
+  }else if(numberSold >= 6){
+    numberSold--;
+    beforeFirstIncrease--;
     seatPrice = seatPrice + firstHalfPriceIncrease;
-    return "Price: " + seatPrice + "\n" + "Seats left: " + numberSold;
-  }else if(numberSold <= 9){
-    numberSold++
+    return "Price: " + seatPrice + "\n" + "Seats left: " + numberSold + "\n" + "Seats before price increase: " + beforeFirstIncrease;
+  }else if(numberSold >=2){
+    numberSold--;
+    beforeSecondIncrease--;
     seatPrice = seatPrice + secondHalfPriceIncrease;
-    return "Price: " + seatPrice + "\n" + "Seats left: " + numberSold;
-  }else if(numberSold === 10){
-    numberSold++;
+    return "Price: " + seatPrice + "\n" + "Seats left: " + numberSold + "\n" + "Seats before price increase: " + beforeSecondIncrease;
+  }else if(numberSold === 1){
+    numberSold--;
+    beforeSecondIncrease--;
     seatPrice = 91000
     return "Price: " + seatPrice + "\n" + "Seats left: " + numberSold;
   }else
