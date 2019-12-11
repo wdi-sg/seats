62482536dcc3464c0cb92938211189e510b859c8
weizheng1910 <weizheng1910@gmail.com>
2019-12-11 19:36:46 +0800

    step5 - added checks

--- script.js
+++ script.js
@@ -8,24 +8,29 @@ var firstIncrease = seatPrice * 0.03;
 var secondIncrease = seatPrice * 0.05;
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var yourSeatPrice = seatPrice;
-  seats--;
+  if (isNaN(currentInput) == true) {
+  	return "invalid input, please type a number"
+  } else {
 
-  if(seats > 5){
-  	seatPrice = seatPrice + firstIncrease;
-  } else if (seats <= 5 && seats > 1){
-  	seatPrice = seatPrice + secondIncrease;
-  } else if (seats === 1){
-  	seatPrice = 91000;
-  }
+  	console.log( currentInput );
+  	var yourSeatPrice = seatPrice;
+  	seats--;
+
+  	if(seats > 5){
+  		seatPrice = seatPrice + firstIncrease;
+  	} else if (seats <= 5 && seats > 1){
+  		seatPrice = seatPrice + secondIncrease;
+  	} else if (seats === 1){
+  		seatPrice = 91000;
+  	}
 
    
-  if (seats > 5) { 
-  	seatsBeforePriceIncrease = Math.max(seats - 5,0);
-  } else {
-  	seatsBeforePriceIncrease = Math.max(seats - 1,0);
-  };
+  	if (seats > 5) { 
+  		seatsBeforePriceIncrease = Math.max(seats - 5,0);
+  	} else {
+  		seatsBeforePriceIncrease = Math.max(seats - 1,0);
+  	};
 
   return "You bought a seat at "+yourSeatPrice+" dollars. There are "+seats+" seats left. Number of seats before price increase: "+seatsBeforePriceIncrease
+}
 };
