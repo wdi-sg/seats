b2dcbb77ddcf89c45126a2df41308eb7e0beebb3
Stuart Myers <stuart.myers@gmail.com>
2019-12-11 18:50:13 +0800

    Completed Part 6

--- script.js
+++ script.js
@@ -1,49 +1,94 @@
 console.log("Ready to sell seats");
 
-var planeSeats = 10;
-var seatsSold = 0;
 var percentageIncrementFirst = 3;
 var percentageIncrementSecond = 5;
 var seatPrice = 50;
 var currentPrice = seatPrice;
 
-console.log((planeSeats - seatsSold) + " seats left.");
+var plane = {
+    firstClass: {
+        planeSeats: 4,
+        seatsSold: 0,
+        percentageIncrementFirst: 15,
+        percentageIncrementSecond: 15,
+        seatPrice: 50,
+        currentPrice: seatPrice,
+        finalSeatPrice: 191000,
+    },
+    businessClass: {
+        planeSeats: 6,
+        seatsSold: 0,
+        percentageIncrementFirst: 6,
+        percentageIncrementSecond: 10,
+        seatPrice: 50,
+        currentPrice: seatPrice,
+        finalSeatPrice: 91000,
+    },
+    economy: {
+        planeSeats: 10,
+        seatsSold: 0,
+        percentageIncrementFirst: 3,
+        percentageIncrementSecond: 5,
+        seatPrice: 50,
+        currentPrice: seatPrice,
+        finalSeatPrice: 91000,
+    }
+}
 
-var sellSeat = function(sellSeat) {
+// console.log((planeSeats - seatsSold) + " seats left.");
+
+var sellSeat = function(flightClass) {
+
+    var seatsSold = plane[flightClass].seatsSold;
+    var planeSeats = plane[flightClass].planeSeats;
+
     if (seatsSold === planeSeats) {
         return "Sorry plane is sold out. :("
     }
-    var percentageIncrement = percentageIncrementFirst;
+    var percentageIncrement = plane[flightClass].percentageIncrementFirst;
     var incrementMessage = ""
     // use if-else statements to determine how expensive the seat should be.
     if (planeSeats - seatsSold === 1) {
         console.log("Sold the final seat!");
-        seatsSold++;
-        return "Sold you the final seat for $91000";
-    } else if ( (planeSeats - seatsSold) <= (planeSeats / 2)) { // less than half the seats left
-        percentageIncrement = percentageIncrementSecond;
+        plane[flightClass].seatsSold++;
+        return "Sold you the final seat for $" + plane[flightClass].finalSeatPrice;
+    } else if (((planeSeats - seatsSold) <= (planeSeats / 2)) && (flightClass !== "firstClass")) { // less than half the seats left
+        percentageIncrement = plane[flightClass].percentageIncrementSecond;
         incrementMessage = (planeSeats - seatsSold - 2) + " more seats available until price increase.";
-        console.log("5% increase");
+        console.log(percentageIncrement + "% increase");
     } else {
         incrementMessage = ((Math.floor(planeSeats / 2)) - seatsSold - 1) + " more seats available until price increase.";
-        percentageIncrement = percentageIncrementFirst;
-        console.log("3% increase");
+        percentageIncrement = plane[flightClass].percentageIncrementFirst;
+        console.log(percentageIncrement + "% increase");
     }
 
-    var seatAdjustedPrice = currentPrice;
-    currentPrice *= 1 + (percentageIncrement / 100);
-    seatsSold++;
+    if (flightClass === "firstClass") {
+        incrementMessage = (planeSeats - seatsSold - 2) + " more seats available until price increase.";
+    }
 
-    console.log((planeSeats - seatsSold) + " seats left.");
-    return "Sold you a seat for $" + seatAdjustedPrice + ". " + incrementMessage;
+    var seatAdjustedPrice = plane[flightClass].currentPrice;
+    plane[flightClass].seatsSold++;
+    plane[flightClass].currentPrice += (seatPrice * percentageIncrement * 0.01);
+
+    console.log((planeSeats - plane[flightClass].seatsSold) + " seats left in " + flightClass);
+    return "Sold you a seat in " + flightClass + " for $" + seatAdjustedPrice + ". " + incrementMessage;
 }
 
 var inputHappened = function(currentInput){
 
-    if (currentInput.toLowerCase() === "buyseat") {
-        return sellSeat();
+    var formattedInput = currentInput.toLowerCase().trim();
+
+    if (formattedInput === "buy economy") {
+        console.log("Sell economy seat");
+        return sellSeat("economy");
+    } else if (formattedInput === "buy business") {
+        console.log("Sell Business Class Seat");
+        return sellSeat("businessClass");
+    } else if (formattedInput === "buy first class") {
+        console.log("Sell First Class Seat");
+        return sellSeat("firstClass");
     } else {
-        return "Please type in 'BuySeat' to buy a seat";
+        return "Please type in (without quote marks): \n 'Buy Economy' to buy an economy seat. \n 'Buy Business' to buy a business class seat. \n 'Buy First Class' to buy a first class seat.";
     }
 
 };
