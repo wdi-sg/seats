f4800769aa4478b193bb227668f025277c519fa1
Robert Kolsek <kolsek.income@gmail.com>
2019-12-11 23:13:28 +0800

    task three done

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ let maxSeats = 10
 let ticketPrice = 50
 
 let ticketPriceCalculation = function(seatsSold) {
-  if (seatsSold > 0 && seatsSold < 5 && seatsSold < 9) {
+  if (seatsSold > 1 && seatsSold < 6 && seatsSold < 10) {
     ticketPrice = ticketPrice/100*103
-  } else if (seatsSold >= 5 && seatsSold < 9){
+  } else if (seatsSold >= 6 && seatsSold < 10){
     ticketPrice = ticketPrice/100*105
-  } else if (seatsSold === 9){
+  } else if (seatsSold === 10){
     ticketPrice = 91000
-  } else if (seatsSold >= 10) {
+  } else if (seatsSold >= 11) {
     return "No more seats available"
   }
-    return `Your seat costs $${ticketPrice}`
+    return `Your seat costs $${ticketPrice} - ${10 - seatsSold} tickets left`
 }
 
 var inputHappened = function(currentInput){
-  let ticketPrice = ticketPriceCalculation(seatsSold)
   seatsSold++
+  let ticketPrice = ticketPriceCalculation(seatsSold)
   return ticketPrice
 };
