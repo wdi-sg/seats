016b183774a944b9ae6a14e121605981e77f45db
Stuart Myers <stuart.myers@gmail.com>
2019-12-11 18:01:43 +0800

    Completed Part 4

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var seatsSold = 0;
 var percentageIncrementFirst = 3;
 var percentageIncrementSecond = 5;
 var seatPrice = 50;
+var currentPrice = seatPrice;
 
 console.log((planeSeats - seatsSold) + " seats left.");
 
@@ -22,11 +23,15 @@ var sellSeat = function(sellSeat) {
     } else if ( (planeSeats - seatsSold) <= (planeSeats / 2)) { // less than half the seats left
         percentageIncrement = percentageIncrementSecond;
         incrementMessage = (planeSeats - seatsSold - 2) + " more seats available until price increase.";
+        console.log("5% increase");
     } else {
         incrementMessage = ((Math.floor(planeSeats / 2)) - seatsSold - 1) + " more seats available until price increase.";
+        percentageIncrement = percentageIncrementFirst;
+        console.log("3% increase");
     }
 
-    var seatAdjustedPrice = seatPrice + (seatPrice * (percentageIncrement / 100) * seatsSold);
+    var seatAdjustedPrice = currentPrice;
+    currentPrice *= 1 + (percentageIncrement / 100);
     seatsSold++;
 
     console.log((planeSeats - seatsSold) + " seats left.");
