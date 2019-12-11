62f05156bca59aad84c5293d742df41c2f3df96f
Robert Kolsek <kolsek.income@gmail.com>
2019-12-11 23:34:03 +0800

    fourth step done

--- script.js
+++ script.js
@@ -1,22 +1,64 @@
-let seatsSold = 0;
-let maxSeats = 10
-let ticketPrice = 50
+let seatsSoldEconomy = 0;
+let seatsSoldBusiness = 0;
+let seatsSoldFirst = 0;
+let ticketPriceEconomy = 50
+let ticketPriceBusiness = 50
+let ticketPriceFirst = 50
 
-let ticketPriceCalculation = function(seatsSold) {
-  if (seatsSold > 1 && seatsSold < 6 && seatsSold < 10) {
-    ticketPrice = ticketPrice/100*103
-  } else if (seatsSold >= 6 && seatsSold < 10){
-    ticketPrice = ticketPrice/100*105
-  } else if (seatsSold === 10){
-    ticketPrice = 91000
-  } else if (seatsSold >= 11) {
+let EconomyTicketPriceCalculation = function(seatsSoldEconomy) {
+  if (seatsSoldEconomy > 1 && seatsSoldEconomy < 8 && seatsSoldEconomy < 15) {
+    ticketPriceEconomy = ticketPriceEconomy/100*103
+  } else if (seatsSoldEconomy >= 8 && seatsSoldEconomy < 15){
+    ticketPriceEconomy = ticketPriceEconomy/100*105
+  } else if (seatsSoldEconomy === 15){
+    ticketPriceEconomy = 91000
+  } else if (seatsSoldEconomy >= 16) {
     return "No more seats available"
   }
-    return `Your seat costs $${ticketPrice} - ${10 - seatsSold} tickets left`
+    return `Your seat costs $${Math.round(ticketPriceEconomy)} - ${15 - seatsSoldEconomy} tickets left`
+}
+
+let BusinessTicketPriceCalculation = function(seatsSoldBusiness) {
+  if (seatsSoldBusiness > 1 && seatsSoldBusiness < 3 && seatsSoldBusiness < 6) {
+    ticketPriceBusiness = ticketPriceBusiness/100*106
+  } else if (seatsSoldBusiness >= 3 && seatsSoldBusiness < 6){
+    ticketPriceBusiness = ticketPriceBusiness/100*110
+  } else if (seatsSoldBusiness === 6){
+    ticketPriceBusiness = 91000
+  } else if (seatsSoldBusiness >= 7) {
+    return "No more seats available"
+  }
+    return `Your seat costs $${Math.round(ticketPriceBusiness)} - ${6 - seatsSoldBusiness} tickets left`
+}
+
+let FirstTicketPriceCalculation = function(seatsSoldFirst) {
+  if (seatsSoldFirst > 1 && seatsSoldFirst < 2 && seatsSoldFirst < 4) {
+    ticketPriceFirst = ticketPriceFirst/100*115
+  } else if (seatsSoldFirst >= 2 && seatsSoldFirst < 4){
+    ticketPriceFirst = ticketPriceFirst/100*115
+  } else if (seatsSoldFirst === 4){
+    ticketPriceFirst = 191000
+  } else if (seatsSoldFirst >= 5) {
+    return "No more seats available"
+  }
+    return `Your seat costs $${Math.round(ticketPriceFirst)} - ${4 - seatsSoldFirst} tickets left`
 }
 
 var inputHappened = function(currentInput){
-  seatsSold++
-  let ticketPrice = ticketPriceCalculation(seatsSold)
-  return ticketPrice
+  if (currentInput === "buy economy class") {
+    seatsSoldEconomy++
+    let ticketPrice = EconomyTicketPriceCalculation(seatsSoldEconomy)
+    document.querySelector('#input').value = ""
+    return ticketPrice
+  } else if (currentInput === "buy business class") {
+    seatsSoldBusiness++
+    let ticketPrice = BusinessTicketPriceCalculation(seatsSoldBusiness)
+    document.querySelector('#input').value = ""
+    return ticketPrice
+  } else if (currentInput === "buy first class") {
+    seatsSoldFirst++
+    let ticketPrice = FirstTicketPriceCalculation(seatsSoldFirst)
+    document.querySelector('#input').value = ""
+    return ticketPrice
+  }
 };
