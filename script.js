f30ff7a146eb1869e20bdf95ef0e4a8ff058c801
elisaes <alisa260785@gmail.com>
2020-03-18 20:01:44 +0800

    Elisa: first part of seats done

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-console.log("hello script js");
-
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
-};
+let seats = 10;
+let seatCost = 50;
+let currentInput;
+document.getElementById("output").innerHTML =
+  "There are 10 sits availables. The cost of the ticket is 50 dolars";
+function inputHappened(currentInput) {
+  if (currentInput !== "") {
+    if (seats !== 1) {
+      document.getElementById("input").value = "";
+      seats = seats - 1;
+      seatCost = seatCost * 0.05 + seatCost;
+      console.log(seatCost);
+      return (
+        "there are " +
+        " " +
+        seats +
+        " " +
+        "seats availables. The cost of the ticket is: " +
+        seatCost +
+        " " +
+        "dolars"
+      );
+    } else {
+      return "Sorry, no more available sits";
+    }
+  }
+}
