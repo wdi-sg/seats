5b67cc068530afefed234ef5881d2440dda20100
osbornechan <osbornecab@gmail.com>
2020-03-18 20:27:31 +0800

    First commit - Completed Part 1

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-console.log("hello script js");
+var originalPrice = 50;
+var numSeats = 10;
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+    console.log(numSeats)
+    if (numSeats === 10) {
+        numSeats = numSeats - 1;
+        return output = `Price of first ticket is $${originalPrice}`;
+    }
+
+    if (numSeats === 0) {
+        return 'Sold out!'
+    } else if (numSeats < 10) {
+        numSeats = numSeats - 1;
+        console.log(numSeats)
+        var newPrice = originalPrice * 1.05;
+        return output = `Price of subsequent ticket is $${newPrice}`;
+    }
 };
