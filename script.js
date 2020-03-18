01aece22c4e887449bd27df1e7f6e2c0d7b33179
Dyana Wu <dyanawu@gmail.com>
2020-03-18 22:26:19 +0800

    Complete basic requirement
    - sell seats until sold out, then rudely turn away

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-console.log("hello script js");
+var seatCount = 10;
+var seatsSold = 0;
+var originalPrice = 50;
+var nextPrice = originalPrice;
+var currentPrice;
+
+var surchargeRate = 0.05;
+var surcharge = originalPrice * surchargeRate;
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  if (seatsSold < seatCount) {
+    seatsSold++;
+    currentPrice = nextPrice;
+    nextPrice += surcharge;
+    console.log(seatsSold, currentPrice, nextPrice);
+
+    clearInput();
+    return `Sold! Your seat cost $${currentPrice}.`;
+  }
+
+  clearInput();
+  return "Sold out. Try the next flight.";
 };
+
+var clearInput = function () {
+  document.getElementById("input").value = "";
+}
