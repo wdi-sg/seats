794e03172a642fd0038c01245a4633f89c66a8bf
dannst <arcanic@gmail.com>
2020-03-18 23:27:25 +0800

    first have add 3 percent, second half add 5 percent, last seat is $91000.

--- script.js
+++ script.js
@@ -15,19 +15,29 @@ let price = 50;
 
 
 var inputHappened = function(currentInput){
-  console.log('User typed ${currentInput}');
   if (currentInput && (seats === 10)){
-  	seats -= 1;
-  	console.log('seats remaining: ' + seats);
-  	price = 50;
-  } else if (currentInput && (seats > 0)){
-  	seats -= 1;
-  	console.log('seats remaining: ' + seats);
+	  	seats -= 1;
+	  	console.log('seats remaining: ' + seats);
+	  	console.log('no markup');
+	  	price = 50;
+  } else if (currentInput && (seats > 5)){
+	  	seats -= 1;
+	  	console.log('seats remaining: ' + seats);
+	  	console.log('markup of three percent');
+  		price = addThreePercent(price);
+  } else if (currentInput && (seats > 1)){
+  		seats -= 1;
+  		console.log('seats remaining: ' + seats);
+  		console.log('markup of five percent');
   		price = addFivePercent(price);
+  } else if (seats === 1){
+  		seats -= 1;
+  		console.log('seats remaining: ' + seats);
+  		price = 91000;
   } else if (seats === 0){
-  	return 'We have sold out!';
+  		return 'We have sold out';
   } else {
-  	return 'I don\'t understand you';
+  		return 'I don\'t understand you';
   };
   return 'No. ' + (10-seats) + ' Seat sold at ' + '$' + price;
 };
