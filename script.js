5aa80a582c5b5fb3fb1d58c74e63ef5a7baf5fe1
travisenquiry <travisenquiry@gmail.com>
2020-03-18 22:02:38 +0800

    completed further 3

--- script.js
+++ script.js
@@ -2,21 +2,23 @@ console.log("hello script js");
 var seatCounter = 0;
 var seat = 10;
 var normalPrice = 50;
-var price;
+var price = 50;
 var secondSeatCounter = 0;
 
 var inputHappened = function(currentInput){
   seatCounter = seatCounter + 1;
-    if (seatCounter < 6){
-        price = normalPrice + (normalPrice / 100 * (3 * seatCounter));
-
-        return `Price: ${price}, ${seat - seatCounter} seats left.`;
+    if (seatCounter === 1){
+        price = price + (normalPrice / 100 * (3 * seatCounter));
+        return `Price: $${price}, ${seat - seatCounter} seats left.`;
+    }else if(seatCounter < 6){
+        price = price + (price / 100 * (3 * seatCounter));
+        return `Price: $${price}, ${seat - seatCounter} seats left.`;
     }else if(seatCounter > 5 && seatCounter < 10){
-        price = price + 2.5;
-        return `Price: ${price}, ${seat - seatCounter} seats left.`;
+        price = price + (price / 100 * (5 * seatCounter));
+        return `Price: $${price}, ${seat - seatCounter} seats left.`;
     }else if(seatCounter === 10){
         price = 91000;
-        return `Price: ${price}, ${seat - seatCounter} seat left.`;
+        return `Price: $${price}, ${seat - seatCounter} seat left.`;
     }else {
       return `Seat sold out.`;
     }
