69cd5b427b0338954669af9e1aa929d67d0ce39a
osbornechan <osbornecab@gmail.com>
2020-03-18 20:42:44 +0800

    Completed Part 2 (finer-grained pricing)

--- script.js
+++ script.js
@@ -2,18 +2,26 @@ var originalPrice = 50;
 var numSeats = 10;
 
 var inputHappened = function(currentInput){
-    console.log(numSeats)
+    var newPrice;
+
     if (numSeats === 10) {
         numSeats = numSeats - 1;
-        return output = `Price of first ticket is $${originalPrice}`;
+        return output = `Price of your ticket is $${originalPrice}`;
     }
 
     if (numSeats === 0) {
         return 'Sold out!'
-    } else if (numSeats < 10) {
+    } else if (numSeats <= 9 && numSeats > 5) {
         numSeats = numSeats - 1;
-        console.log(numSeats)
-        var newPrice = originalPrice * 1.05;
-        return output = `Price of subsequent ticket is $${newPrice}`;
+        var newPrice = originalPrice * 1.03;
+        return output = `Price of your ticket is $${newPrice}`;
+    } else if (numSeats <= 5 && numSeats > 1) {
+        numSeats = numSeats - 1;
+        newPrice = originalPrice * 1.05;
+        return output = `Price of your ticket is $${newPrice}`;
+    } else if (numSeats === 1) {
+        numSeats = numSeats - 1;
+        newPrice = 91000;
+        return output = `Price of your ticket is $${newPrice}`;
     }
 };
