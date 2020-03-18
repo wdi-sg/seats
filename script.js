eb168d24b3632ef930ddd3b728cc989113087135
lekhweemeng <lek.hweemeng@live.com>
2020-03-18 22:14:09 +0800

    corrected the calculation error. final copy of part 1.

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-
 //below are the codes for part 1
     //declaration of values
-    var seatSold = 0;
+    var seatSold = 1;
     var originalSeatPrice = 50;
 //actual functions runnin
     var inputHappened = function(currentInput){
         while (seatSold<11){
-    var incrementPerSeatSold = ((parseInt(seatSold + 1) * 0.05)*50);
+    var incrementPerSeatSold = ((parseInt(seatSold) * 0.05)*50);
     console.log("Number of seats sold: "+seatSold);
     console.log("increase in price:" + incrementPerSeatSold);
-    var currentSeatPrice = (originalSeatPrice + incrementPerSeatSold);
-    console.log ("Current seat price: " + currentSeatPrice);
+    var currentSeatPrice = ((((parseInt(seatSold)-1) * 0.05)*50)+50);
+    var nextSeatPrice = (originalSeatPrice + incrementPerSeatSold);
+    console.log ("current seat sold at: " + currentSeatPrice);
+    console.log ("next seat price: " + nextSeatPrice);
     seatSold = (seatSold + 1);
     var output = "Sold!";
   return output;
