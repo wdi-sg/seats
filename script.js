cae8e5032ca68ffc937284bf4e89eed4bb8e6531
mdazharaw <darkaz62@hotmail.com>
2020-03-18 20:58:23 +0800

    Part 2 done.

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 console.log("hello script js");
-//Part 1
+//Part 2
 var seatsRemaining = 10;
 var seatsSold = 0;
 var standardPrice = 50;
-var priceIncrement = 50 *(5/100);
+var priceIncrement1 = 50 *(3/100);
+var priceIncrement2 = 50 *(5/100);
 var currentPrice;
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  if(seatsRemaining>0){
+  if(seatsRemaining == 10){
     console.log(seatsRemaining);
-    currentPrice = standardPrice + seatsSold*priceIncrement;
+    currentPrice = standardPrice;
+    seatsRemaining = seatsRemaining - 1;
+    seatsSold = seatsSold + 1;
+    return ("Seat sold for $" + standardPrice + ", " + seatsRemaining + " seats remaining.");
+  }
+  else if(seatsRemaining <10 && seatsRemaining>=6){
+    console.log(seatsRemaining);
+    currentPrice = currentPrice + priceIncrement1;
     seatsRemaining = seatsRemaining - 1;
     seatsSold = seatsSold + 1;
     return ("Seat sold for $" + currentPrice + ", " + seatsRemaining + " seats remaining.");
+  }
+  else if (seatsRemaining < 6 && seatsRemaining>1){
+    console.log(seatsRemaining);
+    currentPrice = currentPrice + priceIncrement2;
+    seatsRemaining = seatsRemaining - 1;
+    seatsSold = seatsSold + 1;
+    return ("Seat sold for $" + currentPrice + ", " + seatsRemaining + " seats remaining.");
+  }
+  else if (seatsRemaining == 1){
+    console.log(seatsRemaining);
+    currentPrice = 91000;
+    seatsRemaining = seatsRemaining - 1;
+    seatsSold = seatsSold + 1;
+     return ("Seat sold for $" + currentPrice + ", " + seatsRemaining + " seats remaining.");
   }
   else {
     return "No more seats available.";
   }
 
-
-  ;
 };
