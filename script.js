67e5c7669337420a9a4a676490b522901b9618a0
wickker <choydianchun@gmail.com>
2020-03-18 22:21:49 +0800

    completed compulsory part

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+/*
 console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
   return "WOW SOMETHING HAPPENED";
 };
+*/
+let originalPrice = 50;
+let seatsSold = 0;
+
+var inputHappened = function (currentInput) {
+  let output;
+  seatsSold++;
+  console.log(seatsSold);
+  if (seatsSold > 10) {
+    output = "Sorry, all seats are sold out";
+    return output;
+  }
+  else {
+    output = originalPrice * Math.pow(1.05,seatsSold);
+    return output.toFixed(2);
+  }
+}
