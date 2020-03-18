e139d843326fb65169ebd4a42ed7cf9e45c01f32
travisenquiry <travisenquiry@gmail.com>
2020-03-18 21:24:49 +0800

    completed further 1

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ console.log("hello script js");
 var seatCounter = 0;
 var seat = 10;
 var normalPrice = 50;
+var price;
+var secondSeatCounter = 0;
 
 var inputHappened = function(currentInput){
   seatCounter = seatCounter + 1;
-  if (seatCounter <= 10){
-    var price = normalPrice + (normalPrice / 100 * (5 * seatCounter));
-    return price;
+    if (seatCounter < 6){
+        price = normalPrice + (normalPrice / 100 * (3 * seatCounter));
+        console.log(price);
+        return price;
+    }else if(seatCounter > 5 && seatCounter < 10){
+        price = price + 2.5;
+        console.log(price);
+        return price;
+    }else if(seatCounter === 10){
+        price = 91000;
+        return price;
     }else {
-        return `Seat sold out.`;
+      return `Seat sold out.`;
     }
 };
