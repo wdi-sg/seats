2c8c3105fa2dd57de1339893401c66c8947fc101
perseen <59989724+perseen@users.noreply.github.com>
2020-03-18 20:09:55 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 console.log("hello script js");
 
+var seats = 10;
+
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  var seatsLeft = 10 - currentInput;
+if (currentInput == 0) {
+    return "Plane is empty"
+} if (currentInput > 0 && currentInput < 6) {
+return "The price of the ticket is $" + 50*1.03 + " and "+  seatsLeft + " seats remaining"
+} else if (currentInput > 5 && currentInput < 10) {
+return "The price of the ticket is $" + 50*1.05 + " and "+  seatsLeft + " seats remaining"
+} else if (currentInput == 10) {
+return "The price of the last ticket is $" + 91000
+}
 };
