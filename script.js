d9aca04ed4ce81fdbe8d7ff442c4596da5012bfc
elisaes <alisa260785@gmail.com>
2020-03-18 21:55:21 +0800

    third part finished

--- script.js
+++ script.js
@@ -1,49 +1,111 @@
-let seats = 10;
+let seats = {
+  economy: 15,
+  business: 6,
+  firstClass: 4
+};
 let seatCost = 50;
 let currentInput;
 document.getElementById("output").innerHTML =
-  "There are 10 sits availables. The cost of the ticket is 50 dolars";
+  "Please choose the cabin: buy economy, buy business or buy first class";
 function inputHappened(currentInput) {
-  if (currentInput == seatCost) {
-    if (seats === 1) {
+  if (currentInput == "buy economy") {
+    if (seats.economy === 1) {
       return "the price of the last ticket is $ 91";
     }
-    if (seats !== 0) {
+    if (seats.economy !== 0) {
+      document.getElementById("input").value = "";
+      if (7 < seats.economy <= 15) {
+        seats.economy = seats.economy - 1;
+        seatCost = seatCost * 0.05 + seatCost;
+        return (
+          "there are " +
+          " " +
+          seats.economy +
+          " " +
+          "seats availables. The cost of the ticket is: " +
+          seatCost +
+          " " +
+          "dollars." + "Please choose the cabin: buy economy, buy business or buy first class"
+        );
+      } else {
+        seats.economy = seats.economy - 1;
+        seatCost = seatCost * 0.05 + seatCost;
+        return (
+          "there are " +
+          " " +
+          seats.economy +
+          " " +
+          "seats availables. The cost of the ticket is: " +
+          seatCost +
+          " " +
+          "dollars." + "Please choose the cabin: buy economy, buy business or buy first class"
+        );
+      }
+    } else {
+      return "Sorry, no available economy class seats";
+    }
+  } else if (currentInput === "buy business") {
+    if (seats.business === 1) {
+      return "the price of the last ticket is $ 91";
+    }
+    if (seats.business  !== 0) {
       document.getElementById("input").value = "";
 
-      if (5 < seats <= 10) {
-        seats = seats - 1;
-        seatCost = seatCost * 0.05 + seatCost;
+      if (3 < seats.business  <= 6) {
+        seats.business  = seats.business  - 1;
+        seatCost = seatCost * 0.06 + seatCost;
         console.log(seatCost);
         return (
           "there are " +
           " " +
-          seats +
+          seats.business  +
           " " +
           "seats availables. The cost of the ticket is: " +
           seatCost +
           " " +
-          "dolars"
+          "dollars." + "Please choose the cabin: buy economy, buy business or buy first class"
         );
       } else {
-        seats = seats - 1;
-        seatCost = seatCost * 0.05 + seatCost;
+        seats.business  = seats.business  - 1;
+        seatCost = seatCost * 0.1 + seatCost;
         return (
           "there are " +
           " " +
-          seats +
+          seats.business  +
           " " +
           "seats availables. The cost of the ticket is: " +
           seatCost +
           " " +
-          "dolars"
+          "dollars." + "Please choose the cabin: buy economy, buy business or buy first class"
         );
       }
     } else {
-      return "Sorry, no more available sits";
+      return "Sorry, no available business class seats";
+    }
+  } else if (currentInput === "buy first class") {
+    if (seats.firstClass === 1) {
+      return "the price of the last ticket is $ 191";
+    }
+    if (seats.firstClass !== 0) {
+      document.getElementById("input").value = "";
+
+      if (seats.firstClass <= 4) {
+        seats.firstClass = seats.firstClass - 1;
+        seatCost = seatCost * 0.15 + seatCost;
+        console.log(seatCost);
+        return (
+          "there are " +
+          " " +
+          seats.firstClass +
+          " " +
+          "seats availables. The cost of the ticket is: " +
+          seatCost +
+          " " +
+          "dollars." + "Please choose the cabin: buy economy, buy business or buy first class"
+        );
+      }
     }
   } else {
-    document.getElementById("input").value = "";
-    return "Please insert the correct amount of the price ticket " + seatCost;
+    return "Sorry, no available first class seats";
   }
 }
