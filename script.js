3d6febbeb5eb55eb73b54909d3bd3573d90240fe
Ianshalom <you@example.comianshalom92@gmail.comgit config --global user.email you@example.comianshalom92@gmail.com>
2020-03-18 20:40:58 +0800

    Part 2 of Seats

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ console.log("hello script js");
 var availSeats = 10;
 var seatPrice = 50;
 
+/*
 var inputHappened = function(currentInput) {
   console.log( currentInput );
 if (availSeats === 10) {
-    availSeats --;
+    availSeats--;
     return seatPrice;
 } else if(availSeats < 10 && availSeats > 0) {
     seatPrice = seatPrice + (seatPrice * 0.05);
@@ -18,6 +19,23 @@ if (availSeats === 10) {
     return "Sorry, we are currently fully booked.";
 }
 }
-
+*/
 
 //Part 2
+var inputHappened = function(currentInput) {
+  console.log( currentInput );
+  if (availSeats === 10) {
+    availSeats--;
+    return "$" + seatPrice + " ----" + " " + availSeats + " seats left.";
+  } else if (availSeats >= 6) {
+    availSeats--;
+    seatPrice = seatPrice + (seatPrice * 0.03);
+    return "$" + seatPrice + " ----" + " " + availSeats + " seats left.";
+  } else if (availSeats > 1) {
+    availSeats--;
+    seatPrice = seatPrice + (seatPrice * 0.05);
+    return "$" + seatPrice + " ----" + " " + availSeats + " seats left.";
+  } else {
+    return "You're our lucky final passenger! Your seat ticket is $91,000.";
+  }
+}
