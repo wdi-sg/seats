83543b81ac794a8e7657b3bf221b9cf2bc83876b
lekhweemeng <lek.hweemeng@live.com>
2020-03-18 22:00:32 +0800

    corrected the wrong formula

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+
+//below are the codes for part 1
     //declaration of values
     var seatSold = 0;
     var originalSeatPrice = 50;
 //actual functions runnin
     var inputHappened = function(currentInput){
         while (seatSold<11){
-    var incrementPerSeatSold = (parseInt(seatSold + 1) * 0.05);
+    var incrementPerSeatSold = ((parseInt(seatSold + 1) * 0.05)*50);
     console.log("Number of seats sold: "+seatSold);
     console.log("increase in price:" + incrementPerSeatSold);
     var currentSeatPrice = (originalSeatPrice + incrementPerSeatSold);
