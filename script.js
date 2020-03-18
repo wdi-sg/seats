a36d70cca987be6baf1c56fced684ce20d239863
Rachelik <rachel.ik.87@gmail.com>
2020-03-18 21:57:58 +0800

    Relabelling

--- script.js
+++ script.js
@@ -8,21 +8,22 @@ var ticketPrice = 50.00;
 //--------
 //PART 1
 //--------
-
-// var inputHappened = function(anyInput){
-// //Seat 1 sold. emptySeats is 10.
-//   emptySeats -= 1;
-//   if(emptySeats === 9) {
-//     //emptySeats is 9 after click. Price is $50.
-//     return "Seat sold (" + emptySeats + " left). Price is $" + ticketPrice.toFixed(2);
-//   } else if(emptySeats >= 0 && emptySeats < 9) {
-//     //Seat 2 - 10 sold. Price increase by 0.05%.
-//     ticketPrice *= 1.05;
-//     return "Seat sold (" + emptySeats + " left). Price is $" + ticketPrice.toFixed(2);
-//   } else {
-//     return "Tickets sold out.";
-//   };
-// };
+/*
+var inputHappened = function(anyInput){
+//Seat 1 sold. emptySeats is 10.
+  emptySeats -= 1;
+  if(emptySeats === 9) {
+    //emptySeats is 9 after click. Price is $50.
+    return "Seat sold (" + emptySeats + " left). Price is $" + ticketPrice.toFixed(2);
+  } else if(emptySeats >= 0 && emptySeats < 9) {
+    //Seat 2 - 10 sold. Price increase by 0.05%.
+    ticketPrice *= 1.05;
+    return "Seat sold (" + emptySeats + " left). Price is $" + ticketPrice.toFixed(2);
+  } else {
+    return "Tickets sold out.";
+  };
+};
+*/
 
 //--------
 //PART 2
@@ -51,3 +52,7 @@ var inputHappened = function(anyInput){
     return "Tickets sold out.";
   };
 };
+
+//--------
+//PART 3
+//--------
