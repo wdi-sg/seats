0d4478704a5544f42a463d12e64356420f2124aa
Ianshalom <you@example.comianshalom92@gmail.comgit config --global user.email you@example.comianshalom92@gmail.com>
2020-03-18 19:59:28 +0800

    Part 1 of Seats

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
+//Part 1
+
+var availSeats = 10;
+var seatPrice = 50;
+
+var inputHappened = function(currentInput) {
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
-};
+if (availSeats === 10) {
+    availSeats --;
+    return seatPrice;
+} else if(availSeats < 10 && availSeats > 0) {
+    seatPrice = seatPrice + (seatPrice * 0.05);
+    availSeats --;
+    return seatPrice;
+} else {
+    return "Sorry, we are currently fully booked.";
+}
+}
+
+
+//Part 2
