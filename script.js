2a1b1921ac591ce63b374d52c14ae30972cce9c7
Dyana Wu <dyanawu@gmail.com>
2020-03-19 00:54:27 +0800

    Further work on 6th part
    - handle rate reporting for first class
    - report only next rate for each class

--- script.js
+++ script.js
@@ -75,13 +75,12 @@ var sellSeat = function (seatClass) {
     bigPlane[seatClass].currentPrice = bigPlane[seatClass].nextPrice[0];
     bigPlane[seatClass].nextPrice = getNextPrice(bigPlane[seatClass].seatsSold, bigPlane[seatClass].currentPrice, seatClass);
     var rateLevel = bigPlane[seatClass].nextPrice[1];
-    cheapSeatsLeft = getCheapSeats(bigPlane[seatClass].seatsSold, seatClass);
     console.log("after", bigPlane[seatClass].seatsSold, bigPlane[seatClass].currentPrice, bigPlane[seatClass].nextPrice);
     if (bigPlane[seatClass].seatsSold === bigPlane[seatClass].seatCount) {
       return `Sold! Your seat cost $${bigPlane[seatClass].currentPrice.toFixed(2)}, as the last seat.`;
     }
     console.log(bigPlane[seatClass].currentPrice);
-    return `Sold! Your seat cost $${bigPlane[seatClass].currentPrice.toFixed(2)}. ${cheapSeatsLeft} more seats at ${rateLevel} rate.`;
+    return `Sold! Your seat cost $${bigPlane[seatClass].currentPrice.toFixed(2)}. The next seat will be sold at the ${rateLevel} rate.`;
   }
   return "Sold out. Try the next flight.";
 }
@@ -90,7 +89,14 @@ var getNextPrice = function (seatsSold, currentPrice, seatClass) {
   var lowRate = bigPlane[seatClass].lowRate;
   var highRate = bigPlane[seatClass].highRate;
 
-  if (bigPlane[seatClass].seatsSold < bigPlane[seatClass].seatCount / 2) {
+  if (seatClass === "first") {
+    if (seatsSold === bigPlane.first.seatCount - 1) {
+      return [bigPlane.first.lastSeat, "last seat"];
+    }
+    return [bigPlane[seatClass].currentPrice * highRate, "standard"];
+  }
+
+  if (bigPlane[seatClass].seatsSold < Math.floor(bigPlane[seatClass].seatCount / 2)) {
     console.log("low");
     return [bigPlane[seatClass].currentPrice * lowRate, "low"];
   } else if (bigPlane[seatClass].seatsSold < bigPlane[seatClass].seatCount - 1) {
@@ -100,11 +106,13 @@ var getNextPrice = function (seatsSold, currentPrice, seatClass) {
   return [bigPlane[seatClass].lastSeat, "last seat"];
 }
 
+/* TODO
 var getCheapSeats = function (seats, seatClass) {
   if (seatClass === "first") {
     return;
   }
 }
+*/
 
 var clearInput = function () {
   document.getElementById("input").value = "";
