0d506ac2c9d9f164d567dd1f38b2c4d4d054c00e
kenneththesheep <kenneththesheep@gmail.com>
2020-03-18 17:57:28 +0800

    added the logic for first and 2nd half with different increase

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 console.log("hello script js");
 var planeSeats =10;
 var planePrice=50;
-const increase=1.05;
-var seatsNumber=["A1","A2","A3","A5","B1","B2","B3","B4","B5"];
+const increaseFirstHalf=1.03;
+const increaseSecondHalf=1.05;
+var seatsNumber=["A1","A2","A3","A4","A5","B1","B2","B3","B4","B5"];
 
 
 var inputHappened = function(currentInput){
@@ -14,7 +15,7 @@ var inputHappened = function(currentInput){
     if(currentInput===seatsNumber[i])
     {
         console.log("Here");
-        output=`Thank you for flying with ABC airline. Your seat number is ${seatsNumber[i]} and it cost ${planePrice}.`;
+        output=`Thank you for flying with ABC airline. Your seat number is ${seatsNumber[i]} and it cost $${planePrice}.`;
         seatsNumber[i]="";
         calculatePriceIncrease();
         planeSeats--;
@@ -35,6 +36,17 @@ var inputHappened = function(currentInput){
 
 };
 var calculatePriceIncrease=function(){
-        planePrice=planePrice*increase;
+        if(planeSeats>5)
+        {planePrice=planePrice*increaseFirstHalf;
+        }
+        else if (planeSeats>2){
+            console.log("2nd half");
+            planePrice=planePrice*increaseSecondHalf;
+
+        } else
+        {
+            planePrice=91000;
+        }
+
 }
 alert(`Good morning. The current seat avaliable is ${seatsNumber}.`);
