c06402cccb184cdf02547749f47858872003ca89
chelsejw <chelsejw@gmail.com>
2020-03-18 17:23:21 +0800

    add functionality for base requirements

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 console.log("hello script js");
 
+var seatsLeft = 10;
+
+function clearInput(){
+  document.getElementById("input").value = ""
+}
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+clearInput();
+if (seatsLeft > 0) {
+  seatsLeft--;
+  console.log(seatsLeft);
+  var basePrice = 50;
+  var inflation = 1.0 + 0.05*(9-seatsLeft)
+  var price = basePrice*inflation;
+  return price;
+} else {
+  return `Seats are sold out!`;
+}
 };
