29161f6e06f1ffb3d4f9a28c452bd7e87435ea0b
jameswong9 <kinchong.wong@gmail.com>
2020-03-18 22:14:25 +0800

    part 1 done

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 console.log("hello script js");
 
+var seatsremaining = 10;
+var Originalticketprice = 50;
+
+//numberOfSeatsChose = currentInput
+
+function calculateSeatsremaining(seatsremaining, currentInput) { if(currentInput < seatsremaining) {    seatsremaining = seatsremaining - currentInput
+}else{
+    return calculateSeatsremaining;
+}
+}
+
+function calculateTicketPrice(seatsremaining, Originalticketprice) {
+    var seatsremaining = 1
+    while (seatremaining < 10) { return Originalticketprice * (Originalticketprice * 5/100); seatsremaining++;
+}
+};
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+console.log( currentInput );
+
+if (isNaN(currentInput)) {
+    return output = "Invalid input!";
+}
+
+if (seatsremaining > 0) {
+Originalticketprice = Originalticketprice + (Originalticketprice * 5/100);
+seatsremaining --;
+return Originalticketprice;
+}
+else if (currentInput < 10) {
+return output = currentInput + "tickets sold!";
+calculateTicketPrice();
+return output =calculateTicketPrice;
+}
+else {
+return output = "Not process";
+}
 };
