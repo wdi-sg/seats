976c7ce750540bac0deca1dda34449012fce483c
ThomasOh92 <ohcsthomas@gmail.com>
2020-03-18 17:32:57 +0800

    Amended initial part 1 functionality - made sure it outputs 52.5 second seat onwards, instead of compounding

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ console.log("hello script js");
 var seatsLeft = 10;
 var seatPrice = 50;
 
-var seatSold = function(){
-    seatsLeft--;
-    seatPrice = seatPrice * 1.05;
-}
+var firstSeat = true;
+
+// var seatSold = function(){
+//     if (seatsLeft > 5){
+//         seatPrice = seatPrice * 1.03;
+//     }
+
+//     seatsLeft--;
+
+// }
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
   var output;
   output = `Your seat is $${seatPrice}`;
-  seatSold();
+
+  if (firstSeat){
+    seatPrice = 50 * 1.05;
+    firstSeat = false;
+  }
+  //seatSold();
 
   return output;
 };
