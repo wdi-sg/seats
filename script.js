35bb00937779aa565221b5dcfba4116f8abcafeb
dannst <arcanic@gmail.com>
2020-03-18 23:49:34 +0800

    Price increases by 3% or 5% over current price. Check if user input === true. Output says how many tickets left before price increase.

--- script.js
+++ script.js
@@ -20,24 +20,33 @@ var inputHappened = function(currentInput){
 	  	console.log('seats remaining: ' + seats);
 	  	console.log('no markup');
 	  	price = 50;
+  		return '1 ticket sold at ' + '$' + price + '. The next ticket will be more expensive.';
+
   } else if (currentInput && (seats > 5)){
 	  	seats -= 1;
 	  	console.log('seats remaining: ' + seats);
 	  	console.log('markup of three percent');
   		price = addThreePercent(price);
+  		return '1 ticket sold at ' + '$' + price + '. There are ' + (seats-4) + ' tickets left before price increase.';
+
   } else if (currentInput && (seats > 1)){
   		seats -= 1;
   		console.log('seats remaining: ' + seats);
   		console.log('markup of five percent');
   		price = addFivePercent(price);
+  		return '1 ticket sold at ' + '$' + price + '. There are ' + (seats) + ' tickets left before price increase.';
+
   } else if (seats === 1){
   		seats -= 1;
   		console.log('seats remaining: ' + seats);
   		price = 91000;
+
   } else if (seats === 0){
   		return 'We have sold out';
+
   } else {
   		return 'I don\'t understand you';
+
   };
-  return 'No. ' + (10-seats) + ' Seat sold at ' + '$' + price;
+  return '1 ticket sold at ' + '$' + price;
 };
