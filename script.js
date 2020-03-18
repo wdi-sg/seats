db62a49d5be962ca9b557cd696727aad0435ae10
Kenan <xinkenan.11@hotmail.com>
2020-03-18 23:37:30 +0800

    added part 1 soluion

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
-};
+// there are two types of plane, one 10 seater, another have 3 cabins
+// for each seat sold the ticket price goes up by 5% over original price
+// tell user how many tickets are left before price bracket goes up
+//
+
+// calculate economy cabin price
+// first half price goes up 3
+// firstHalfPrice =
+
+// seecond half goes up 5
+
+// last seat 91000
+
+let seatsSold = 0
+
+const calPercentageOf = (number, percentage) => (number / 100) * percentage
+const getCurrentSeatPrice = (seatsSold, basePrice, increaseRate) => {
+  const totalPerToIncrease = seatsSold * increaseRate
+  const price = basePrice + calPercentageOf(basePrice, totalPerToIncrease)
+  return price
+}
+
+const processInput = function (currentInput) {
+  // [economy | business | first] to [destination] [buy | cancel]
+  const commands = currentInput.split(' ')
+}
+var inputHappened = function (currentInput) {
+  seatsSold++
+  return getCurrentSeatPrice(seatsSold, 50, 5)
+
+}
