a237efe9fb73e661360a5bfe55470366075a6485
ThomasOh92 <ohcsthomas@gmail.com>
2020-03-18 17:21:10 +0800

    Functionality for Part 1 of the Exercise

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 console.log("hello script js");
 
+var seatsLeft = 10;
+var seatPrice = 50;
+
+var seatSold = function(){
+    seatsLeft--;
+    seatPrice = seatPrice * 1.05;
+}
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  var output;
+  output = `Your seat is $${seatPrice}`;
+  seatSold();
+
+  return output;
 };
