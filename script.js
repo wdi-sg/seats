3aa622af39e319c2740e1d833e2f8ea7eb066ae5
osbornechan <osbornecab@gmail.com>
2020-03-18 20:47:43 +0800

    Completed Part 3 (output remaining tickets)

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var inputHappened = function(currentInput){
 
     if (numSeats === 10) {
         numSeats = numSeats - 1;
-        return output = `Price of your ticket is $${originalPrice}`;
+        return output = `Price of your ticket is $${originalPrice}. There are ${numSeats} tickets left.`;
     }
 
     if (numSeats === 0) {
@@ -14,14 +14,14 @@ var inputHappened = function(currentInput){
     } else if (numSeats <= 9 && numSeats > 5) {
         numSeats = numSeats - 1;
         var newPrice = originalPrice * 1.03;
-        return output = `Price of your ticket is $${newPrice}`;
+        return output = `Price of your ticket is $${newPrice}. There are ${numSeats} tickets left.`;
     } else if (numSeats <= 5 && numSeats > 1) {
         numSeats = numSeats - 1;
         newPrice = originalPrice * 1.05;
-        return output = `Price of your ticket is $${newPrice}`;
+        return output = `Price of your ticket is $${newPrice}. There are ${numSeats} tickets left.`;
     } else if (numSeats === 1) {
         numSeats = numSeats - 1;
         newPrice = 91000;
-        return output = `Price of your ticket is $${newPrice}`;
+        return output = `Price of your ticket is $${newPrice}. There are ${numSeats} tickets left.`;
     }
 };
