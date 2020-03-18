bc75c8ea817550bfcddd25602d9a92d77d3211ef
perseen <59989724+perseen@users.noreply.github.com>
2020-03-19 00:31:59 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 console.log("hello script js");
 
-var seats = 10;
-
+//When a user types anything into the input, consider that seat sold and show the user the price of their seat
+var currentInput = Math.random()* 10 +1
 
 var inputHappened = function(currentInput){
+  console.log( currentInput );
+var currentInput = Math.random()* 10 +1
+
+if (currentInput < 5 && currentInput > 0) {
+   return "Seat number  " + Math.round(currentInput) + " booked. Ticked price is $51.50 "
+} else if (currentInput < 9 && currentInput > 6) {
+ return "Seat number  " + Math.round(currentInput) + " booked. Ticked price is $52.50 "
+ } else {
+    return "Only last ticket available at $91000"
+}
+};
+
+
+
+
+/* var inputHappened = function(currentInput){
   console.log( currentInput );
   var seatsLeft = 10 - currentInput;
 if (currentInput == 0) {
@@ -15,4 +31,4 @@ return "The price of the ticket is $" + 50*1.05 + " and "+  seatsLeft + " seats 
 } else if (currentInput == 10) {
 return "The price of the last ticket is $" + 91000
 }
-};
+}; *?
