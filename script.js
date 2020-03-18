e2cb086ef2f56b343a33d8617e572b4adc1701c1
mdazharaw <darkaz62@hotmail.com>
2020-03-18 20:30:21 +0800

    Part 1 done.

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 console.log("hello script js");
-
+//Part 1
+var seatsRemaining = 10;
+var seatsSold = 0;
+var standardPrice = 50;
+var priceIncrement = 50 *(5/100);
+var currentPrice;
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  if(seatsRemaining>0){
+    console.log(seatsRemaining);
+    currentPrice = standardPrice + seatsSold*priceIncrement;
+    seatsRemaining = seatsRemaining - 1;
+    seatsSold = seatsSold + 1;
+    return ("Seat sold for $" + currentPrice + ", " + seatsRemaining + " seats remaining.");
+  }
+  else {
+    return "No more seats available.";
+  }
+
+
+  ;
 };
