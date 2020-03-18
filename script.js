b28b2925ece573d2dff327a6a41732ab8466ec6e
chelsejw <chelsejw@gmail.com>
2020-03-18 20:56:06 +0800

    fix errors in logic from previous code, added business & economy class options

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 console.log("hello script js");
 
-var seatsLeft = 10;
+var econLeft = 10;
+var businessLeft = 6;
+var firstLeft = 4;
+var surcharge = 1;
+var price = 50
+var category;
+
 
 //Function to clear user input.
 function clearInput() {
@@ -8,33 +14,56 @@ function clearInput() {
 }
 //Function that runs every time a new input is detected.
 var inputHappened = function(currentInput) {
-    clearInput(); //Clears input.
-    //If user gives a valid input "a", run the following commands:
-    if (currentInput === "a") {
-        //If there are still seats, calculate inflation and final price of the ticket.
-        seatsLeft--;
-        console.log(seatsLeft); //For debugging.
-        var inflation;
-        //Inflation for first half of seats
-        if (seatsLeft > 5) {
-            inflation = 1.00 + 0.03 * (9 - seatsLeft);
-            console.log(inflation)
-            //Inflation for second half of seats
-        } else if (seatsLeft > 1) {
-            inflation = 1.12 + 0.05 * (5 - seatsLeft);
-            console.log(inflation);
-            //Fixed price for last ticket.
-        } else if (seatsLeft === 1) {
-            return `You got the last ticket and the price is $91,000.`
-        } else {
-            return `Seats are sold out.`
-        }
-        //Calculate the final price of the ticket with the conditional inflation, to 2 decimal places.
-        var price = (50 * inflation).toFixed(2);
-        //Give the final output with details.
-        return `There are ${seatsLeft} seats left. Your price is $${price}.`;
-        //If user gives an input other than "buy".
+  clearInput();
+
+  //FOR ECONOMY CLASS TICKETS
+  if (currentInput==="e"){
+    econLeft--;
+    if (econLeft===9) {
+      return `Seats left = ${econLeft}. The price of your ticket was $${price}.`
+    } else if (econLeft > 4) {
+      surcharge = 1.03;
+    } else if (econLeft > 0) {
+      surcharge = 1.05;
+    } else if (econLeft===0) {
+      return `Last seat was $91,000.`
     } else {
-        return `Sorry, that was an invalid input.`
+      return `There are no more seats available in economy.`
     }
+    var econPrice = (price*surcharge).toFixed(2);
+    return `Seats left in economy class = ${econLeft}. The price of your ticket was $${econPrice}.`
+
+//FOR BUSINESS CLASS TICKETS
+} else if (currentInput==="b") {
+    businessLeft--;
+    if (businessLeft===5) {
+      return `Seats left = ${businessLeft}. The price of your ticket was $${price}.`
+    } else if (businessLeft > 3) {
+      surcharge = 1.06;
+    } else if (businessLeft > 0) {
+      surcharge = 1.10;
+    } else if (businessLeft===0) {
+      return `Last seat was $91,000.`
+    } else {
+      return `There are no more seats available in business.`
+    }
+    var businessPrice = (price*surcharge).toFixed(2);
+    return `Seats left in business class = ${businessLeft}. The price of your ticket was $${businessPrice}.`
+} else if (currentInput==="f") {
+    firstLeft--;
+    if (firstLeft===3) {
+      return `Seats left = ${firstLeft}. The price of your ticket was $${price}.`
+    } else if (firstLeft > 0) {
+      surcharge = 1.15;
+    } else if (firstLeft===0) {
+      return `Last seat was $191,000.`
+    } else {
+      return `There are no more seats available in first class.`
+    }
+    var firstPrice = (price*surcharge).toFixed(2);
+    return `Seats left in first class = ${firstLeft}. The price of your ticket was $${firstPrice}.`
+  } else {
+    return `Invalid input detected.`
+  }
+
 };
