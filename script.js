2b90f96c4955c0e5abd8be444570ab60eb612d41
chelsejw <chelsejw@gmail.com>
2020-03-18 17:58:09 +0800

    implement checks for input to only accept a buy command

--- script.js
+++ script.js
@@ -8,24 +8,32 @@ function clearInput(){
 
 var inputHappened = function(currentInput){
 clearInput();
-if (seatsLeft > 0) {
-  seatsLeft--;
-  console.log(`There are ${seatsLeft} seats left.`);
-    var basePrice = 50;
-    var inflation;
 
-  //Inflation for first half of seats
-    if (seatsLeft > 4) {
-      inflation = 1.0 + 0.03*(9-seatsLeft)
-    } else if (seatsLeft >1) {
-      inflation = 1.12 + 0.05*(5-seatsLeft)
-    } else {
-      return `You got the last ticket and the price is $91,000.`
-    }
-    var price = (50*inflation).toFixed(2);
+if (currentInput==="buy") {
+  if (seatsLeft > 0) {
+    seatsLeft--;
+    console.log(`There are ${seatsLeft} seats left.`);
+      var inflation = 1;
 
-    return `There are ${seatsLeft} seats left. Surcharge is at ${inflation}. Your price is $${price}.`;
+    //Inflation for first half of seats
+      if (seatsLeft > 4) {
+        inflation = 1.0 + 0.03*(9-seatsLeft)
+      } else if (seatsLeft >1) {
+        inflation = 1.12 + 0.05*(5-seatsLeft)
+      } else if (seatsLeft===1) {
+        return `You got the last ticket and the price is $91,000.`
+      } else  {
+        return `Seats are sold out.`
+      }
+      var price = (50*inflation).toFixed(2);
+      return `There are ${seatsLeft} seats left. Surcharge is at ${inflation}. Your price is $${price}.`;
+  } else {
+    return `Sorry, there are no more seats available.`;
+  }
 } else {
-  return `Seats are sold out!`;
+  return `Sorry, that was an invalid input.`
 }
+
+
+
 };
