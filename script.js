8a3c75a7805ff776a0c41b5b8a289280edf9c260
Rachelik <rachel.ik.87@gmail.com>
2020-03-18 21:28:30 +0800

    Part 1, Add seat left

--- script.js
+++ script.js
@@ -10,15 +10,13 @@ var ticketPrice = 50.00;
 var inputHappened = function(anyInput){
 //Seat 1 sold. emptySeats is 10.
   emptySeats -= 1;
-  if(emptySeats === 9){
+  if(emptySeats === 9) {
     //emptySeats is 9 after click. Price is $50.
-    console.log("Seat left: " + emptySeats);
-    return "Seat sold. Price is $" + ticketPrice.toFixed(2);
+    return "Seat sold (" + emptySeats + " left). Price is $" + ticketPrice.toFixed(2);
   } else if(emptySeats >= 0 && emptySeats < 9) {
     //Seat 2 - 10 sold. Price increase by 0.05%.
-    console.log("Seat left: " + emptySeats);
     ticketPrice *= 1.05;
-    return "Seat sold. Price is $" + ticketPrice.toFixed(2);
+    return "Seat sold (" + emptySeats + " left). Price is $" + ticketPrice.toFixed(2);
   } else {
     return "Tickets sold out.";
   };
