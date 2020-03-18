27ef7dab7c71080bc479fd20046b2a5ebeb0e395
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-03-18 17:30:50 +0800

    First Commit

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 console.log("hello script js");
 
+var seatsLeft = 10;
+var ticketPrice = 50
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+
+  //when user input, deduct one seat
+  seatsLeft--;
+  console.log(seatsLeft);
+
+  //When seat deducted, increase ticket price by 5%
+  ticketPrice = (ticketPrice * 1.05).toFixed(2);
+
+  //initialize input box
+  document.getElementById("input").value = "";
+
+  return ticketPrice;
+
 };
