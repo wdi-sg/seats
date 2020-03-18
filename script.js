24a5a6a945f36cdd81a9fc59fbfb72289c8cc0b3
kokchuantan <kokchuan1996@gmail.com>
2020-03-18 23:04:05 +0800

    added part 6

--- script.js
+++ script.js
@@ -1,73 +1,162 @@
 console.log("hello script js");
-
-var freeSeats = 10;
-var bookedSeats = 0;
+var econFree = 15;
+var econBooked = 0;
+var businessFree = 6
+var businessBooked = 0;
+var firstFree = 4;
+var firstBooked = 0;
+var control = false;
+var econ = false;
+var business = false;
+var first = false;
+// var econFree = 10;
+// var econBooked = 0;
 var price = 50;
 var cost;
 var y = 0;
 var priceIncreased = 1;
-document.querySelector('#output').innerText = 'Hi! There are ' + freeSeats + ' tickets left for sale! \n Please input a number to buy a seat!';
+var classSelected;
+
+document.querySelector('#output').innerText = 'Hi! There are ' + econFree + ' tickets left for sale! \n Please choose among "economy" or "business" or "first" class.';
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  if(parseFloat(currentInput)){
-    if(freeSeats === 10 && bookedSeats === 0){
-        freeSeats--;
-        bookedSeats++;
-        console.log(bookedSeats + ' bookedSeats');
-        console.log(freeSeats + ' freeSeats');
-        return output = 'Your plane ticket costs $' + price + '\n There are ' + freeSeats + ' seats left for sale!';
+    classSelected = currentInput;
+    console.log( currentInput );
+    console.log(classSelected);
+    if(!control){
+        if(classSelected === 'economy'){
+            econ = true;
+            console.log('test');
+            return output = 'Please enter any number to buy a ticket! \n There are ' + econFree + ' seats left.';
+        }
+        else if (classSelected === 'business'){
+            business = true;
+            console.log('test1');
+            return output = 'Please enter any number to buy a ticket! \n There are ' + businessFree + ' seats left.';
+        }
+        else if (classSelected === 'first'){
+            first = true;
+            console.log('test2');
+            return output = 'Please enter any number to buy a ticket! \n There are ' + firstFree + ' seats left.';
+        }
     }
-    else if(freeSeats > 5 && freeSeats <= 10 && bookedSeats < 5 && bookedSeats >= 0){
-    //3% increment per booked seat
-        increase3();
-        freeSeats--;
-        bookedSeats++;
-        cost = price * priceIncreased;
-        console.log(cost + ' cost');
-        console.log(price + ' price');
-        console.log(bookedSeats + ' bookedSeats');
-        console.log(freeSeats + ' freeSeats');
-        console.log('test2');
-        return output = 'Your plane ticket costs $' + cost + '\n There are ' + freeSeats + ' seats left for sale!';
+    if(econ){
+        if(parseFloat(currentInput)){
+            if(econFree > 8 && econFree <= 15 && econBooked < 7 && econBooked >= 0){
+                increase3();
+                econFree--;
+                econBooked++;
+                cost = price * priceIncreased;
+                return output = 'Your plane ticket costs $' + cost + '\n There are ' + econFree + ' seats left for sale!';
+            }
+                else if(econFree > 1 && econFree <= 8 && econBooked < 14 && econBooked >= 7 ){
+                    increase5();
+                    econFree--;
+                    econBooked++;
+                    cost = price * priceIncreased;
+                    return output = 'Your plane ticket costs $' + cost + '\n There are ' + econFree + ' seats left for sale!';
+                }
+                else if(econFree ===1 && econBooked === 14){
+                    econFree--;
+                    econBooked++;
+                    return output = 'You bought the last ticket which costs $21000!';
+                }
+                else if(econFree === 0 && econBooked === 15){
+                    return output = 'Sorry we are fully booked.';
+                }
+            }
+            else{
+                return output = 'Invalid input! Please try again.'
+            }
     }
-    else if(freeSeats > 1 && freeSeats <= 5 && bookedSeats < 9 && bookedSeats >= 5 ){
-    //5% increment per booked seat
-        increase5();
-        freeSeats--;
-        bookedSeats++;
-        cost = price * priceIncreased;
-        console.log(cost + ' cost');
-        console.log(price + ' price');
-        console.log(bookedSeats + ' bookedSeats');
-        console.log(freeSeats + ' freeSeats');
-        console.log('test3');
-        return output = 'Your plane ticket costs $' + cost + '\n There are ' + freeSeats + ' seats left for sale!';
+    if(business){
+        if(parseFloat(currentInput)){
+            if(businessFree > 3 && businessFree <= 6 && businessBooked < 3 && businessBooked >= 0){
+                increase6();
+                businessFree--;
+                businessBooked++;
+                cost = price * priceIncreased;
+                return output = 'Your plane ticket costs $' + cost + '\n There are ' + businessFree + ' seats left for sale!';
+            }
+                else if(businessFree > 1 && businessFree <= 3 && businessBooked < 5 && businessBooked >= 3 ){
+                    increase10();
+                    businessFree--;
+                    businessBooked++;
+                    cost = price * priceIncreased;
+                    return output = 'Your plane ticket costs $' + cost + '\n There are ' + businessFree + ' seats left for sale!';
+                }
+                else if(businessFree ===1 && businessBooked === 5){
+                    businessFree--;
+                    businessBooked++;
+                    return output = 'You bought the last ticket which costs $91000!';
+                }
+                else if(businessFree === 0 && businessBooked === 6){
+                    return output = 'Sorry we are fully booked.';
+                }
+            }
+            else{
+                return output = 'Invalid input! Please try again.'
+            }
     }
-    else if(freeSeats ===1 && bookedSeats === 9){
-        console.log('test4');
-        freeSeats--;
-        bookedSeats++;
-        return output = 'You bought the last ticket which costs $21000!';
+    if(first){
+        if(parseFloat(currentInput)){
+            if(firstFree > 1 && firstFree <= 4 && firstBooked < 3 && firstBooked >= 0){
+                increase15();
+                firstFree--;
+                firstBooked++;
+                cost = price * priceIncreased;
+                return output = 'Your plane ticket costs $' + cost + '\n There are ' + firstFree + ' seats left for sale!';
+            }
+                else if(firstFree ===1 && firstBooked === 3){
+                    firstFree--;
+                    firstBooked++;
+                    return output = 'You bought the last ticket which costs $191000!';
+                }
+                else if(firstFree === 0 && econBooked === 4){
+                    return output = 'Sorry we are fully booked.';
+                }
+            }
+            else{
+                return output = 'Invalid input! Please try again.'
+            }
     }
-    else if(freeSeats === 0 && bookedSeats === 10){
-        return output = 'Sorry we are fully booked.';
+    else{
+        return output = "Invalid input! Please try again."
     }
-  }
-  else{
-    return output = 'Invalid input! Please try again with a number.'
-  }
-}
+};
+
 
 var increase5 = function(){
-    while ( y < bookedSeats){
+    while ( y < econBooked){
         priceIncreased = priceIncreased * 1.05;
         y++;
         console.log(priceIncreased + ' priceIncreased');
     }
 }
 var increase3 = function(){
-    while ( y < bookedSeats){
+    while ( y < econBooked){
         priceIncreased = priceIncreased * 1.03;
+        y++;
+        console.log(priceIncreased + ' priceIncreased');
+    }
+}
+var increase6 = function(){
+    while ( y < businessBooked){
+        priceIncreased = priceIncreased * 1.06;
+        y++;
+        console.log(priceIncreased + ' priceIncreased');
+    }
+}
+var increase10 = function(){
+    while ( y < businessBooked){
+        priceIncreased = priceIncreased * 1.1;
+        y++;
+        console.log(priceIncreased + ' priceIncreased');
+    }
+}
+var increase15 = function(){
+    while ( y < firstBooked){
+        priceIncreased = priceIncreased * 1.15;
         y++;
         console.log(priceIncreased + ' priceIncreased');
     }
