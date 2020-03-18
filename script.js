8f85a8030a53ce18d8ddfc315df17dc4fcac5e5d
lekhweemeng <lek.hweemeng@live.com>
2020-03-18 23:35:48 +0800

    completed part 4 added multiplier

--- script.js
+++ script.js
@@ -1,4 +1,60 @@
-//below are the codes for part 1
+    var seatSold = 1;
+    var originalSeatPrice = 50;
+    var partOnePriceIncrease = 1.03;
+    var partTwoPriceIncrease = 1.05;
+    var currentPrice = 50;
+    var ticketsLeft = 10;
+//actual functions runnin
+    var inputHappened = function(currentInput){
+        while (seatSold<11){
+            if(seatSold<5){
+    console.log("Number of seats sold: "+seatSold);
+    console.log("increase in price:" + partOnePriceIncrease);
+    console.log ("current seat sold at: " + currentPrice);
+    var nextSeatPrice = (currentPrice * partOnePriceIncrease);
+    var twodecpx = nextSeatPrice.toFixed(2)
+    console.log ("next seat price: " + twodecpx);
+    currentPrice = twodecpx;
+    seatSold = (seatSold + 1);
+    ticketsLeft = (ticketsLeft -1);
+    var output = "We are left with: " + ticketsLeft + "tickets!";
+  return output;
+} else if (seatSold>=5 && seatSold<9){
+    console.log("Number of seats sold: "+seatSold);
+    console.log("increase in price:" + partTwoPriceIncrease);
+    console.log ("current seat sold at: " + currentPrice);
+    var nextSeatPrice1 = (currentPrice * partTwoPriceIncrease);
+    var twodecpx1 = nextSeatPrice1.toFixed(2)
+    console.log ("next seat price: " + twodecpx1);
+    currentPrice = twodecpx1;
+    seatSold = (seatSold + 1);
+        ticketsLeft = (ticketsLeft -1);
+    var output = "We are left with: " + ticketsLeft + "tickets!";
+  return output;
+}else if (seatSold===9){
+    console.log("Number of seats sold: "+seatSold);
+    console.log("increase in price:" + partTwoPriceIncrease);
+    console.log ("current seat sold at: " + currentPrice);
+    var nextSeatPrice2 = 91000;
+    console.log ("next seat price: " + nextSeatPrice2);
+    currentPrice = nextSeatPrice2.toFixed(2)
+    seatSold = (seatSold + 1);
+        ticketsLeft = (ticketsLeft -1);
+    var output = "We are left with: " + ticketsLeft + "tickets!";
+  return output;
+}else if (seatSold===10){
+    console.log("Number of seats sold: "+seatSold);
+    console.log("increase in price:" + partTwoPriceIncrease);
+    console.log ("current seat sold at: " + currentPrice);
+    console.log ("next seat price: We are sold out for today!");
+    seatSold = (seatSold + 1);
+    var output = "Sorry there are no tickets left. Please try again tomorrow!";
+      return output;
+}
+}
+};
+
+/*below are the codes for part 2
     //declaration of values
     var seatSold = 1;
     var originalSeatPrice = 50;
@@ -53,7 +109,7 @@
 }
 }
 };
-
+*/
 
 
 /*below are the codes for part 1
