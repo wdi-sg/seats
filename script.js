3ba6715426b42e2d0b8b8fe46c17e49819880cc4
vigbit <vigneshb@live.com>
2020-03-19 06:12:39 +0800

    part1 how to start

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 console.log("hello script js");
 
+//assign ticket price and number of seats as variables
+let ticketPrice, numOfSeats;
+
+ticketPrice = 50 ;
+numOfSeats = 10 ;
+
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
-};
+
+
+// ticket price for first seat
+if ( numOfSeats === 10 ) {
+    // calculate number of seats left
+while ( numOfSeats <= 0);
+//console.log(numOfSeats);
+numOfSeats = numOfSeats - 1;
+//console.log(numOfSeats);
+    return ticketPrice;
+    //ticket price for the balance seats
+} else {
+    return (ticketPrice * (5/100) + ticketPrice);
+}
+
+
+
+}
