068f5c92b2bb55510d65e6d0cc53cbebd7e76b36
qundax <qunda.lim@outlook.sg>
2020-03-18 17:50:35 +0800

    - Added Part 1

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+// Initialisation
+
+var seatsSold = 0;
+var surcharge = 0;
+var price = 50;
+
+document.querySelector("#output").innerText =
+`Welcome to General Airways!
+Would you like to buy a seat?
+
+--- Seats Left : ${10 - seatsSold} ---`
+
+
+
+// Helper functions
+
+function twoDP(num) {
+  return  (Math.floor(num * 100) / 100).toFixed(2);
+}
+
+
+
+// Function
+
+var inputHappened = function(intput){
+  if (seatsSold === 10) {
+    return "Sorry! Seats are all sold out!";
+  }
+
+  if (seatsSold > 0) {
+    price = 50 * 1.05;
+  };
+
+  seatsSold++;
+
+  return `Thank you for your purchase!
+  You bought a seat for $${twoDP(price)}.
+  Would you like to buy another seat?
+
+  --- Seats Left : ${10 - seatsSold} ---`
 };
