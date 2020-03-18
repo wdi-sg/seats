0230708fd14d8a512e090c0f7949b1fba0bbb251
lekhweemeng <lek.hweemeng@live.com>
2020-03-18 20:26:18 +0800

    Added in calculation logic incremental completed part 1

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-console.log("hello script js");
-
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+    //declaration of values
+    var seatSold = 0;
+    var originalSeatPrice = 50;
+//actual functions runnin
+    var inputHappened = function(currentInput){
+        while (seatSold<11){
+    var incrementPerSeatSold = (parseInt(seatSold + 1) * 0.05);
+    console.log("Number of seats sold: "+seatSold);
+    console.log("increase in price:" + incrementPerSeatSold);
+    var currentSeatPrice = (originalSeatPrice + incrementPerSeatSold);
+    console.log ("Current seat price: " + currentSeatPrice);
+    seatSold = (seatSold + 1);
+    var output = "Sold!";
+  return output;
+}
 };
