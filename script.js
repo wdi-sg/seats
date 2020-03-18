74dc0344f98f85d19b836b616b5ff0825d434ed8
ThomasOh92 <ohcsthomas@gmail.com>
2020-03-18 17:46:44 +0800

    Added Functionality for Part 2 of the Exercise. Pricing as follows: First Seat $50 /// Seat 2-4 add 3% ///Seats 5 - 9 add 5% /// Seat 10 is 91k

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 console.log("hello script js");
 
 var seatsLeft = 10;
-var seatPrice = 50;
+var seatPrice;
 
-var firstSeat = true;
-
-// var seatSold = function(){
-//     if (seatsLeft > 5){
-//         seatPrice = seatPrice * 1.03;
-//     }
-
-//     seatsLeft--;
-
-// }
-
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output;
-  output = `Your seat is $${seatPrice}`;
-
-  if (firstSeat){
-    seatPrice = 50 * 1.05;
+var seatSold = function(){
+  //first seat functionality
+  if (seatsLeft === 10){
+    seatPrice = 50;
     firstSeat = false;
   }
-  //seatSold();
+
+  //first half functionality
+  if (seatsLeft < 10 && seatsLeft > 5){
+    seatPrice = 50 * 1.03;
+  }
+
+  //second half functionality
+  if (seatsLeft > 1 && seatsLeft <=5){
+    seatPrice = 50 * 1.05;
+  }
+
+  //last seat functionality
+  if (seatsLeft === 1){
+    seatPrice = "91,000"
+  }
+
+  seatsLeft--;
+
+}
+
+var inputHappened = function(currentInput){
+  //console.log( currentInput );
+  var output;
+  console.log(seatsLeft);
+  seatSold();
+  output = `Your seat is $${seatPrice}`;
 
   return output;
 };
