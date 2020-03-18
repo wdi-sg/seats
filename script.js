857592b9c54b410abd37ce9f56030845b8f522c5
YANNIEYEUNG <K_T_DINO@HOTMAIL.COM>
2020-03-18 22:49:51 +0800

    Added seats tracking logic

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-console.log("hello script js");
 
+var i = 10;
+var inputHappened = function(currentInput){
+  console.log("hello");
+  if (i <10 && i> 0){
+    console.log (i+" tickets left with" );
+    i = i-1 ;
+    var output = "Your ticket cost $52.50 and we are left with " + (i) + " tickets.";
+    return output;
+  } else if (i === 10) {
+    console.log ("50");
+    i = i-1 ;
+    console.log (i+" tickets left." );
+    var output = "Your ticket cost $50 and we are left with " + i + "tickets.";
+    return output;
+  }else {
+    console.log ("no more")}
+    return "SOLD OUT!!!"
+};
+
+
+//console.log("hello script js");
+/*
 var inputHappened = function(currentInput){
   console.log( currentInput );
   return "WOW SOMETHING HAPPENED";
 };
+*/
