8c7d6f2bd4de77527470cb6b02fa21a3e59a87f9
dannst <arcanic@gmail.com>
2020-03-18 23:05:31 +0800

    Part One

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 console.log("hello script js");
 
+// helper functions
+const addThreePercent = function(price){
+	return price * 1.03;
+};
+
+const addFivePercent = function(price){
+	return price * 1.05;
+};
+
+// all seats empty
+let seats = 10;
+let price = 50;
+
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  console.log('User typed ${currentInput}');
+  if (currentInput && (seats === 10)){
+  	seats -= 1;
+  	console.log('seats remaining: ' + seats);
+  	price = 50;
+  } else if (currentInput && (seats > 0)){
+  	seats -= 1;
+  	console.log('seats remaining: ' + seats);
+  	for (i = 10; i > seats; i--){
+  		price = addFivePercent(price);
+  	};
+  } else if (seats === 0){
+  	return 'We have sold out';
+  } else {
+  	return 'I don\'t understand you';
+  };
+  return 'Seat sold at ' + price;
 };
