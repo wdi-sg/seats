2e7fda6df34fc1dcb5f8539b8409ab00d12b3484
Rachelik <rachel.ik.87@gmail.com>
2020-03-18 21:09:38 +0800

    Rachel's Airline Seat Price Calculator

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ console.log("hello script js");
 var emptySeats = 10;
 var ticketPrice = 50.00;
 
-
-
 var inputHappened = function(anyInput){
 //Seat 1 sold. emptySeats is 10.
   emptySeats -= 1;
@@ -14,12 +12,12 @@ var inputHappened = function(anyInput){
   if(emptySeats === 9){
     //emptySeats is 9 after click. Price is $50.
     console.log("Seat left: " + emptySeats);
-    return "Seat sold. Price is $" + ticketPrice + ".";
+    return "Seat sold. Price is $" + ticketPrice.toFixed(2);
   } else if(emptySeats >= 0 && emptySeats < 9) {
     //Seat 2 - 10 sold. Price increase by 0.05%.
     console.log("Seat left: " + emptySeats);
     ticketPrice *= 1.05;
-    return "Seat sold. Price is $" + ticketPrice.toFixed(2) + ".";
+    return "Seat sold. Price is $" + ticketPrice.toFixed(2);
   } else {
     return "Tickets sold out.";
   };
