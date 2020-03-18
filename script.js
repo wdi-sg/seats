65591e2e16e90c7ced423dc7646275fb285d0794
osbornechan <osbornecab@gmail.com>
2020-03-18 21:54:29 +0800

    Completed Part 4 (percentage increment of last ticket)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var originalPrice = 50;
 var numSeats = 10;
 
 var inputHappened = function(currentInput){
-    var newPrice;
+    var newPrice, calcPrice;
 
     if (numSeats === 10) {
         numSeats = numSeats - 1;
@@ -13,11 +13,13 @@ var inputHappened = function(currentInput){
         return 'Sold out!'
     } else if (numSeats <= 9 && numSeats > 5) {
         numSeats = numSeats - 1;
-        var newPrice = originalPrice * 1.03;
+        calcPrice = originalPrice * Math.pow(1.03, (9 - numSeats));
+        newPrice = calcPrice.toFixed(2);
         return output = `Price of your ticket is $${newPrice}. There are ${numSeats} tickets left.`;
     } else if (numSeats <= 5 && numSeats > 1) {
         numSeats = numSeats - 1;
-        newPrice = originalPrice * 1.05;
+        calcPrice = originalPrice * Math.pow(1.03, 4) * Math.pow(1.05, 5 - numSeats);
+        newPrice = calcPrice.toFixed(2);
         return output = `Price of your ticket is $${newPrice}. There are ${numSeats} tickets left.`;
     } else if (numSeats === 1) {
         numSeats = numSeats - 1;
