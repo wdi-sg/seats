de03111ba199515b8a84fbabc155d3050b3752ad
Kenan <xinkenan.11@hotmail.com>
2020-03-19 00:21:04 +0800

    update

--- script.js
+++ script.js
@@ -13,12 +13,40 @@
 // last seat 91000
 
 let seatsSold = 0
+const PRICE_INCRE_RATE_PER_SEAT = 5
+
+class Cabin {
+  constructor (cabinClass, capacity, pricingModel) {
+    this.cabinClass = cabinClass
+    this.capacity = capacity
+    this.pricingModel = pricingModel
+    this.availableSeats = capacity
+  }
+}
 
 const calPercentageOf = (number, percentage) => (number / 100) * percentage
-const getCurrentSeatPrice = (seatsSold, basePrice, increaseRate) => {
+const getPrice = (seatsSold, basePrice, increaseRate) => {
   const totalPerToIncrease = seatsSold * increaseRate
   const price = basePrice + calPercentageOf(basePrice, totalPerToIncrease)
   return price
+}
+
+const EconomyPricingModel = new PricingModel(3, 5, 50, 91000)
+
+class PricingModel {
+  constructor (firstHalfRate, secondHalfRate, basePrice, lastSeatPrice) {
+    this.firstHalfRate = firstHalfRate
+    this.secondHalfRate = secondHalfRate
+    this.basePrice = basePrice
+    this.lastSeatPrice = lastSeatPrice
+  }
+}
+
+firsthalf = getPrice(seatsSold, basePrice, PRICE_INCRE_RATE_PER_SEAT) + additional 3% of <=
+
+
+const getEnhancedPrice = (seatsSold, basePrice, increaseRate) => {
+
 }
 
 const processInput = function (currentInput) {
@@ -27,6 +55,5 @@ const processInput = function (currentInput) {
 }
 var inputHappened = function (currentInput) {
   seatsSold++
-  return getCurrentSeatPrice(seatsSold, 50, 5)
-
+  return getCurrentSeatPrice(seatsSold, 50, PRICE_INCRE_RATE_PER_SEAT)
 }
