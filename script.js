e97f06bbd55bfdbd7ec84765c475552668e69c37
travisenquiry <travisenquiry@gmail.com>
2020-03-19 09:37:29 +0800

    modified part 5

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ var inputHappened = function(currentInput){
         if(currentInput === "buy economy class"){
           seatCounter = seatCounter + 1;
         if(seatCounter < 7){
-          economyClassPrice = economyClassPrice + (economyClassPrice / 100 * (3 * seatCounter));
+          economyClassPrice = Math.floor(economyClassPrice + (economyClassPrice / 100 * (3 * seatCounter)));
           return `Price: $${economyClassPrice}, ${economySeat - seatCounter} seats left.`;
         }else if(seatCounter > 6 && seatCounter < 15){
-          economyClassPrice = economyClassPrice + (economyClassPrice / 100 * (5 * seatCounter));
+          economyClassPrice = Math.floor(economyClassPrice + (economyClassPrice / 100 * (5 * seatCounter)));
           return `Price: $${economyClassPrice}, ${economySeat - seatCounter} seats left.`;
         }else if(seatCounter === 15){
           economyClassPrice = 91000;
@@ -34,10 +34,10 @@ var inputHappened = function(currentInput){
       if(currentInput === "buy business class"){
           businessClassSeatCounter = businessClassSeatCounter + 1;
         if(businessClassSeatCounter < 4){
-          businessClassPrice = businessClassPrice + (businessClassPrice / 100 * (6 * businessClassSeatCounter));
+          businessClassPrice = Math.floor(businessClassPrice + (businessClassPrice / 100 * (6 * businessClassSeatCounter)));
           return `Price: $${businessClassPrice}, ${businessClassSeat - businessClassSeatCounter} seats left.`;
         }else if(businessClassSeatCounter > 3 && businessClassSeatCounter < 6){
-          businessClassPrice = businessClassPrice + (businessClassPrice / 100 * (10 * businessClassSeatCounter));
+          businessClassPrice = Math.floor(businessClassPrice + (businessClassPrice / 100 * (10 * businessClassSeatCounter)));
           return `Price: $${businessClassPrice}, ${businessClassSeat - businessClassSeatCounter} seats left.`;
         }else if(businessClassSeatCounter === 6){
           businessClassPricerice = 91000;
@@ -50,7 +50,7 @@ var inputHappened = function(currentInput){
       if(currentInput === "buy first class"){
           firstClassSeatCounter = firstClassSeatCounter + 1;
         if(firstClassSeatCounter < 4){
-          firstClassPrice = firstClassPrice + (firstClassPrice / 100 * (15 * firstClassSeatCounter));
+          firstClassPrice = Math.floor(firstClassPrice + (firstClassPrice / 100 * (15 * firstClassSeatCounter)));
           return `Price: $${firstClassPrice}, ${firstClassSeat - firstClassSeatCounter} seats left.`;
         }else if(firstClassSeatCounter === 4){
           firstClassPrice = 191000;
