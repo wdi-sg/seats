9dc71a4707710495ea366ba85c541e9894666691
YusofGotboudine <yusof95@hotmail.com>
2020-03-19 09:06:16 +0800

    First Part Commit

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 console.log("hello script js");
+var seats = 10;
+var price = 50 * 1.05;
+var inputHappened = function (currentInput) {
+  console.log(currentInput);
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  seats--;
+
+  return "Seats Available: " + seats + " Price: " + price;
+
 };
