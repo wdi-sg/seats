a694f98be1aea94ea9f7890a14cae3b13324e4a6
Denny Koh <denny.koh.kaichao@gmail.com>
2020-03-19 09:53:10 +0800

    Done up to Economy/Business/FirstClass - trouble doing last 2 part without DOM

--- script.js
+++ script.js
@@ -8,30 +8,44 @@ var befPriceChange;
 var compoundingPrice = 50;
 var lastSeat = 91000;
 
-
-var inputHappened = function (currentInput) {
+var inputHappened = function(currentInput) {
   if (isNaN(currentInput)) {
     return "Sorry please enter Economy , Business or First Class!";
   } else if (totalPlaneSeats === 10) {
     totalPlaneSeats = totalPlaneSeats - 1;
     befPriceChange = totalPlaneSeats - 9;
-    return "Your ticket is $ " +
+    return (
+      "Your ticket is $ " +
       firstSeatOnThePlane +
       " tickets before price change : " +
-      befPriceChange;
+      befPriceChange
+    );
   } else if (totalPlaneSeats < 10 && totalPlaneSeats > 5) {
     totalPlaneSeats = totalPlaneSeats - 1;
     befPriceChange = totalPlaneSeats - 5;
     compoundingPrice = compoundingPrice * 1.03;
-    return "Your ticket is $ " + Math.round((compoundingPrice + Number.EPSILON) * 100) / 100 + " tickets before price change : " +
-      befPriceChange;
+    return (
+      "Your ticket is $ " +
+      Math.round((compoundingPrice + Number.EPSILON) * 100) / 100 +
+      " tickets before price change : " +
+      befPriceChange
+    );
   } else if (totalPlaneSeats < 6 && totalPlaneSeats > 1) {
     totalPlaneSeats = totalPlaneSeats - 1;
     befPriceChange = totalPlaneSeats - 1;
     compoundingPrice = compoundingPrice * 1.05;
-    return "Your ticket is $ " + Math.round((compoundingPrice + Number.EPSILON) * 100) / 100 + " tickets before price change : " + befPriceChange;
+    return (
+      "Your ticket is $ " +
+      Math.round((compoundingPrice + Number.EPSILON) * 100) / 100 +
+      " tickets before price change : " +
+      befPriceChange
+    );
   } else if (totalPlaneSeats === 1) {
     totalPlaneSeats = totalPlaneSeats - 1;
-    return "Your ticket is $ " + lastSeat + " This is the last ticket available!";
+    return (
+      "Your ticket is $ " + lastSeat + " This is the last ticket available!"
+    );
+  } else {
+    return "Sorry no more tickets!";
   }
 };
