b09d0e106ee927b1864e1ff5d5ea53a8967ded52
jordanswp <sohweepin@gmail.com>
2020-03-19 10:09:30 +0800

    edited homework

--- script.js
+++ script.js
@@ -4,29 +4,19 @@ console.log("hello script js");
 var ticketPrice = 50;
 //number of empty seats
 var emptySeats = 10;
+var nextTicket = ticketPrice;
 
-//price of ticket purchased
 var inputHappened = function(currentInput){
     console.log(currentInput);
 
-if(isNaN(currentInput)){
-    return output = "please try again"
-}
-
-if(currentInput > 10){
-    return output = "the maximum puchase is 10."
-}
-
-if(emptySeats <= 0) {
+if(emptySeats === 0) {
     return output = "Plane is full, please wait for next flight";
 }
-
-if(currentInput == 1) {
-    ticketCost = ticketPrice;
-    emptySeats - 1;
-    return output = "Thank you for your purchase, the ticket was $" + ticketCost;
-} else if(currentInput >= 2 && currentInput <= 10){
-    ticketCost = ticketPrice + (currentInput * (ticketPrice * 0.05));
-    return output = "Thank you for your purchase, the ticket was $" + ticketCost;
-}
+    document.getElementById("input").value = ""
+    emptySeats--;
+    console.log(emptySeats);
+    ticketPrice = nextTicket;
+    nextTicket = ticketPrice * 1.05;
+    console.log(nextTicket);
+    return output = "Thank you for your purchase, the ticket was $" + ticketPrice;
 }
