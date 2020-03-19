9d1672da0068e5931485248b9491032ad741523c
“Marcelchia” <marchl.chiayl@gmail.com>
2020-03-19 09:18:48 +0800

    1st part

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  return "Please key in a number 1-10";
 };
+let TicketsSold (input);
+let NoOfSeatsLeft = (10-input);
+
+function (priceOfTicket)
+{x=(50*1.05)^NoOfSeatsleft};
+
+
+Console.log ()
