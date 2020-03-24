71f5dc32bfd8bdf3d0cfd15974a7a5b4415d0cfd
“Marcelchia” <marchl.chiayl@gmail.com>
2020-03-24 18:05:09 +0800

    saved

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 console.log("hello script js");
-
+var NoOfSeatsleft = 10
 var inputHappened = function(currentInput){
   console.log( currentInput );
+    var TicketsSold = currentInput;
+    var NoOfSeatsLeft = 10-input;
   return "Please key in a number 1-10";
 };
-let TicketsSold (input);
-let NoOfSeatsLeft = (10-input);
-
-function (priceOfTicket)
-{x=(50*1.05)^NoOfSeatsleft};
 
 
-Console.log ()
+// function (priceOfTicket)
+// {x=(50*1.05)^NoOfSeatsleft};
+
+
+// Console.log ()
