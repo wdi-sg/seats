3902801bb140a2236131b5b2ccd41b1bf038adce
yonghan <wangyh.92@gmail.com>
2020-06-10 19:48:38 +0800

    verBeforeBaliKL

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ var display = function( data ){
   output.innerText = data;
 }
 
+let planeSeat = 10;
 let economicSeats = 15;
 let businessSeats = 6;
 let firstClassSeats = 4;
-let seatPrice = 50;
-let currentPrice = 0;
+let ecoPrice = 50;
+let busPrice = 50;
+let firstPrice = 50;
 
 
 var inputHappened = function(currentInput){
@@ -23,16 +25,16 @@ var inputHappened = function(currentInput){
     if(economicSeats===1){
       return "Economy seats sold out"
     } else if(economicSeats===15){
-      seatPrice = seatPrice*1;
+      ecoPrice = ecoPrice*1;
     } else if(economicSeats>7){
-    seatPrice = seatPrice*1.05;
+      ecoPrice = ecoPrice*1.05;
   } else if(economicSeats>2){
-  seatPrice = seatPrice*1.03;
+    ecoPrice = ecoPrice*1.03;
   } else if(economicSeats===2){
-    seatPrice = 91000;
+    ecoPrice = 91000;
   }
   economicSeats--;
-    return "Thank you. For Economy, the price is $"+ Math.ceil(seatPrice) + " and there are still " + economicSeats + " seats left";
+    return "Thank you. For Economy, the price is $"+ Math.ceil(ecoPrice) + " and there are still " + economicSeats + " seats left";
   }
 }else if(currentInput==="buy business class"){
   document.querySelector('#input').value="";
@@ -40,16 +42,16 @@ while (businessSeats>=0) {
   if(businessSeats===1){
     return "Seats sold out"
   } else if(businessSeats===6){
-    seatPrice = seatPrice*1;
+    busPrice = busPrice*1;
   } else if(businessSeats>4){
-  seatPrice = seatPrice*1.06;
+    busPrice = busPrice*1.06;
 } else if(businessSeats>2){
-seatPrice = seatPrice*1.1;
+  busPrice = busPrice*1.1;
 } else if(businessSeats===2){
-  seatPrice = 91000;
+  busPrice = 91000;
 }
 businessSeats--;
-  return "Thank you. For business class, the price is $"+ Math.ceil(seatPrice) + " and there are still " + businessSeats + " seats left";
+  return "Thank you. For business class, the price is $"+ Math.ceil(busPrice) + " and there are still " + businessSeats + " seats left";
 }
 }else if(currentInput==="buy first class"){
   document.querySelector('#input').value="";
@@ -57,15 +59,39 @@ while (firstClassSeats>=0) {
   if(firstClassSeats===1){
     return "Seats sold out"
 } else if(firstClassSeats>2){
-seatPrice = seatPrice*1.5;
+  firstPrice = firstPrice*1.5;
 } else if(firstClassSeats===2){
-  seatPrice = 91000;
+  firstPrice = 91000;
 }
 firstClassSeats--;
-  return "Thank you. For first class, the price is $"+ Math.ceil(seatPrice) + " and there are still " + firstClassSeats + " seats left";
+  return "Thank you. For first class, the price is $"+ Math.ceil(firstPrice) + " and there are still " + firstClassSeats + " seats left";
 }
 }
 else{
   return "Please type buy to buy the ticket."
 } 
 };
+
+
+
+
+
+
+// if(currentInput==="buy plane seat"){
+//   document.querySelector('#input').value="";
+// while (planeSeat>=0) {
+//   if(planeSeat===1){
+//     return "Economy seats sold out"
+//   } else if(planeSeat===10){
+//     seatPrice = seatPrice*1;
+//   } else if(planeSeat>5){
+//   seatPrice = seatPrice*1.05;
+// } else if(planeSeat>2){
+// seatPrice = seatPrice*1.03;
+// } else if(planeSeat===2){
+//   seatPrice = 91000;
+// }
+// planeSeat--;
+//   return "Thank you. The price is $"+ Math.ceil(seatPrice) + " and there are still " + planeSeat + " seats left";
+// }
+// }
