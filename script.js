cb280b8bbcb00b34541802c2338f6c491a4a658e
akjw <aseykoh@gmail.com>
2020-06-10 14:44:54 +0800

    step 1

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-console.log("hello script js");
+
+var seatsAvailable = 10;
+var seatsSold = 0;
+var price = 50;
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  if (seatsSold > 0) {
+    price = 50 * 1.05;
+  }
+  if (seatsSold === 10) {
+    return 'Sorry, there are no tickets left!';
+  }
+
+  seatsAvailable--;
+  seatsSold++;
+
+  return `Your total is $${price}. There are ${seatsAvailable} seats left on the plane.`
 };
