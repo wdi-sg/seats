52864f75a8efdbb28678532bd4c51792efd5195f
Darrell Yeo <zeniethlily@gmail.com>
2020-06-10 17:04:32 +0800

    2nd part completed

--- script.js
+++ script.js
@@ -7,11 +7,22 @@ var currentSeats = airplane.seats;
 var currentPrice = 0;
 
 var calculateCurrentPrice = function(){
-  if(currentSeats != airplane.seats){ //as long as airplane isn't empty increase price
+  var seatsTaken = airplane.seats - currentSeats;
+  //for the first half of the plane
+  if(seatsTaken > 0 && seatsTaken < (airplane.seats/2)){
+    currentPrice = airplane.basePrice + (airplane.basePrice * .03);
+  }
+  else if(seatsTaken >= (airplane.seats/2) && seatsTaken < (airplane.seats-1)){
     currentPrice = airplane.basePrice + (airplane.basePrice * .05);
   }
-  else{
+  else if(seatsTaken == 0) { //first seat
     currentPrice = airplane.basePrice;
+  }
+  else if(seatsTaken == (airplane.seats-1)){
+    currentPrice = 91000;
+  }
+  else{
+    currentPrice = 0;
   }
 }
 
