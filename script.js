4e98ab14af5e139bd492a25dbcbfb569ddb551d1
siu-sing <lai.siusing@gmail.com>
2020-06-10 15:50:05 +0800

    added how many tickets left before the price bracket goes up

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ const seatCapacity = 10;
 const lastSeatPrice = 91000;
 var seatsLeft = seatCapacity;
 var currSeatPrice = 50;
+var seatsLeftTillNextBracket=seatCapacity*0.5;;
 
 //function implements a seat sale
 //updates the latest price, updates seatsleft
 var seatSale = function(){
     //update seats left
     seatsLeft -= 1;
+    //update seatsLeftTillNextBracket
+    updateSeatsLeftTillNextBracket();
+    document.querySelector("#msg").innerText = "Seats left till next bracket: "+seatsLeftTillNextBracket
     var sellingPrice = currSeatPrice;
     //update current seat price - 5% over previous price
     updatePrice();
@@ -22,11 +26,12 @@ var seatSale = function(){
     }
 }
 
+//update price based on boss requirement
 var updatePrice = function(){
-    //last seat
+
     console.log("seatsLeft: " + seatsLeft);
     console.log("oldSeatPrice: " + currSeatPrice);
-
+    //last seat
     if(seatsLeft==1){
         currSeatPrice = lastSeatPrice;
     }
@@ -34,12 +39,38 @@ var updatePrice = function(){
     else if(seatsLeft/seatCapacity <=0.5){
         currSeatPrice *= 1.05;
     }
+    //first half of plane
     else {
         currSeatPrice *= 1.03;
     }
     console.log("currSeatPrice: " + currSeatPrice);
 }
 
+var updateSeatsLeftTillNextBracket = function(){
+    if (seatsLeft > seatCapacity*0.5){
+        seatsLeftTillNextBracket=seatsLeft - seatCapacity*0.5;
+    }
+    else if (seatsLeft <= seatCapacity*0.5) {
+        if (seatsLeft==0){
+            seatsLeftTillNextBracket=0;
+        }
+        seatsLeftTillNextBracket=seatsLeft - 1;
+    }
+    console.log("seatsLeftTillNextBracket:" + seatsLeftTillNextBracket);
+}
+
+
+//Event listeners
+document.querySelector('#input').addEventListener('change', function(event){
+        var currentInput = event.target.value;
+        var result = inputHappened(currentInput);
+        display( result );
+      });
+
+      var display = function( data ){
+        var output = document.querySelector('#output');
+        output.innerText = data;
+      }
 
 var inputHappened = function(currentInput){
   // console.log( currentInput );
