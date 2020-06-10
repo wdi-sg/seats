a9cce495842b62325c0aadaa7716c079fddce470
yonghan <wangyh.92@gmail.com>
2020-06-10 15:31:51 +0800

    ver1

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-console.log("hello script js");
+document.querySelector('#input').addEventListener('change', function(event){
+  var currentInput = event.target.value;
+  var result = inputHappened(currentInput);
+  display( result );
+});
+
+var display = function( data ){
+  var output = document.querySelector('#output');
+  output.innerText = data;
+}
+
+let planeseats = 10;
+let seatPrice = 50;
+
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  while (planeseats>=0) {
+    if(planeseats===0){
+      return "Seats sold out"
+    }
+    seatPrice = seatPrice*1.05;
+    planeseats--;
+    return "The price is $"+ Math.ceil(seatPrice) + " and there are still " + planeseats + " seats left";
+  }
 };
