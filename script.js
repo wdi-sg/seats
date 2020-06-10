f7b593143b55a1b6e0d61eb3f94d4dc634bb0f87
joey <joeywongsw@gmail.com>
2020-06-10 23:04:26 +0800

    editted js for part 1

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 console.log("hello script js");
 
+var seat= 10;
+var seatPrice= 50;
+var increasedSeatPrice= 50*1.05;
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+
+  if (currentInput != "") {
+
+    if (seat == 10) {
+      seat --;
+      output= "Thank you for your purchase! Your ticket was $" + seatPrice + ". We have "+ seat + " seats left.";
+    }
+
+    else if (seat < 10 && seat > 0) {
+       seat --;
+      output= "Thank you for your purchase! Your ticket was $" + increasedSeatPrice.toFixed(2) + ". We have " + seat + " seats left.";
+    }
+
+    else {
+      output= "We're all out of seats! Better luck next time!"
+    }
+  }
+  return output;
 };
