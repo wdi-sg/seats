74c906d5291e54497915ebe413ea6c834e5c6ee9
Fabien Lim Zheng Kai <fabienlimzk@gmail.com>
2020-06-10 17:37:11 +0800

    Change pricing to increase by 3% and 5% over the current price

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function inputHappened(currentInput){
   console.log(currentInput);
   if (currentInput != ""){
     if(seats >= 6 && seats <= 9){
-        //seat 2 to 5 is 3% more than original price
+        //seat 2 to 5 is 3% more than previous price
         seats--;
-        price = 50 * 1.03;
+        price = price * 1.03;
     } else if (seats >= 2 && seats <= 5){
-        //seat 6 to 9 is 5% more than original price
+        //seat 6 to 9 is 5% more than previous price
         seats--;
-        price = 50 * 1.05;
+        price = price * 1.05;
     } else if (seats > 0 && seats == 1){
         //seat 10 is $91000
         seats--;
