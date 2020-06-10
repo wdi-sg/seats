4cea89c9463e966631f752f42c46964e1e46bd94
zackSolidd <zacktjc@gmail.com>
2020-06-10 23:30:34 +0800

    completed how to start

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-console.log("hello script js");
+var totalSeat = 10;
+var startPrice = 50;
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
-};
+document.getElementById("pricing").innerHTML = startPrice +" for your ticket";
+document.getElementById("seatLeft").innerHTML = totalSeat+" seats left";
+function buyTicket(){
+  if(totalSeat <= 10 && totalSeat > 1){
+    document.getElementById("thankYouMessage").innerHTML = "Thank you please pay me "+ startPrice+" NOW!"
+    totalSeat -= 1;
+    startPrice = Number(startPrice * 1.05).toFixed(2);
+    document.getElementById("seatLeft").innerHTML = totalSeat+" seats left";
+    document.getElementById("pricing").innerHTML = startPrice +" for your ticket";
+    console.log(totalSeat);
+    console.log(startPrice);
+    return startPrice;
+    return totalSeat;
+  }
+  else if (totalSeat == 1 ){
+    document.getElementById("thankYouMessage").innerHTML = "Thank you please pay me "+ startPrice+" NOW!"
+    document.getElementById("pricing").innerHTML = "call us to pay big money for ticket";
+    document.getElementById("seatLeft").innerHTML = "no seats left, come back tomorrow";
+  }
+}
+
