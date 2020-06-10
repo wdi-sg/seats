5bb5de897e7dd898bf5907f3eda4c7d9fcc69d59
mrsweet5 <cherry123@live.com.sg>
2020-06-11 00:09:44 +0800

    candy

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-console.log("hello script js");
+var totalTicket= 10;
+const ticketCost= 52.5;
+const ticket2Cost=50;
+var currCost= 0;
+var ticketSold=0;
+document.getElementById("totalTicket").innerHTML = totalTicket;
+document.getElementById("ticketCost").innerHTML = ticketCost;
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
-};
+function buyTicket(){
+  totalTicket-=1;
+  ticketSold +=1;
+  var ticketsLeft=totalTicket-ticketSold;
+  // currCost=ticket2Cost+(0.05*ticket2Cost*ticketSold);
+  if (totalTicket<=10 && totalTicket>5) {
+    currCost=ticket2Cost+0.03*ticket2Cost
+  }
+  else if (totalTicket<=5 && totalTicket>2) {
+    currCost=ticket2Cost+0.05*ticket2Cost
+  }
+  else if(totalTicket==1) {
+    currCost="91000"
+  }
+  else if(totalTicket==0){
+    currCost="NIL"
+  }
+  else {
+  const button = document.querySelector('buy');
+    button.disabled = true;
+  }
+  document.getElementById("ticketCost").innerHTML = currCost;
+  document.getElementById("totalTicket").innerHTML = totalTicket;
+  document.getElementById("ticketsLeft").innerHTML = ticketsLeft;
+ }
+
