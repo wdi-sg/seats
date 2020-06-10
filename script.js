464caec659003f79811e1dfc69e3535845230b34
Fabien Lim Zheng Kai <fabienlimzk@gmail.com>
2020-06-10 15:34:26 +0800

    Hi

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+let seats = 10; //total number of seats
+let price = 50; //price of each seats
+
+
+function inputHappened(currentInput){
+  console.log(currentInput);
+  if (currentInput != ""){
+    if(seats < 10 && seats > 0){
+        seats--;
+        price = price * 1.05;
+    } else if (seats == 10){
+        seats--;
+    }
+    return "You have bought a seat, the price of your seat is $" + price +".";
+  }
 };
