8f305dba20353f9ec7e7ca70ebcff8705787a84c
siu-sing <lai.siusing@gmail.com>
2020-06-10 15:07:48 +0800

    added basic functionality

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 console.log("hello script js");
 
+var seatCapacity = 10;
+var seatsLeft = seatCapacity;
+var currSeatPrice = 50;
+
+//function implements a seat sale
+//updates the latest price, updates seatsleft
+var seatSale = function(){
+    var sellingPrice = currSeatPrice;
+    //update current seat price - 5% over previous price
+    currSeatPrice *=1.05;
+    //update seats left
+    seatsLeft -= 1;
+    //output price of the seat solde
+    if(seatsLeft<0){
+        return "no seats left";
+    }
+    else {
+        return sellingPrice;
+    }
+}
+
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  return seatSale();;
 };
