e8e986012f60748beb90b18b5bfb62dac752e306
Darrell Yeo <zeniethlily@gmail.com>
2020-06-10 15:50:18 +0800

    basic functionality

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 console.log("hello script js");
+var airplane = {
+  seats: 10,
+  basePrice: 50
+}
+var currentSeats = airplane.seats;
+var currentPrice = 0;
+
+var calculateCurrentPrice = function(){
+  if(currentSeats != airplane.seats){ //as long as airplane isn't empty increase price
+    currentPrice = airplane.basePrice + (airplane.basePrice * .05);
+  }
+  else{
+    currentPrice = airplane.basePrice;
+  }
+}
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  //console.log( currentInput );
+  calculateCurrentPrice();
+  console.log(currentPrice);
+  currentSeats--;
+  console.log(currentSeats);
+  return "Your ticket costs $" + currentPrice;
 };
+
+
