79173e5f818b97539849d0ac36783162f9deb83d
akjw <aseykoh@gmail.com>
2020-06-10 16:35:44 +0800

    add finer-grained pricing

--- script.js
+++ script.js
@@ -3,17 +3,31 @@ var seatsAvailable = 10;
 var seatsSold = 0;
 var price = 50;
 
+//Finer-grade pricing
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  if (seatsSold > 0) {
-    price = 50 * 1.05;
-  }
-  if (seatsSold === 10) {
-    return 'Sorry, there are no tickets left!';
-  }
+ if (seatsSold === 9) {
+        price = 91000;
+        return `Lucky you! You bought the last ticket at $${price}!`;
+    }
+
+  if (seatsSold > 0 && seatsSold < 5){
+        price = 50 * 1.03;
+    }
+
+    if (seatsSold === 9) {
+        price = 91000;
+        return `Lucky you! You bought the last ticket at $${price}!`;
+    }
+
+    if (seatsSold > 5) {
+        price = 50 * 1.05;
+    }
 
   seatsAvailable--;
   seatsSold++;
 
-  return `Your total is $${price}. There are ${seatsAvailable} seats left on the plane.`
+  return `Your total is $${price}. There are ${seatsAvailable} seats left on the plane.`;
 };
+
+//
