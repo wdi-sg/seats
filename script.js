39e581f8e2653230e7e079ed60133fc773a83e42
exhlim <exhlimm@gmail.com>
2020-08-11 22:44:53 +0800

    sei-24-Eugene Part 5 and 6

--- script.js
+++ script.js
@@ -1,40 +1,62 @@
 console.log("hello script js");
-var seatsLeft = numSeats(90950, 3, 5, 50);
-var currentPrice = 50, oriPrice = 50, increment = 0;
-var planeSeats = numSeats(90950, 3, 5, 50);
+var ecoSeats = 15, bizSeats = 6, firstSeats = 4;
+var ecoPrice = 50, bizPrice = 50, firstPrice = 50;
+var currentPriceEco = 50, currentPriceBiz = 50, currentPriceFirst = 50;
 var inputHappened = function(currentInput){
-    if(currentInput.toLowerCase() == "buy" || currentInput.toLowerCase() == "skippart1" || currentInput.toLowerCase() == "skippart2"){
-    if(seatsLeft == 1){
-        currentPrice += 2.5;
-        return "No more seats left and the final price of is $" + currentPrice + ".";
+    if(currentInput.toLowerCase() == "buy first class"){
+        increasePrice("f", firstSeats);
+        return output(firstSeats);
     }
-    if(currentInput == "skipPart1"){
-        while(seatsLeft > Math.ceil(planeSeats/2)){
-            increasePrice(0.03);
-        }
-        return "Price of seat is $" + currentPrice.toFixed(2) + ". " + seatsLeft + "/" + planeSeats + " seats left";
+    if(currentInput.toLowerCase() == "buy business class"){
+            increasePrice("b", bizSeats);
+            return output(bizSeats);
     }
-    if(currentInput == "skipPart2"){
-        while(seatsLeft !== 1){
-            increasePrice(0.05);
-        }
-        return "Price of seat is $" + currentPrice.toFixed(2) + ". " + seatsLeft + "/" + planeSeats + " seats left";
+    if(currentInput.toLowerCase() == "buy economy class"){
+            increasePrice("e", ecoSeats);
+            return output(ecoSeats);
     }
-    if(seatsLeft > planeSeats/2){
-        return increasePrice(0.03);
-    }
-    else if(seatsLeft < planeSeats/2 && seatsLeft >= 0){
-        return increasePrice(0.05);
-    }
-}
     else
-        return "Please input 'Buy' to purchase a ticket";
+        return "Please input valid buy option. [Buy first class, Buy Business class or Buy Economy class.] \n" + output();
 };
-function numSeats(priceOfLastSeat, x, y, price) {
-    return 2 * priceOfLastSeat/((x * price /100 + y * price/100));
+function increasePrice(x, seats) {
+    switch(x) {
+        case "f":{
+            firstSeats--;
+            if(seats !== 2)
+                {currentPriceFirst *= 1.15; break;}
+            else
+                {currentPriceFirst = 191000; break;}
+        }
+        case "b": {
+            bizSeats--;
+            if(seats == 2)
+                {currentPriceBiz = 91000; break;}
+            else if(seats < 4)
+                {currentPriceBiz *= 1.1; break;}
+            else
+                {currentPriceBiz *= 1.06; break;}
+        }
+        case 'e': {
+            ecoSeats--;
+            if(seats == 2)
+                {currentPriceEco = 91000; break;}
+            else if(seats < 9)
+                {currentPriceEco *= 1.05; break;}
+            else
+                {currentPriceEco *= 1.03; break;}
+        }
+    }
+
 }
-function increasePrice(x) {
-    currentPrice *= 1 + x;
-    seatsLeft--;
-    return "Price of seat is $" + currentPrice.toFixed(2) + ". " + seatsLeft + "/" + planeSeats + " seats left";
+function output() {
+    return outputEco(ecoSeats) + outputBiz(bizSeats) + outputFirst(firstSeats);
+}
+function outputEco(x) {
+    return x == 0 ? "No more seats for Economy Class." : "Current available seats are: \n Economy: " + ecoSeats + "/15 seats. Current Price: $" + currentPriceEco.toFixed(2);
+}
+function outputBiz(x) {
+    return x == 0 ? "\n No more seats for Business Class." : ".\n Business: " + bizSeats + "/6 seats. Current Price: $" + currentPriceBiz.toFixed(2);
+}
+function outputFirst(x) {
+    return x == 0 ? "\n No more seats for First Class" : ".\n First Class: " + firstSeats + "/4 seats. Current Price: $" + currentPriceFirst.toFixed(2) + ".";
 }
