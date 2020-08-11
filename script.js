78c76eaaccbc72175723239c07f2aa7f28c37296
exhlim <exhlimm@gmail.com>
2020-08-11 19:59:59 +0800

    sei-24 Eugene part 2 & 3

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 console.log("hello script js");
-var seats = 10, price = 50, oriPrice = 50, increment = 0;
+var seatsLeft = numSeats(90950, 3, 5, 50);
+var currentPrice = 50, oriPrice = 50, increment = 0;
+var planeSeats = numSeats(90950, 3, 5, 50);
 var inputHappened = function(currentInput){
-    if(seats == 1){
-        oriPrice += increment;
-        return "No more seats left and the final price of is $" + oriPrice + ".";}
-    increment = price * 0.05;
-    seats--, oriPrice += increment;
-    return "Price of seat is $" + oriPrice.toFixed(2) + ". " + seats + "/10 seats left";
+    if(seatsLeft == 1){
+        currentPrice += 2.5;
+        return "No more seats left and the final price of is $" + currentPrice + ".";
+    }
+    if(currentInput == "skipPart1"){
+        while(seatsLeft > Math.ceil(planeSeats/2)){
+            increasePrice(0.03);
+        }
+        return "Price of seat is $" + currentPrice.toFixed(2) + ". " + seatsLeft + "/" + planeSeats + " seats left";
+    }
+    if(currentInput == "skipPart2"){
+        while(seatsLeft !== 1){
+            increasePrice(0.05);
+        }
+        return "Price of seat is $" + currentPrice.toFixed(2) + ". " + seatsLeft + "/" + planeSeats + " seats left";
+    }
+    if(seatsLeft > planeSeats/2){
+        return increasePrice(0.03);
+    }
+    else if(seatsLeft < planeSeats/2 && seatsLeft >= 0){
+        return increasePrice(0.05);
+    }
 };
+function numSeats(priceOfLastSeat, x, y, price) {
+    return 2 * priceOfLastSeat/((x * price /100 + y * price/100));
+}
+function increasePrice(x) {
+    currentPrice += oriPrice * x;
+    seatsLeft--;
+    return "Price of seat is $" + currentPrice.toFixed(2) + ". " + seatsLeft + "/" + planeSeats + " seats left";
+}
