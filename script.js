ac2bc96f0a876241af0d472da0999db3601161cd
clairetay96 <clairetay96@gmail.com>
2020-08-11 20:49:20 +0800

    Added stepped increases and no of seats left in the pricing bracket.

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-var emptySeats = 10
+var totalSeats = 10
+var emptySeats = totalSeats
 var percentagePrice = 1.00
 var price = 50
+var seatsLeft = emptySeats - totalSeats/2 -1
+
+
 
 var inputHappened = function(currentInput){
-  if (emptySeats > 0){
-      emptySeats -= 1
-      pricePaid = (price * percentagePrice).toFixed(2)
-      percentagePrice += 0.05
-      return "you paid " + pricePaid + " dollars for your ticket."
+   if (emptySeats == 1) {
+    emptySeats -= 1
+    return "You paid $91000. You bought the last seat."
+  } else if (emptySeats == 0) {
+    return "There are no more empty seats!"
+  } else if (emptySeats > totalSeats/2 && emptySeats < totalSeats){
+      percentagePrice += 0.03
+      seatsLeft = emptySeats - totalSeats/2 - 1
+  } else if(emptySeats <= totalSeats/2 && emptySeats > 1){
+    percentagePrice += 0.05
+    seatsLeft = emptySeats - 2
   }
-  return "no more empty seats!"
+  emptySeats -= 1
+  var pricePaid = (percentagePrice*price).toFixed(2)
+  console.log(emptySeats)
+  return `You paid ${pricePaid}. There are ${seatsLeft} seats left till the next pricing bracket.`
 
 };
