f2ed37c6659fb0d23853abc938c6f2edcc57525b
Vincent <vincent.ong90@gmail.com>
2020-08-12 00:47:35 +0800

    completed 4th and 5th further

--- script.js
+++ script.js
@@ -6,38 +6,90 @@ For each ticket sold, the price will increase by 1.05
 currentInpunt.trim() will remove the whitespace from the input
 */
 
-var price = 50;
-var seats = 10;
+var economyPrice = 50;
+var businessPrice = 50;
+var firstPrice = 50;
+var economySeats = 15;
+var businessSeats = 6;
+var firstSeats = 4;
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  var value = currentInput.trim();
+  var value = currentInput;
 
-  if (value) {
-    if (seats !== 0) {
-      if (seats === 1) {
-        price = 91000;
-        seats--;
-        var output = "No. ticket left: " + seats + " Current price: $" + price;
-        return output;
-      } else if (seats > 5) {
-        price *= 1.03;
-        price = parseFloat(price.toFixed(2));
-        seats--;
-        var output = "No. ticket left: " + seats + " Current price: $" + price;
-        return output;
-      } else {
-        price *= 1.05;
-        price = parseFloat(price.toFixed(2));
-        seats--;
-        var output = "No. ticket left: " + seats + " Current price: $" + price;
-        return output;
-      }
+  if (value === "buy economy class") {
+    if (economySeats > 1) {
+      economySeats--
+      var econ = economy(economySeats);
+      return econ;
     } else {
-      var output = "Seats are sold out!";
+      var output = "Economy.C seats are sold out!";
+      return output;
+    }  
+  } else if (value === "buy business class") {
+    if (businessSeats > 1) {
+      businessSeats--
+      var busi = business(businessSeats);
+      return busi;
+    } else {
+      var output = "Business.C seats are sold out!";
+      return output;
+    }  
+  } else if (value === "buy first class") {
+    if (firstSeats > 1) {
+      firstSeats--
+      var fi = first(firstSeats);
+      return fi;
+    } else {
+      var output = "First.C seats are sold out!";
       return output;
     }  
   } else {
-    return "You key nothing!"
+    return "Please key in only (buy first class), (buy business class), (buy economy class)"
+  }
+};
+
+var economy = function(economySeats) {
+  for (var i = 0; i < economySeats; i++) {
+    if (economySeats === 1) {
+      economyPrice = 91000;
+    } else if (economySeats > 5) {
+      economyPrice *= 1.03;
+      economyPrice = parseFloat(economyPrice.toFixed(2));
+    } else {
+      economyPrice *= 1.05;
+      economyPrice = parseFloat(economyPrice.toFixed(2));
+    }
+    var output = "Economy ticket left: " + economySeats + " Current price: $" + economyPrice;
+      return output;
+  }
+};
+
+var business = function(businessSeats) {
+  for (var i = 0; i < businessSeats; i++) {
+    if (businessSeats === 1) {
+      businessPrice = 91000;
+    } else if (businessSeats > 3) {
+      businessPrice *= 1.06;
+      businessPrice = parseFloat(businessPrice.toFixed(2));
+    } else {
+      businessPrice *= 1.1;
+      businessPrice = parseFloat(businessPrice.toFixed(2));
+    }
+    var output = "Business ticket left: " + businessSeats + " Current price: $" + businessPrice;
+      return output;
+  }
+};
+
+var first= function(firstSeats) {
+  for (var i = 0; i < firstSeats; i++) {
+    if (firstSeats === 1) {
+      firstPrice = 191000;
+    } else {
+      firstPrice *= 1.15;
+      firstPrice = parseFloat(firstPrice.toFixed(2));
+    }
+    var output = "First ticket left: " + firstSeats + " Current price: $" + firstPrice;
+      return output;
   }
 };
