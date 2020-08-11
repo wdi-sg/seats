b704e657985927450bc0c0b0c370fdb3d6559ff2
joey <joeywongsw@gmail.com>
2020-08-12 01:28:34 +0800

    Edited index.html and script.js.Incomplete commit.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 console.log("hello script js");
 
+var ticket = "";
 var state = 0;
-var seatPrice = 50;
-var seats = 10;
+var economySeatPrice = 50;
+var businessSeatPrice = 50;
+var firstClassSeatPrice = 50;
+var economySeats = 15;
+var businessSeats = 6;
+var firstClassSeats = 4;
 var confirmation = "Ticket purchased! Your ticket was $";
 var seatsRemaining = "\n\nSeats Available before price bracket goes up: ";
-var next = "\n\nInput 'Buy' to purchase another ticket, or 'Done' to end the transaction.";
+var next = "\n\nInput 'Buy Economy', 'Buy Business', or 'Buy First Class' to purchase another ticket, or 'Done' to end the transaction.";
 
 var inputHappened = function(currentInput){
 
@@ -14,24 +19,56 @@ var inputHappened = function(currentInput){
     *ticket sold when user inputs anything
     */
 
-    if ( currentInput.toLowerCase() == "buy" && seats != 0 ) {
+    // if ( currentInput.toLowerCase() ==  "buy economy") {
+    //     ticket = "economy";
+    //     var output = ""
+    // }
 
-        if ( seats <= 10 && seats > 5 ) {
-        seats --;
-        seatPrice *= 1.03;
-        var cheaperSeats = seats - 5;
-        var output = confirmation + seatPrice.toFixed(2) + "." + seatsRemaining + cheaperSeats + next;
+    if ( currentInput.toLowerCase() == "buy economy" && economySeats != 0 ) {
+
+        if ( economySeats <= 15 && economySeats > 8 ) {
+        economySeats --;
+        economySeatPrice *= 1.03;
+        var cheaperSeats = economySeats - 8;
+        console.log(cheaperSeats);
+        var output = confirmation + economySeatPrice.toFixed(2) + "." + seatsRemaining + cheaperSeats + next;
         }
 
-        else if ( seats > 1 && seats < 6 ) {
-        seats --;
-        seatPrice *= 1.05;
-        cheaperSeats = seats - 1;
-        output = confirmation + seatPrice.toFixed(2) + "." + seatsRemaining + cheaperSeats + next;
+        else if ( economySeats > 1 && economySeats < 9 ) {
+        economySeats --;
+        economySeatPrice *= 1.05;
+        var cheaperSeats = economySeats - 1;
+        console.log(cheaperSeats);
+        output = confirmation + economySeatPrice.toFixed(2) + "." + seatsRemaining + cheaperSeats + next;
         }
 
-        else if ( seats == 1 ) {
-        seats --;
+        else if ( economySeats == 1 ) {
+        economySeats --;
+        var lastSeat = 91000;
+        output = "You got the last seat! Your ticket was $" + lastSeat + "."
+        }
+    }
+
+    else if ( currentInput.toLowerCase() == "buy business" && businessSeats != 0 ) {
+
+        if ( businessSeats <= 6 && businessSeats > 3 ) {
+        businessSeats --;
+        businessSeatPrice *= 1.06;
+        var cheaperSeats = businessSeats - 3;
+        console.log(cheaperSeats);
+        var output = confirmation + businessSeatPrice.toFixed(2) + "." + seatsRemaining + cheaperSeats + next;
+        }
+
+        else if ( businessSeats > 1 && businessSeats < 4 ) {
+        businessSeats --;
+        businessSeatPrice *= 1.10;
+        var cheaperSeats = businessSeats - 1;
+        console.log(cheaperSeats);
+        output = confirmation + businessSeatPrice.toFixed(2) + "." + seatsRemaining + cheaperSeats + next;
+        }
+
+        else if ( businessSeats == 1 ) {
+        businessSeats --;
         var lastSeat = 91000;
         output = "You got the last seat! Your ticket was $" + lastSeat + "."
         }
@@ -47,17 +84,16 @@ var inputHappened = function(currentInput){
         state = 2;
     }
 
-    else if ( seats == 0 ) {
+    else if ( economySeats = 0 || businessSeats == 0 ) {
         output = "We're all out of tickets!"
     }
 
     else {
-        output = "Input not recognised! \n\nPlease input 'Buy' to purchase a ticket or 'Quit' to cancel the transaction."
+        output = "Input not recognised! \n\nPlease input 'Buy Economy', 'Buy Business', or 'Buy First Class' to purchase a ticket or 'Quit' to cancel the transaction."
     }
 
 
   input.value="";
-  console.log(seats);
   return output;
 
 };
