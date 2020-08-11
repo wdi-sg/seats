4ab9d32f8e25a2ca9bdf6e3c896af422c6083891
Vincent <vincent.ong90@gmail.com>
2020-08-11 21:12:56 +0800

    Completed part 1

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 console.log("hello script js");
 
+/*
+price (number) -> seats (number)
+For each ticket sold, the price will increase by 1.05
+currentInpunt.trim() will remove the whitespace from the input
+*/
+
+var price = 50;
+var seats = 10;
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+  var value = currentInput.trim();
+
+  if (value) {
+    if (seats !== 0) {
+      price *= 1.05;
+      price = parseFloat(price.toFixed(2));
+      var output = "current seats: " + seats + " price: " + price;
+      seats--;
+      return output;
+    } else {
+      var output = "Seats are sold out!";
+      return output;
+    }  
+  } else {
+    return "You key nothing!"
+  }
 };
