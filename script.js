551cde39b287b76351963eda40c02bb0b07be33e
joey <joeywongsw@gmail.com>
2020-08-12 00:43:36 +0800

    Editted index.html and script.js. Completed further 3 and 4.

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
 console.log("hello script js");
 
-var firstSeat = 50;
+var state = 0;
+var seatPrice = 50;
 var seats = 10;
-var confirmation = "Ticket purchased! Your ticket was $"
-var seatsRemaining = "\n\nSeats Available before price bracket goes up: "
+var confirmation = "Ticket purchased! Your ticket was $";
+var seatsRemaining = "\n\nSeats Available before price bracket goes up: ";
+var next = "\n\nInput 'Buy' to purchase another ticket, or 'Done' to end the transaction.";
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
 
-  /*when plane tickets = 10, ticket = $50
-  *plane tickets 1-9, ticket $50*1.05
-  *ticket sold when user inputs anything
-  */
+    /*when plane tickets = 10, ticket = $50
+    *plane tickets 1-9, ticket $50*1.05
+    *ticket sold when user inputs anything
+    */
 
-  if ( seats <= 10 && seats > 5 ) {
-    seats --;
-    var cheaperSeats = seats - 5;
-    console.log(seats);
-    var output = confirmation + threePercent() + "." + seatsRemaining + cheaperSeats;
-  }
+    if ( currentInput.toLowerCase() == "buy" && seats != 0 ) {
 
-  else if ( seats > 1 && seats < 6 ) {
-    seats --;
-    cheaperSeats = seats - 1;
-    console.log(seats);
-    output = confirmation + fivePercent() + "." + seatsRemaining + cheaperSeats;
-  }
+        if ( seats <= 10 && seats > 5 ) {
+        seats --;
+        seatPrice *= 1.03;
+        var cheaperSeats = seats - 5;
+        var output = confirmation + seatPrice.toFixed(2) + "." + seatsRemaining + cheaperSeats + next;
+        }
 
-  else if ( seats == 1 ) {
-    seats --;
-    var lastSeat = 91000;
-    console.log(seats);
-    output = "You got the last seat! Your ticket was $" + lastSeat + "."
-  }
+        else if ( seats > 1 && seats < 6 ) {
+        seats --;
+        seatPrice *= 1.05;
+        cheaperSeats = seats - 1;
+        output = confirmation + seatPrice.toFixed(2) + "." + seatsRemaining + cheaperSeats + next;
+        }
 
-  else {
-    output = "We're all out of tickets!"
-  }
+        else if ( seats == 1 ) {
+        seats --;
+        var lastSeat = 91000;
+        output = "You got the last seat! Your ticket was $" + lastSeat + "."
+        }
+    }
+
+    else if ( currentInput.toLowerCase() == "quit" || state == 1 ) {
+        output = "Transaction Cancelled! Thank you."
+        state = 1;
+    }
+
+    else if ( currentInput.toLowerCase() == "done" || state == 2 ) {
+        output = "Transaction Ended! Thank you."
+        state = 2;
+    }
+
+    else if ( seats == 0 ) {
+        output = "We're all out of tickets!"
+    }
+
+    else {
+        output = "Input not recognised! \n\nPlease input 'Buy' to purchase a ticket or 'Quit' to cancel the transaction."
+    }
+
 
   input.value="";
+  console.log(seats);
   return output;
 
 };
