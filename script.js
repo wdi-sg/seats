be315ff5ea708515d2f004f20919baa4985c01de
Aurelia Lim <aureliadotlim@gmail.com>
2020-08-11 21:20:05 +0800

    completed further 1

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 console.log("hello script js");
 
 
-let price = 47.5;
+let price = 50;
 let seatsSold = 0;
 let seatsLeft = 10;
+const seatNum = 10;
 
 
-
-
+//for loop for looping user input
 var priceCalc = function(currentInput)  {
   for( i=0; i < currentInput ; i++) {
     seatsSold ++;
     seatsLeft --;
-    price += 2.5
-    console.log(price);
-    console.log(seatsSold);
-    console.log(seatsLeft)
+    console.log("seats sold is" + seatsSold);
+    console.log("seats left is" + seatsLeft);
+  //if statements to check price
+    if(seatsSold === 1) {
+      price = 50;
+    } else if (seatsSold > 1 && seatsSold < 6){
+      price *= 1.03;
+      console.log("seats sold is" + seatsSold);
+      console.log("seats left is" + seatsLeft);
+      console.log("price is " + price);
+    } else if (seatsSold < 9 && seatsSold > 5) {
+      price += 1.05;
+      console.log("seats sold is" + seatsSold);
+      console.log("seats left is" + seatsLeft);
+      console.log("price is " + price);
+    } else if (seatsSold === 9) {
+      price = 91000;
+    }
+    
   }
   return `Lucky you! There are ${seatsLeft} seats left and you get your most expensive seat at ${price}`
 }
