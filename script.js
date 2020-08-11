3db1d8d09368f8ba18f6cdc0f41038aa4eee5eaa
exhlim <exhlimm@gmail.com>
2020-08-11 19:04:04 +0800

    sei-24-Eugene Part 1

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 console.log("hello script js");
-
+var seats = 10, price = 50, oriPrice = 50, increment = 0;
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
+    if(seats == 1){
+        oriPrice += increment;
+        return "No more seats left and the final price of is $" + oriPrice + ".";}
+    increment = price * 0.05;
+    seats--, oriPrice += increment;
+    return "Price of seat is $" + oriPrice.toFixed(2) + ". " + seats + "/10 seats left";
 };
