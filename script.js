6010820c716d22bda25ea7e65ee3bac5b0e26aef
Aurelia Lim <aureliadotlim@gmail.com>
2020-08-11 20:18:29 +0800

    first part of exercise

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPENED";
-};
+
+let price = 47.5;
+let seatsSold = 0;
+let seatsLeft = 10;
+
+
+
+
+var priceCalc = function(currentInput)  {
+  for( i=0; i < currentInput ; i++) {
+    seatsSold ++;
+    seatsLeft --;
+    price += 2.5
+    console.log(price);
+    console.log(seatsSold);
+    console.log(seatsLeft)
+  }
+  return `Lucky you! There are ${seatsLeft} seats left and you get your most expensive seat at ${price}`
+}
